f99c1368c6e02272c8b612d3c28f63dfa2921d1d
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2021-11-26 01:18:42 +0530

    Time: 1 ms (95.43%), Space: 52.8 MB (11.04%) - LeetHub

---
+++ reverse-string/reverse-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        if (s == null)
+            return;
+        int l = 0, r = s.length - 1;
+        while (l < r) {
+            char tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+            l++; r--;
+        }
+    }
+}
