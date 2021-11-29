972dde3d037a285c5c83bd4b724a535da4b36651
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2021-11-30 02:22:35 +0530

    Time: 8 ms (95.11%), Space: 10.9 MB (43.14%) - LeetHub

---
+++ two-sum/two-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> imap;
+    
+    for (int i = 0;; ++i) {
+        auto it = imap.find(target - nums[i]);
+        
+        if (it != imap.end()) 
+            return vector<int> {i, it->second};
+            
+        imap[nums[i]] = i;
+    }
+}
+};
