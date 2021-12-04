02007e0b86a217442782e48857b152cac98e98ed
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2021-12-05 02:33:51 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (56.61%) - LeetHub

---
+++ combination-sum-iii/combination-sum-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    std::vector<std::vector<int> > combinationSum3(int k, int n) {
+        std::vector<std::vector<int> > res;
+        std::vector<int> combination;
+        combinationSum3(n, res, combination, 1, k);
+        return res;
+    }
+private:
+    void combinationSum3(int target, std::vector<std::vector<int> > &res, std::vector<int> &combination, int begin, int need) {
+        if (!target) {
+            res.push_back(combination);
+            return;
+        }
+        else if (!need)
+            return;
+        for (int i = begin; i != 10 && target >= i * need + need * (need - 1) / 2; ++i) {
+            combination.push_back(i);
+            combinationSum3(target - i, res, combination, i + 1, need - 1);
+            combination.pop_back();
+        }
+    }
+};
