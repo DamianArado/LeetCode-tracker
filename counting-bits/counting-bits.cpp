a2f3545c92bdb0f4af4c170555c777903cae2988
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2021-12-29 19:33:04 +0530

    Time: 5 ms (32.75%), Space: 8 MB (44.89%) - LeetHub

--- counting-bits/counting-bits.cpp
+++ counting-bits/counting-bits.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> v(num + 1, 0);
         // when you multiply a number by 2, you simply shift all the bits to the left. 
         // If it's even, you're done, if it's odd, you add 1 more.
-        for(int i = 1;i <=  num; i++) 
+        for(int i = 1; i <=  num; i++) 
             v[i] = v[i / 2] + i % 2;
         return v;
     }
