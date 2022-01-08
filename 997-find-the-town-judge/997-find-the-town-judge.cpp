e2a7e0d16b67c8a11cb812d1e6a5d13181e75659
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-01-08 13:56:37 +0530

    Time: 148 ms (95.00%), Space: 60.6 MB (97.97%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
     int findJudge(int N, vector<vector<int>>& trust) {
-      vector<vector<int>> knows(N + 1, vector<int>(N + 1));
-      for (auto &t : trust) knows[t[0]][t[1]] = 1;
-      return findCelebrity(N, knows);
-    }
-    int findCelebrity(int n, vector<vector<int>>& knows, int i = 1) {
-      for (auto j = i + 1; j <= n; ++j) if (knows[i][j]) i = j;
-      for (auto j = 1; j < i; ++j) if (knows[i][j]) return -1;
-      for (auto j = 1; j <= n; ++j) if (i != j && !knows[j][i]) return -1;
-      return i;
+      vector<int> balance(N + 1);
+      for (auto &t : trust) --balance[t[0]], ++balance[t[1]];
+      for (auto i = 1; i <= N; ++i) if (balance[i] == N - 1) return i;
+      return -1;
     }
 };
