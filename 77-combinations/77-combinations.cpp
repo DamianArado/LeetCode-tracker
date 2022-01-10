dd82443d3daf191c792e444f1d74b7836ce0a8c8
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-01-11 00:43:37 +0530

    Time: 50 ms (29.98%), Space: 9 MB (89.22%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,22 +1,22 @@
 class Solution {
 private:
-    void backtrack(vector<vector<int>>&ans,vector<int>&curr,int n,int k,int index){
-        if(curr.size()==k){
-            ans.push_back(curr);
+    void backtrack(vector<vector<int>>&ans, vector<int>&curr, int n, int k, int index) {
+        if(curr.size() == k) {
+            ans.emplace_back(curr);
             return;
-        } else {
-            for(int i=index;i<=n;i++){
-                curr.push_back(i);
-                backtrack(ans,curr,n,k,i+1);
+        } else { 
+            for(int i = index; i <= n; ++i) {
+                curr.emplace_back(i);
+                backtrack(ans, curr, n, k, i + 1);
                 curr.pop_back();
             }
         }
     }
 public:
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>>ans;
-        vector<int>curr;
-        backtrack(ans,curr,n,k,1);
+        vector<vector<int>> ans;
+        vector<int> curr;
+        backtrack(ans, curr, n, k, 1);
         return ans;
     }
 };
