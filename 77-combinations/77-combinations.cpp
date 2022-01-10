cce0da2f2e51a449aaed2e79056c057138305077
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-01-11 00:10:34 +0530

    Time: 55 ms (27.75%), Space: 8.7 MB (98.71%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -14,7 +14,7 @@ public:
              * If the index is at the end of the vector c, then (because the other conditions are
              * obeyed), we know we have a valid combination, so push it to our ans vector<vector<>>
              */
-			else if (i == k - 1) result.push_back(nums);
+			else if (i == k - 1) result.emplace_back(nums);
             
             /* 
              * Move index to the right and set the element at that index equal to the element at the previous index.
