2e25759b1a734c28f372ff4731d3afc80cd6e513
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-01-10 21:35:28 +0530

    Time: 105 ms (34.62%), Space: 22.3 MB (67.37%) - LeetHub

---
+++ 491-increasing-subsequences/491-increasing-subsequences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void helper(vector<int> &nums, vector<int> &subs, set<vector<int>> &result, int index) {
+    
+        for (int i = index; i < nums.size(); ++i) {
+            if (subs.size() == 0 || nums[i] >= subs.back()) {
+                subs.push_back(nums[i]);
+                helper(nums, subs, result, i + 1);
+                subs.pop_back();
+            }
+        }
+        if (subs.size() >= 2) result.insert(subs);
+    }
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        set<vector<int>> res;
+        vector<int> subs;
+        helper(nums, subs, res, 0);
+        return vector<vector<int>>(res.begin(), res.end());
+    }
+};
