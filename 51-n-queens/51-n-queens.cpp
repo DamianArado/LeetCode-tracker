0a211762fb85a52cca58f14469f7ba096dfccf6a
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-01-12 20:20:53 +0530

    Time: 4 ms (92.16%), Space: 7.3 MB (70.76%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,23 +1,34 @@
 class Solution {
 private:
     void solve(int col, vector<string> &board, vector<vector<string>> &ans, 
-    vector<int> &leftRow, vector<int> &upperDiagonal, vector<int> &lowerDiagonal, int n) {
+    vector<bool> &leftRow, vector<bool> &upperDiagonal, vector<bool> &lowerDiagonal, int n) {
+        // check if we reached the last column
         if(col == n) {
+            // add to current board of queens
             ans.emplace_back(board);
             return;
         }
+        // for each row check if we can place a queen or not
         for(int row = 0; row < n; ++row) {
-            if(leftRow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[n - 1 + col - row] == 0) {
-                board[row][col] = 'Q';
-                leftRow[row] = 1;
-                lowerDiagonal[row + col] = 1;
-                upperDiagonal[n - 1 + col - row] = 1;
-                solve(col + 1, board, ans, leftRow, upperDiagonal, lowerDiagonal, n);
-                // backtracking
-                board[row][col] = '.';
-                leftRow[row] = 0;
-                lowerDiagonal[row + col] = 0;
-                upperDiagonal[n - 1 + col - row] = 0;
+            /*
+             * leftRow = all the rows to the left of the cell([row][col])
+             * lowerDiagonal = all the cells to the bottom left of the cell([row][col])
+             * upperDiagonal = all the cells to the top right of the cell([row][col])
+             *
+             */
+            if(leftRow[row] == false && lowerDiagonal[row + col] == false 
+               && upperDiagonal[n - 1 + col - row] == false) {
+                    // if no queen at these positions we place one
+                    board[row][col] = 'Q';
+                    leftRow[row] = true;
+                    lowerDiagonal[row + col] = true;
+                    upperDiagonal[n - 1 + col - row] = true;
+                    solve(col + 1, board, ans, leftRow, upperDiagonal, lowerDiagonal, n);
+                    // backtracking
+                    board[row][col] = '.';
+                    leftRow[row] = false;
+                    lowerDiagonal[row + col] = false;
+                    upperDiagonal[n - 1 + col - row] = false;
             }
         }
     }
@@ -27,7 +38,8 @@ public:
         vector<string> board(n);
         string s(n, '.');
         for(int i = 0; i < n; ++i) board[i] = s;
-        vector<int> leftRow(n, 0), upperDiagonal(2*n - 1, 0), lowerDiagonal(2*n - 1, 0);
+        // auxilary space required for storing whether a queen is present or not
+        vector<bool> leftRow(n, false), upperDiagonal(2*n - 1, false), lowerDiagonal(2*n - 1, false);
         solve(0, board, ans, leftRow, upperDiagonal, lowerDiagonal, n);
         return ans;
     }
