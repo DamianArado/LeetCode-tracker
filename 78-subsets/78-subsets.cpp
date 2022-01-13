b10b2a3c8d608de15ba90f5fd65865515bc266dc
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-01-13 18:56:25 +0530

    Time: 9 ms (6.75%), Space: 7.1 MB (63.23%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -4,9 +4,8 @@ public:
         vector<vector<int>> ans;
         for(int nos = 0; nos < (1 << nums.size()); ++nos) {
             vector<int> ds;
-            for(int j = 0; j < nums.size(); ++j) {
+            for(int j = 0; j < nums.size(); ++j) 
                 if(nos & (1 << j)) ds.emplace_back(nums[j]);
-            }
             ans.emplace_back(ds);
         }
         return ans;
