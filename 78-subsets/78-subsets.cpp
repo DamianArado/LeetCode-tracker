f68c9bbdeb060323a41eca61b24e5873e7aa4201
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-01-13 19:00:13 +0530

    Time: 8 ms (12.22%), Space: 7.2 MB (63.23%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -3,10 +3,10 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         for(int nos = 0; nos < (1 << nums.size()); ++nos) {
-            vector<int> ds;
+            vector<int> subset;
             for(int j = 0; j < nums.size(); ++j) 
-                if(nos & (1 << j)) ds.emplace_back(nums[j]);
-            ans.emplace_back(ds);
+                if(nos & (1 << j)) subset.emplace_back(nums[j]);
+            ans.emplace_back(subset);
         }
         return ans;
     }
