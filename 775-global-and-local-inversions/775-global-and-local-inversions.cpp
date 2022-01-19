d62a3c6310244e95aca2f481f38b6b43642650c6
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-01-19 18:28:46 +0530

    Time: 206 ms (36.11%), Space: 83.3 MB (74.11%) - LeetHub

--- 775-global-and-local-inversions/775-global-and-local-inversions.cpp
+++ 775-global-and-local-inversions/775-global-and-local-inversions.cpp
@@ -1,8 +1,24 @@
+/*
+
+Basic on this idea, I tried to arrange an ideal permutation.
+Then I found that to place number i
+I could only place i at A[i-1], A[i] or A[i+1].
+So it came up to me,
+It will be easier just to check if all A[i] - i equals to -1, 0 or 1
+
+*/
 class Solution {
 public:
-    bool isIdealPermutation(vector<int>& A) {
-        for (int i = 0; i < A.size(); ++i)
-            if (abs(A[i] - i) > 1) return false;
+    /*
+    The problem asks us to find whether the number of global inversions are equal to local inversion.
+    And we know all local inversion are global. Why? .Because local inversions are basically gobal 
+    with a distance as one between them.So if we can find at least one global inversion which is 
+    not local our job is done and we can eliminate by returning false.
+    And since we are maintaining the maximum value all the cases will be covered in it.
+    */
+    bool isIdealPermutation(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); ++i)
+            if (abs(nums[i] - i) > 1) return false;
         return true;
     }
 };
