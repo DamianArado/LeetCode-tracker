d64f1d73f5e5c3c3bd3984483ed0515ce9819ce0
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-01-27 21:57:58 +0530

    Time: 311 ms (5.18%), Space: 83.9 MB (54.67%) - LeetHub

---
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -0,0 +1,23 @@
+// Convert all the 0s in array to -1. Now if an array has equal 0s and 1, the sum will be 0. Use unordered map to check 
+// if you encountered the same sum before. If you did it means all the elements between the last time you encountered 
+// the sum and now add up to zero, i.e. you have equal 0s and 1s. Keep updating result accordingly.
+
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int res = 0;
+        for(int i = 0; i < nums.size(); ++i) {
+            if(nums[i] == 0) nums[i] = -1;
+        }
+        
+        unordered_map<int,int> mp;
+        int sum = 0;
+        for(int i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
+            if(sum == 0) res = i + 1;
+            else if(mp.find(sum) == mp.end()) mp[sum] = i;
+            else res = max(res, i - mp[sum]);
+        }
+        return res;
+    }
+};
