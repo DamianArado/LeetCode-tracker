c50b6fbf5dc2e6dec2512be68cd32ce5a4b320d0
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-02-03 13:54:29 +0530

    Time: 7 ms (78.39%), Space: 14.8 MB (82.33%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -8,7 +8,8 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-// Recursive approach: TC -> O(n), SC -> O(n)
+/*
+// Recursive approach: TC -> O(m + n), SC -> O(m + n)
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
@@ -22,5 +23,30 @@ public:
             list2->next = mergeTwoLists(list1, list2->next);
             return list2;
         }
+    }
+};
+*/
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // Edge cases
+        if(list1 == NULL) return list2;
+        if(list2 == NULL) return list1;
+        ListNode *tail = new ListNode(-1);
+        ListNode *ans = tail;
+        while(list1 != NULL && list2 != NULL) {
+            if(list1->val < list2->val) {
+                tail->next = list1;
+                tail = list1;
+                list1 = list1->next;
+            } else {
+                tail->next = list2;
+                tail = list2;
+                list2 = list2->next;
+            }
+        }
+        if(list1 == NULL) tail->next = list2;
+        else tail->next = list1;
+        return ans->next;
     }
 };
