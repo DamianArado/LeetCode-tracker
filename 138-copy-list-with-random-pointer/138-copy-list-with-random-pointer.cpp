dff4ca1b26928a0caaac70ebe471fa5f60c8b621
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-02-12 22:21:47 +0530

    Time: 8 ms (83.54%), Space: 11.2 MB (60.18%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -15,25 +15,68 @@ public:
 */
 
 class Solution {
-public:
-    Node* copyRandomList(Node* head) {
-        // firstly make a linked list of copied nodes
-        Node *temp = head;
-        // store copy nodes having same values: {A, A'}, {B, B'}, {C, C'}, etc 
-        unordered_map<Node*, Node*> m;
+private:
+    // make a copy of the original list
+    void copyList(Node* head) {
+        Node *temp = head, *nxt = head->next;
         while(temp) {
             Node *copy = new Node(temp->val);
-            m[temp] = copy;
-            temp = temp->next;
+            temp->next = copy;
+            copy->next = nxt;
+            temp = nxt;
+            if(nxt != NULL) nxt = nxt->next;
         }
-        temp = head;
-        // make connections using the copy nodes: deep-copy
+    }
+    // each node of original list now point to copy and each node of copy point to the next original node
+    void random(Node* head) {
+        Node *temp = head;
         while(temp) {
-            m[temp]->next = m[temp->next];
-            m[temp]->random = m[temp->random];
+            if(temp->random) temp->next->random = temp->random->next;
+            temp = temp->next->next;
+        }
+    }
+    // separate the original and copy linked list
+    Node* detach(Node* head) {
+        Node* dummy = new Node(-1);
+        Node *tail = dummy, *temp = head;
+        while(temp) {
+            tail->next = temp->next;
+            tail = tail->next;
+            temp->next = tail->next;
             temp = temp->next;
         }
-        // return copy's head: m[head] === m[head']
-        return m[head];
+        return dummy->next;
+    }
+public:
+    Node* copyRandomList(Node* head) {
+        if(head == NULL) return NULL;
+        copyList(head);
+        random(head);
+        return detach(head);
     }
 };
+
+/*
+    O(n) extra space -
+    
+        Node* copyRandomList(Node* head) {
+            // firstly make a linked list of copied nodes
+            Node *temp = head;
+            // store copy nodes having same values: {A, A'}, {B, B'}, {C, C'}, etc 
+            unordered_map<Node*, Node*> m;
+            while(temp) {
+                Node *copy = new Node(temp->val);
+                m[temp] = copy;
+                temp = temp->next;
+            }
+            temp = head;
+            // make connections using the copy nodes: deep-copy
+            while(temp) {
+                m[temp]->next = m[temp->next];
+                m[temp]->random = m[temp->random];
+                temp = temp->next;
+            }
+            // return copy's head: m[head] === m[head']
+            return m[head];
+        }
+*/
