56537a2742541a7e20467ec8e84e19375716a8b0
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-02-19 17:26:07 +0530

    Time: 3 ms (40.79%), Space: 6.3 MB (38.28%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,5 +1,45 @@
 class Solution {
 public:
+/* Follow up: O(1) Space
+Can you do it in O(N) time and O(1) space?
+I believe you have one difficulty here:
+When we meet a char, we are not sure if it will be still there or be deleted.
+
+However, we can do a back string compare (just like the title of problem).
+If we do it backward, we meet a char and we can be sure this char won't be deleted.
+If we meet a '#', it tell us we need to skip next lowercase char.
+
+The idea is that, read next letter from end to start.
+If we meet #, we increase the number we need to step back, until back = 0*/
+
+    bool backspaceCompare(string S, string T) {
+        int i = S.length() - 1, j = T.length() - 1, back;
+        while (true) {
+            back = 0;
+            while (i >= 0 && (back > 0 || S[i] == '#')) {
+                back += S[i] == '#' ? 1 : -1;
+                i--;
+            }
+            back = 0;
+            while (j >= 0 && (back > 0 || T[j] == '#')) {
+                back += T[j] == '#' ? 1 : -1;
+                j--;
+            }
+            if (i >= 0 && j >= 0 && S[i] == T[j]) {
+                i--;
+                j--;
+            } else {
+                break;
+            }
+        }
+        return i == -1 && j == -1;
+    }
+};
+    
+    /* 
+    
+    SC and TC -> O(s.length() + t.length())
+    
     bool backspaceCompare(string s, string t) {
         stack<char> st1;
         for(int i = 0; i < s.length(); ++i) {
@@ -24,3 +64,5 @@ public:
         return !x1.compare(x2);
     }
 };
+
+*/
