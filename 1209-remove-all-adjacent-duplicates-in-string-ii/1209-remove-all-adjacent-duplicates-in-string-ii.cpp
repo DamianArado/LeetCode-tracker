3f7db4dcb5d6d0ba0fd522110f931f3ec8619a45
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-02-20 21:02:01 +0530

    Time: 16 ms (93.74%), Space: 10.8 MB (47.84%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -12,3 +12,18 @@ public:
         return s.substr(0, j);
     }
 };
+
+/*
+
+string removeDuplicates(string s, int k) {
+  vector<pair<char, short>> st;
+  string res;
+  for (auto ch : s) {
+    if (st.empty() || st.back().first != ch) st.push_back({ ch, 0 });
+    if (++st.back().second == k) st.pop_back();
+  }
+  for (auto& p : st) res += string(p.second, p.first);
+  return res;
+}
+
+*/
