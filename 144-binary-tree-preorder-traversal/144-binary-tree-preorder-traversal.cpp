04b0013b4d9a5a4f4a03e464946eaccd31c99b22
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-03-05 17:24:08 +0530

    Time: 3 ms (56.89%), Space: 8.4 MB (77.75%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -11,20 +11,28 @@
  */
 class Solution {
 public:
-    // TC & SC -> O(n) 
+    // Morris Traversal - O(n) TC & O(1) SC
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        if(!root) return ans;
-        stack<TreeNode*> s;
-        // Root -> Left -> Right
-        s.emplace(root);
-        while(!s.empty()) {
-            TreeNode *temp = s.top();
-            s.pop();
-            ans.emplace_back(temp->val);
-            if(temp->right) s.emplace(temp->right);
-            if(temp->left) s.emplace(temp->left);
+        vector<int> preorder;
+        if(!root) return preorder;
+        TreeNode *curr = root;
+        while(curr) {
+            if(!curr->left) {  // if there is nothing on left it's the leftmost
+                preorder.emplace_back(curr->val);  // print the current root
+                curr = curr->right;  // move right from here
+            } else {  // we have something on left
+                TreeNode *prev = curr->left;  // use prev as marker for the left subtree analysis
+                while(prev->right && prev->right != curr) prev = prev->right; // till we reach the rightmost node of that left subtree
+                if(!prev->right) {  // we reached the rightmost node of the left subtree
+                    preorder.emplace_back(curr->val);  // print the current root
+                    prev->right = curr;  // create the thread that joins the rightmost element to the current root
+                    curr = curr->left;  // now move towards left from here
+                } else {  // if the thread already exists
+                    prev->right = NULL;  // cut the thread that exists
+                    curr = curr->right;  // now move right from here
+                }
+            }
         }
-        return ans;
+        return preorder;
     }
 };
