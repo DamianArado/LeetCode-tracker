0d50039837c060e6dc391166a2b0bd3bccf815f4
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-03-05 15:13:55 +0530

    Time: 3 ms (57.90%), Space: 8.4 MB (76.36%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -11,21 +11,26 @@
  */
 class Solution {
 public:
-    // Iterative version - O(n) TC & SC
+    // Morris Traversal - O(n) time and O(1) space
     vector<int> inorderTraversal(TreeNode* root) {
-        stack <TreeNode*> s;
-        vector <int> inorder;
-        TreeNode *node = root;
-        while (1) {
-            if (node) {
-                s.emplace(node);
-                node = node->left;
-            } else {
-                if (s.empty()) break;
-                node = s.top();
-                s.pop();
-                inorder.emplace_back(node->val);
-                node = node->right;
+        vector<int> inorder;
+        if(!root) return inorder;
+        TreeNode *curr = root;
+        while(curr) {
+            if(!curr->left) {  // if there is nothing on left it's the leftmost
+                inorder.emplace_back(curr->val);  // print the current root
+                curr = curr->right;  // move right from here
+            } else {  // we have something on left
+                TreeNode *prev = curr->left;  // use prev as marker for the left subtree analysis
+                while(prev->right && prev->right != curr) prev = prev->right; // till we reach the rightmost node of that left subtree
+                if(!prev->right) {  // we reached the rightmost node of the left subtree
+                    prev->right = curr;  // create the thread that joins the rightmost element to the current root
+                    curr = curr->left;  // now move towards left from here
+                } else {  // if the thread already exists
+                    prev->right = NULL;  // cut the thread that exists
+                    inorder.emplace_back(curr->val);  // print the current root
+                    curr = curr->right;  // now move right from here
+                }
             }
         }
         return inorder;
