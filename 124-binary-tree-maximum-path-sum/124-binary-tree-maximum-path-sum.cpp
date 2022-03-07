a054075f872b4982160188686fe9ba130cf348b3
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-03-07 13:06:18 +0530

    Time: 33 ms (61.07%), Space: 27.6 MB (92.38%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -15,8 +15,8 @@ private:
         if(!node) return 0;
         int leftSum = max(0, calculate(node->left, maxPSum)); // don't take negative path sum, instead take 0 in case of negative path sum
         int rightSum = max(0, calculate(node->right, maxPSum));
-        maxPSum = max(maxPSum, leftSum + rightSum + node->val); // store the max path sum in this
-        return node->val + max(leftSum, rightSum); // to decide which path to take among the left and right as the longest path
+        maxPSum = max(maxPSum, leftSum + rightSum + node->val); // store the max path sum among all the nodes
+        return node->val + max(leftSum, rightSum); // to decide which path to take among the left and right as the longest path (maximum path sum from the current node)
     }
 public:
     int maxPathSum(TreeNode* root) {
