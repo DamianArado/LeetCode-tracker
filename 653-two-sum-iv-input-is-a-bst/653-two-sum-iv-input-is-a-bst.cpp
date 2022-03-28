31df20702503d81433375db883eb59378146a7ff
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-03-28 18:40:53 +0530

    Time: 39 ms (86.68%), Space: 36.7 MB (81.00%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -1,65 +1,66 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-/*
+    /**
+     * Definition for a binary tree node.
+     * struct TreeNode {
+     *     int val;
+     *     TreeNode *left;
+     *     TreeNode *right;
+     *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+     * };
+     */
+    /*
 
-Approach 1 - O(N) TC & O(N) SC
-We'll do an inorder and save the result inside a vector, which will be sorted and then to check for the 2 sum
-We could have easily used the 2 pointers strategy.
+    Approach 1 - O(N) TC & O(N) SC
+    We'll do an inorder and save the result inside a vector, which will be sorted and then to check for the 2 sum
+    We could have easily used the 2 pointers strategy.
 
-Approach 2 - O(N) TC & 2*O(H) SC
-We'll use the BST iterator class to iterate over the inorder of the BST
+    Approach 2 - O(N) TC & 2*O(H) SC
+    We'll use the BST iterator class to iterate over the inorder of the BST and then we will take 2 pointers
+    i and j for inorder and reverse inorder and then we'll check if their sum is equal to k.
 
-*/
+    */
 
-class BSTIterator {
-  private:
-    stack<TreeNode*> s;
-    bool reverse = true;
-    
-    void pushAll(TreeNode* node) {
-        for(; node != NULL;) {
-            s.push(node);
-            if(reverse) node = node->right;
-            else node = node->left;
+    class BSTIterator {
+      private:
+        stack<TreeNode*> s;
+        bool reverse = true;
+
+        void pushAll(TreeNode* node) {
+            for(; node != NULL;) {
+                s.push(node);
+                if(reverse) node = node->right;
+                else node = node->left;
+            }
         }
-    }
-    
-  public:
-    BSTIterator(TreeNode* root, bool isReverse) {
-        reverse = isReverse;
-        pushAll(root);
-    }
-    
-    int next() {
-        TreeNode* temp = s.top();
-        s.pop();
-        if(reverse) pushAll(temp->left);
-        else pushAll(temp->right);
-        return temp->val;
-    }
-};
 
-class Solution {
-public:
-    bool findTarget(TreeNode* root, int k) {
-        if(!root) return false;
-        BSTIterator l(root, false);
-        BSTIterator r(root, true);
-        int i = l.next(), j = r.next(); // inorder, reverse inorder
-        while(i < j) { // simulating 2 pointers in bst
-            if(i + j == k) return true;
-            else if(i + j < k) i = l.next();
-            else j = r.next();
+      public:
+        BSTIterator(TreeNode* root, bool isReverse) {
+            reverse = isReverse;
+            pushAll(root);
         }
-        return false;
-    }
-};
+
+        int next() {
+            TreeNode* temp = s.top();
+            s.pop();
+            if(reverse) pushAll(temp->left);
+            else pushAll(temp->right);
+            return temp->val;
+        }
+    };
+
+    class Solution {
+    public:
+        bool findTarget(TreeNode* root, int k) {
+            if(!root) return false;
+            BSTIterator l(root, false);
+            BSTIterator r(root, true);
+            int i = l.next(), j = r.next(); // inorder, reverse inorder
+            while(i < j) { // simulating 2 pointers in bst
+                if(i + j == k) return true;
+                else if(i + j < k) i = l.next();
+                else j = r.next();
+            }
+            return false;
+        }
+    };
