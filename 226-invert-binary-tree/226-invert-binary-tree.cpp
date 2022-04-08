c811b899b5fcd7b9ddbdc4e4ba3b1ab08a1d266c
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-04-08 14:53:09 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (38.17%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/*
 // Recursive approach - O(N) TC & O(N) SC
 class Solution {
 public:
@@ -17,6 +18,27 @@ public:
         swap(root->left, root->right);
         invertTree(root->left);
         invertTree(root->right);
+        return root;
+    }
+};
+*/
+
+// Iterative approach - O(N) TC & O(N) SC
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return NULL;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            for(int i = 0; i < q.size(); i++) {
+                TreeNode* temp = q.front(); 
+                q.pop();
+                swap(temp->left, temp->right);
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+            }
+        }
         return root;
     }
 };
