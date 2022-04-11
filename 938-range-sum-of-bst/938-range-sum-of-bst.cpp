851143c1739ae212093d508fa22f11398822cb99
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-04-11 15:55:38 +0530

    Time: 199 ms (28.07%), Space: 67.2 MB (5.08%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.cpp
+++ 938-range-sum-of-bst/938-range-sum-of-bst.cpp
@@ -9,13 +9,32 @@ public:
     }
 };*/
 
-class Solution {
+// BST-optimized DFS
+/*class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
         if(!root) return 0;
         int ans = (root->val >= low && root->val <= high) ? root->val : 0;
         if(root->val >= low) ans += rangeSumBST(root->left, low, high);
         if(root->val <= high) ans += rangeSumBST(root->right, low, high);
+        return ans;
+    }
+};*/
+
+// BST-optimized BFS
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(!root) return 0;
+        queue<TreeNode*> q;
+        int ans = 0;
+        q.push(root);
+        while(!q.empty()) {
+            TreeNode* temp = q.front(); q.pop();
+            if(temp->val <= high && temp->val >= low) ans += temp->val;
+            if(temp->left) q.push(temp->left);
+            if(temp->right) q.push(temp->right);
+        }    
         return ans;
     }
 };
