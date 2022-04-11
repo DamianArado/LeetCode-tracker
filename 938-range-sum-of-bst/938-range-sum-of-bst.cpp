d878b8b19e419bdebe135384dd72ae414838aaba
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-04-11 15:50:47 +0530

    Time: 124 ms (89.32%), Space: 64.6 MB (61.34%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.cpp
+++ 938-range-sum-of-bst/938-range-sum-of-bst.cpp
@@ -1,10 +1,21 @@
 // Simple DFS traversal - O(n) TC & SC
-class Solution {
+/*class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
         if(!root) return 0;
         return (root->val >= low && root->val <= high ? root->val : 0) // check if root lies within the range
             + rangeSumBST(root->left, low, high) // recurse left
             + rangeSumBST(root->right, low, high); // recurse right
+    }
+};*/
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(!root) return 0;
+        int ans = (root->val >= low && root->val <= high) ? root->val : 0;
+        if(root->val >= low) ans += rangeSumBST(root->left, low, high);
+        if(root->val <= high) ans += rangeSumBST(root->right, low, high);
+        return ans;
     }
 };
