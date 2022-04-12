4336491697a52a3220e2619c18341cc5a31b6030
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-04-13 02:19:46 +0530

    Time: 12 ms (83.45%), Space: 25.5 MB (59.30%) - LeetHub

--- 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -1,38 +1,23 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>&pre,vector<int>&post,int &preindex,int start,int end)
-    {
-        if(start>end) return NULL;
+    TreeNode* buildTree(vector<int> &pre, vector<int> &post, int &preindex, int start, int end) {
+        if(start > end) return NULL;
         TreeNode* new_head = new TreeNode(pre[preindex++]);
-        if(preindex==pre.size())
-            return new_head;
-        int i=start;
-        for(;i<=end;i++)
-        {
-            if(pre[preindex]==post[i])
+        if(preindex == pre.size()) return new_head;
+        int i = start;
+        for(; i <= end; i++) {
+            if(pre[preindex] == post[i])
                 break;
         }
-        if(i<=end)
-        {
-            new_head->left = buildTree(pre,post,preindex,start,i);
-            new_head->right = buildTree(pre,post,preindex,i+1,end-1);
+        if(i <= end) {
+            new_head->left = buildTree(pre, post, preindex, start, i);
+            new_head->right = buildTree(pre, post, preindex, i + 1, end - 1);
         }
         return new_head;
     }
     TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
         if(pre.size() == 1) return new TreeNode(pre[0]);
         int preIdx = 0;
-        return buildTree(pre,post,preIdx,0,pre.size()-1);
+        return buildTree(pre, post, preIdx, 0, pre.size() - 1);
     }
 };
