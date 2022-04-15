29728fd0da9664aa0030625b5f566066ac29fd91
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-04-15 20:28:57 +0530

    Time: 22 ms (40.56%), Space: 25.7 MB (49.01%) - LeetHub

--- 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -1,6 +1,8 @@
-class Solution {
-public:
-    TreeNode* buildTree(vector<int> &pre, vector<int> &post, int &preindex, int start, int end) {
+/*
+
+Approach 1 - O(n^2) TC & O(n) SC
+
+TreeNode* buildTree(vector<int> &pre, vector<int> &post, int &preindex, int start, int end) {
         if(start > end) return NULL;
         TreeNode* new_head = new TreeNode(pre[preindex++]);
         if(preindex == pre.size()) return new_head;
@@ -19,5 +21,21 @@ public:
         if(pre.size() == 1) return new TreeNode(pre[0]);
         int preIdx = 0;
         return buildTree(pre, post, preIdx, 0, pre.size() - 1);
+    }
+
+*/
+
+class Solution {
+public:
+    // TC & SC - O(n)
+    int preIdx = 0, postIdx = 0;
+    TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
+        TreeNode* root = new TreeNode(pre[preIdx++]);
+        if (root->val != post[postIdx])
+            root->left = constructFromPrePost(pre, post);
+        if (root->val != post[postIdx])
+            root->right = constructFromPrePost(pre, post);
+        postIdx++;
+        return root;
     }
 };
