f09126915b97fe80aef5bc278b58aebd3c7a5d24
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-04-19 12:01:06 +0530

    Time: 27 ms (73.44%), Space: 17.1 MB (34.06%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.cpp
@@ -1,33 +1,14 @@
 class NumArray {
-    vector<int> segTree;
-    inline static int n;
-    
-    void buildTree(vector<int> &nums, int nodeIdx, int l, int r) {
-        if(l == r) segTree[nodeIdx] = nums[l];
-        else {
-            int mid = l + (r - l) / 2;
-            buildTree(nums, 2 * nodeIdx, l, mid);
-            buildTree(nums, 2 * nodeIdx + 1, mid + 1, r);
-            segTree[nodeIdx] = segTree[2 * nodeIdx] + segTree[2 * nodeIdx + 1];
-        }
-    }
 public:
-    NumArray(vector<int>& nums) {
-        n = nums.size();
-        segTree.resize(4 * n, 0);
-        buildTree(nums, 1, 0, n - 1);
+    NumArray(vector<int> &nums) {
+        accu.push_back(0);
+        for (int num : nums)
+            accu.push_back(accu.back() + num);
     }
-    
-    int sumRange(int left, int right, int nodeIdx = 1, int l = 0, int r = n - 1) {
-        if(left > right) return 0;
-        if(left == l && right == r) return segTree[nodeIdx];
-        int mid = l + (r - l) / 2;
-        return sumRange(left, min(right, mid), 2 * nodeIdx, l, mid) + sumRange(max(left, mid + 1), right, 2 * nodeIdx + 1, mid + 1, r);
-    }
-};
 
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray* obj = new NumArray(nums);
- * int param_1 = obj->sumRange(left,right);
- */
+    int sumRange(int i, int j) {
+        return accu[j + 1] - accu[i];
+    }
+private:
+    vector<int> accu;
+};
