e89c75a59a1f9b1dfc84ea15e083171c931b9d2c
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-04-19 23:16:17 +0530

    Time: 726 ms (69.54%), Space: 194.9 MB (37.78%) - LeetHub

--- 327-count-of-range-sum/327-count-of-range-sum.cpp
+++ 327-count-of-range-sum/327-count-of-range-sum.cpp
@@ -1,103 +1,25 @@
 class Solution {
-
-struct SegmentTree {
-    SegmentTree* left;
-    SegmentTree* right;
-    int count;
-    long min;
-    long max;
-    
-    // nums is sorted
-    SegmentTree(vector<long>& nums, int l, int r):left(NULL),right(NULL) {
-        if (l>r) {
-            return;
-        }
-        min = nums[l];
-        max = nums[r];
-        count=0;
-        
-        if (l==r) {
-            return;
-        }
-        
-        int m = (l+r)/2;
-        if (l<=m) {
-            left = new SegmentTree(nums, l,m);
-        } else {
-            left=NULL;
-        }
-        if (m+1<=r) {
-            right = new SegmentTree(nums, m+1, r);
-        } else {
-            right = NULL;
-        }
-    }
-    
-    void add(long value) {
-        if (value<min || value>max) {
-            return;
-        }
-        if (value==0) {
-            // cout<<"add value"<<endl;
-        }
-        count++;
-        if (left && value<=left->max) {
-            left->add(value);
-        } else if (right && value>=right->min) {
-            right->add(value);
-        }
-    }
-    
-    int getCount(long minValue, long maxValue) {
-        if (minValue<=min && max<=maxValue) {
-            // cout<<minValue<<","<<maxValue<<endl;
-            return count;
-        }
-        
-        if (maxValue<min || minValue>max) {
-            return 0;
-        }
-        
-        int leftCount = left ? left->getCount(minValue, maxValue) : 0;
-        int rightCount = right ? right->getCount(minValue, maxValue) : 0;
-        
-        return leftCount+rightCount;
-    }
-    
-};
-    
-    
 public:
-    int countRangeSum(vector<int>& nums, int lower, int upper) {
-        if (nums.size()==0) {
-            return 0;
-        }
+    int mergeSort(vector<long>& sum, int lower, int upper, int low, int high) {
+        if(high - low <= 1) return 0;
+        int mid = (low + high) / 2, m = mid, n = mid, count =0;
         
-        unordered_set<long> values;
-        long sum = 0;
-        for(int i=0;i<nums.size();++i) {
-            sum+=nums[i];
-            values.insert(sum);
-        }
-
-        vector<long> sums;
-        for(auto& v : values) {
-            sums.push_back(v);
-        }
-
-        sort(sums.begin(), sums.end());
+        count = mergeSort(sum, lower, upper, low, mid) + mergeSort(sum, lower, upper, mid, high);
         
-        SegmentTree* tree = new SegmentTree(sums, 0, sums.size()-1);
-
-        int count = 0;
-        for(int i=nums.size()-1;i>=0;i--) {
-            tree->add(sum);
-            sum-=nums[i];
-            // lower<= sum[i]-sum[j]<=upper
-            count += tree->getCount(lower+sum, upper+sum);
+        for(int i = low; i < mid; ++i) {
+            while(m < high && sum[m] - sum[i] < lower) ++m;
+            while(n < high && sum[n] - sum[i] <= upper) ++n;
+            count += n - m;
         }
-        
+        inplace_merge(sum.begin() + low, sum.begin() + mid, sum.begin() + high);
         return count;
-        
+    }
+
+    int countRangeSum(vector<int>& nums, int lower, int upper) {
+        int n = nums.size();
+        vector<long> sum(n + 1, 0);
+        for(int i = 0; i < n; ++i) 
+            sum[i + 1] = sum[i] + nums[i];
+        return mergeSort(sum, lower, upper, 0, n + 1);
     }
 };
