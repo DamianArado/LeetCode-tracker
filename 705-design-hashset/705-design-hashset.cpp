0e574580df1d5774bafe7424ec67d4c6af4fad03
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-04-24 00:49:55 +0530

    Time: 113 ms (66.00%), Space: 44.6 MB (37.36%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,3 +1,7 @@
+/* 
+
+Smart way but may not impress interviewer -
+
 class MyHashSet {
     vector<bool> ans;
 public:
@@ -18,10 +22,87 @@ public:
     }
 };
 
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet* obj = new MyHashSet();
- * obj->add(key);
- * obj->remove(key);
- * bool param_3 = obj->contains(key);
- */
+*/
+
+// TODO : write analysis
+class MyHashSet {
+    struct TreeNode {
+        int digit;
+        bool exists;
+        vector<TreeNode*> children;
+        TreeNode(int digit) {
+            this->exists = false;
+            this->digit = digit;
+        }
+    };
+    
+    TreeNode* root;
+    
+public:
+    MyHashSet() {
+        root = new TreeNode(0);
+    }
+    
+    void add(int key) {
+        if(key == 0) {
+            root->exists = true;
+            return;
+        }
+        TreeNode* current = root;
+        while(key) {
+            bool found = false;
+            for(auto it : current->children) {
+                if(it->digit == key % 10) {
+                    found = true;
+                    current = it;
+                    break;
+                }
+            }
+            if(!found) {
+                current->children.emplace_back(new TreeNode(key % 10));
+                current = current->children.back();
+            }
+            key /= 10;
+        }
+        current->exists = true;
+    }
+    
+    void remove(int key) {
+        if(key == 0) {
+            root->exists = false;
+            return;
+        }
+        TreeNode* current = root;
+        while(key) {
+            bool found = false;
+            for(auto it : current->children) {
+                if(it->digit == key % 10) {
+                    found = true;
+                    current = it;
+                    break;
+                }
+            }
+            if(!found) return;
+            key /= 10;
+        }
+        current->exists = false;
+    }
+    
+    bool contains(int key) {
+        if(key == 0) return root->exists;
+        TreeNode* current = root;
+        while(key) {
+            bool found = false;
+            for(auto it : current->children) {
+                if(it->digit == key % 10) {
+                    found = true;
+                    current = it;
+                    break;
+                }
+            }
+            if(!found) return false;
+            key /= 10;
+        }
+        return current->exists;
+    }
+};
