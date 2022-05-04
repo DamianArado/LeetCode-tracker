df3ef04bf4fedbf059113e8fe1a63dac4b1b922d
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-05-04 23:02:41 +0530

    Time: 412 ms (46.11%), Space: 65.3 MB (85.82%) - LeetHub

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,73 @@
+struct Node {
+    Node* linkNext[2];
+    
+    bool containsKey(int bit) {
+        return (linkNext[bit] != NULL);
+    }
+    
+    void put(int bit, Node* newNode) {
+        linkNext[bit] = newNode;
+    }
+    
+    Node* getNext(int bit) {
+        return linkNext[bit];
+    }
+
+};
+
+class Trie {
+private: Node* root;
+public:
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(int number) {
+       Node* current = root;
+        for(int i = 31; i >= 0; i--) {
+            int bit = (number >> i) & 1;
+            if(!current->containsKey(bit)) {
+                current->put(bit, new Node());
+            }
+            current = current->getNext(bit);
+        }
+    }
+    
+    int findMax(int number) {
+         Node* current = root;
+        int maxXOR = 0;
+        // inserting from left to right
+        for(int i = 31; i >= 0; i--) {
+            // check if the bit is 1 or 0
+            int bit = (number >> i) & 1;
+            // check if we have the opposite bit at the current node in trie
+            if(current ->containsKey(1 - bit)) {
+                // making sure we get 1 at this bit
+                maxXOR = maxXOR | (1 << i);
+                // move to the reference node of this choice
+                current = current->getNext(1 - bit);
+            }else {
+                // if cant we take the 0 and keep moving
+                current = current->getNext(bit);
+            }
+        }
+        return maxXOR;
+    }
+};
+
+class Solution {
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        Trie trieObj;
+        for(auto &it : nums) {
+            // first we insert all the numbers bit-by-bit into the trie
+            trieObj.insert(it);
+        }
+        int ans = 0;
+        for(auto &it : nums){
+            // returns the max from this array
+            ans = max(ans, trieObj.findMax(it));
+        }
+        return ans;
+    }
+};
