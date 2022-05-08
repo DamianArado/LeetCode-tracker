b8759140ab971c94fdc3be47d4de43f37997b45f
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-05-08 18:26:12 +0530

    Time: 92 ms (69.25%), Space: 29 MB (73.57%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -10,16 +10,18 @@ public:
     
     */
     int longestConsecutive(vector<int>& nums) {
-	unordered_set<int> s(begin(nums), end(nums));
-	int longest = 0;
-	for(auto& num : s) {
-        if(s.count(num - 1)) continue;
-		int j = 1;
-		while(s.count(num + j)) j++;
-		longest = max(longest, j);
-	}
-	return longest;
-}
+        if(nums.size() == 0) return 0;
+        if(nums.size() == 1) return 1;
+        unordered_set<int> s(begin(nums), end(nums));
+        int longest = 0;
+        for(auto& num : s) {
+            if(s.count(num - 1)) continue;
+            int j = 1;
+            while(s.count(num + j)) j++;
+            longest = max(longest, j);
+        }
+        return longest;
+    }
     // TC & SC - O(n)   
     
     /*
