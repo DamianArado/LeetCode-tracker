9854ee917228757ff6025d6aa518b8e5a72db803
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-05-11 22:34:30 +0530

    Time: 41 ms (35.03%), Space: 98.9 MB (85.44%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
@@ -17,19 +17,26 @@ class Solution {
     private boolean dfs(int curX, int curY, int lastX, int lastY, int n, int m, boolean[][] vis, char[][] grid, char startChar) {
         vis[curX][curY] = true;
         boolean hasCycle = false;
+        
         // Visit all directions
         for (int i = 0; i < 4; ++i) {
             int newX = curX + DIR_X[i];
             int newY = curY + DIR_Y[i];
+            
             // Valid point?
             if (newX >= 0 && newX < n && newY >= 0 && newY < m) {
+                
                 // Don't visit last visited point
                 if (!(newX == lastX && newY == lastY)) {
+                    
                     // Only visit nodes that equal start character
                     if (grid[newX][newY] == startChar) {
+
                         if (vis[newX][newY]) {
+                            
 						    // Still visited? There is a cycle.
                             return true;
+                            
                         } else {
                             hasCycle |= dfs(newX, newY, curX, curY, n, m, vis, grid, startChar);
                         }
@@ -45,6 +52,7 @@ class Solution {
         int m = grid[0].length;
         boolean[][] vis = new boolean[n][m];
         boolean hasCycle = false;
+        
         for (int i = 0; i < grid.length; ++i) {
             for (int j = 0; j < grid[i].length; ++j) {
                 if (!vis[i][j]) {
@@ -52,6 +60,7 @@ class Solution {
                 }
             }
         }
+        
         return hasCycle;
     }
 }
