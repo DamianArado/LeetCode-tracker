dbca956bd6d6527d7bf9765eb273dca776f84c44
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-05-16 01:00:03 +0530

    Time: 31 ms (45.95%), Space: 14.6 MB (33.46%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,40 +1,52 @@
 class Solution {
 private:
     bool checkCycle(int start, vector<int> adjList[], vector<bool> &visited, vector<bool> &dfsCurrentVisit, vector<int> &ans) {
+        // we visit the start vertex
         visited[start] = dfsCurrentVisit[start] = true;
         
+        // for all nodes in the adjacency list of start vertex
         for(auto it : adjList[start]) {
+            // if its not visited and we found a cycle
             if(!visited[it] && checkCycle(it, adjList, visited, dfsCurrentVisit, ans))
                 return true;
+            // if its visited in the current dfs call
             else if(dfsCurrentVisit[it])
                 return true;
         }
+        // adding start vertex to answer
         ans.push_back(start);
+        // we will return from the current dfs call hence marking it as unvisited
         dfsCurrentVisit[start] = false;
         return false;
     }
+    
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites, vector<int> &ans) {
+        // to label all vertices as visited or not
         vector<bool> visited(numCourses, false);
+        // to label all vertices in the current dfs call as visited or not
         vector<bool> dfsCurrentVisit(numCourses, false);
-        vector<int> adjList[numCourses];  // array of vectors
+        // array of vectors for adjacency list
+        vector<int> adjList[numCourses];
         
+        // constructing our adjacency list
         for(auto it : prerequisites) {
             adjList[it[0]].push_back(it[1]);
         }
+        // checking all components for cycle
         for(int i = 0; i < numCourses; ++i) {
+            // if we found a cycle we can't finish all courses
             if(!visited[i] && checkCycle(i, adjList, visited, dfsCurrentVisit, ans))
                 return false;
         }
+        // no cycle so we have an answer
         return true;
     }
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> ans;
         if(numCourses == 1) return {0};
-        
-        if(canFinish(numCourses, prerequisites, ans)) {
+        if(canFinish(numCourses, prerequisites, ans))
             return ans;
-        }    
         return {};
     }
 };
