c18a2a854deab0f81eae949909cd8c01ec26ac07
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-05-15 23:52:19 +0530

    Time: 34 ms (36.90%), Space: 14.5 MB (36.11%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    bool checkCycle(int start, vector<int> adjList[], vector<bool> &visited, vector<bool> &dfsCurrentVisit) {
+        visited[start] = dfsCurrentVisit[start] = true;
+        
+        for(auto it : adjList[start]) {
+            if(!visited[it] && checkCycle(it, adjList, visited, dfsCurrentVisit))
+                return true;
+            else if(dfsCurrentVisit[it])
+                return true;
+        }
+        
+        dfsCurrentVisit[start] = false;
+        return false;
+    }
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<bool> visited(numCourses, false);
+        vector<bool> dfsCurrentVisit(numCourses, false);
+        vector<int> adjList[numCourses];  // array of vectors
+        
+        for(auto it : prerequisites) {
+            adjList[it[1]].push_back(it[0]);
+        }
+        for(int i = 0; i < numCourses; ++i) {
+            if(!visited[i] && checkCycle(i, adjList, visited, dfsCurrentVisit))
+                return false;
+        }
+        return true;
+    }
+};
