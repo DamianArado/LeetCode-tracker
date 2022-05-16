5587249b17f928eca490c129d612711a3134e8dc
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-05-16 14:55:07 +0530

    Time: 485 ms (76.07%), Space: 131.6 MB (88.67%) - LeetHub

---
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.cpp
@@ -0,0 +1,40 @@
+/*
+
+Idea
+
+This problem is an advanced version of 1494. Parallel Courses II, which has prerequisite relationship between courses.
+To solve prerequisite relationship, we can obviously use Topology Sort idea to complete prev courses before next courses.
+Let dist[u] is the number of months required to finish u course, dist[u] is calculated as maximum dist of the predecessor nodes + times[u].
+
+*/
+
+class Solution {
+public:
+    int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
+        vector<int> inDegree(n);
+        vector<vector<int>> graph(n, vector<int>());
+        for (auto& edge : relations) {
+            int prev = edge[0] - 1, next = edge[1] - 1;
+            graph[prev].push_back(next);
+            inDegree[next]++;
+        }
+        
+        vector<int> dist(n);
+        queue<int> q;
+        for (int u = 0; u < n; ++u) {
+            if (inDegree[u] == 0) {
+                q.push(u);
+                dist[u] = time[u];
+            }
+        }
+        while (!q.empty()) {
+            int u = q.front(); q.pop();
+            for (int v : graph[u]) {
+                dist[v] = max(dist[v], dist[u] + time[v]); // Update `dist[v]` using the maximum dist of the predecessor nodes
+                if (--inDegree[v] == 0) 
+                    q.push(v);
+            }
+        }
+        return *max_element(dist.begin(), dist.end());
+    }
+};
