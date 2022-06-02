4e0fc04ff4058bfce40ccb783e79ae48a8c090e3
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-06-03 00:43:14 +0530

    Time: 3 ms (98.66%), Space: 8.8 MB (69.20%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -2,9 +2,16 @@
 
 (Union By Rank and Find with path Compression)
 
-This optimization involves doing the union based on the rank (or size) of parents / representative of a component instead of just attaching one to the other randomly. This will ensure that even in the worst-case, we don't end up in a scenario where the par forms a skewed tree (all nodes on one-side) and we wouldn't need to iterate all nodes on each find call. Performing the union based on rank will keep the components/tree balanced in size.
+This optimization involves doing the union based on the rank (or size) of parents / representative of a 
+component instead of just attaching one to the other randomly. This will ensure that even in the worst-case, 
+we don't end up in a scenario where the par forms a skewed tree (all nodes on one-side) and we wouldn't need 
+to iterate all nodes on each find call. Performing the union based on rank will keep the components/tree 
+balanced in size.
 
-Time Complexity : O(n*α(n)) ≈ O(n), the time complexity of each find call after union-by-rank and path compression comes out to be O(α(n)), where α(n) is the inverse Ackermann function. It doesn't exceed 4 for any n < 10600 and hence is practically constant. We make O(n) calls in total.
+Time Complexity : O(n*α(n)) ≈ O(n), the time complexity of each find call after union-by-rank and path 
+compression comes out to be O(α(n)), where α(n) is the inverse Ackermann function. It doesn't exceed 4 for 
+any n < 10600 and hence is practically constant. We make O(n) calls in total.
+
 Space Complexity : O(n)
 
 **/
