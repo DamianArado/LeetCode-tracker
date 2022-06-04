8b6117f15d92be6c2aa05f336eec7e910ce49d39
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-06-04 21:58:39 +0530

    Time: 6 ms (10.67%), Space: 6.7 MB (35.24%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> res;
+        if(digits.size() == 0) return res;
+        string charmap[10] = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        res.push_back("");
+        for (int i = 0; i < digits.size(); i++)
+        {
+            vector<string> tempres;
+            string chars = charmap[digits[i] - '0'];
+            for (int c = 0; c < chars.size();c++)
+                for (int j = 0; j < res.size();j++)
+                    tempres.push_back(res[j]+chars[c]);
+            res = tempres;
+        }
+        return res;
+    }
+};
