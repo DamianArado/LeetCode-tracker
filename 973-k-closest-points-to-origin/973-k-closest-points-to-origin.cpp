029940c6726868591b0bae93918a3838b34c0d14
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-06-09 16:55:59 +0530

    Time: 536 ms (20.60%), Space: 77.6 MB (23.96%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        // firstly create a max heap of type [distanceFromOrigin, xi, yi]
+        priority_queue<vector<int>> maxHeap;
+        for(auto point : points) {
+            int x = point[0], y = point[1];
+            maxHeap.push({x*x + y*y, x, y});
+            if(maxHeap.size() > k)
+                maxHeap.pop();
+        }
+        // now we have only the k points inside the maxHeap that are closest to the origin
+        vector<vector<int>> ans(k);
+        for(int i = 0; i < k; ++i) {
+            vector<int> topElem = maxHeap.top();
+            maxHeap.pop();
+            ans[i] = {topElem[1], topElem[2]};
+        }
+        return ans;
+    }
+};
