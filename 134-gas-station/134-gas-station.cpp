60f6109a608d476d19c396da6ffc93c9a7db8f94
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-06-13 17:26:01 +0530

    Time: 173 ms (7.52%), Space: 69.2 MB (99.18%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int startIdx = 0, gasLeft = 0, totalGasLeft = 0, c = 0, s = 0;
-        for(int i = 0; i < gas.size(); ++i) {
-            s += gas[i], c += cost[i];
-        }
-        if(c > s) return -1;
+        int startIdx = 0, gasLeft = 0, costSum = 0, gasSum = 0;
+        for(int i = 0; i < gas.size(); ++i)
+            gasSum += gas[i], costSum += cost[i];
+        if(costSum > gasSum) return -1;
+        // we can complete a circular route, we just need to find the starting index
         for(int i = 0; i < gas.size(); ++i) {
             gasLeft += gas[i] - cost[i];
-            totalGasLeft += gas[i] - cost[i];
             if(gasLeft < 0) {
                 // if we ran out of gas at i-th station, we cannot complete a circular route
                 // we should again start at the next i-th position
@@ -16,8 +15,6 @@ public:
                 startIdx = i + 1;
             }
         }
-        // if -ve that means total cost was greater than total gas
-        // implying we couldn't complete a circular route
         return startIdx;
     }
 };
