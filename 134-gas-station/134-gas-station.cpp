4614bbbd504f204bc63ccdfd0792388308eca267
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-06-13 17:26:48 +0530

    Time: 166 ms (9.68%), Space: 69.4 MB (52.39%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -4,6 +4,7 @@ public:
         int startIdx = 0, gasLeft = 0, costSum = 0, gasSum = 0;
         for(int i = 0; i < gas.size(); ++i)
             gasSum += gas[i], costSum += cost[i];
+        // we cannnot complete a circular route
         if(costSum > gasSum) return -1;
         // we can complete a circular route, we just need to find the starting index
         for(int i = 0; i < gas.size(); ++i) {
