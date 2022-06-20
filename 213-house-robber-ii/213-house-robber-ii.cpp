13e2f6e77cd1338477004c6d2a06d3250a0306c3
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-06-20 17:59:02 +0530

    Time: 3 ms (56.84%), Space: 7.8 MB (60.69%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,28 +1,23 @@
 class Solution {
+private:
+    int maxRob(vector<int> &nums, int l, int r) {
+        int prev2 = 0, prev = nums[l];
+        for(int i = l + 1; i < r; ++i) {
+            int pick = nums[i] + prev2;
+            int dontPick = prev;
+            int currentMax = max(pick, dontPick);
+            prev2 = prev;
+            prev = currentMax;
+        }
+        return prev;
+    }
 public:
     int rob(vector<int>& nums) {
         // O(n) tc & O(1) sc 
         // just took into account that first and last can't exist together
         int n = nums.size();
         if(n == 1) return nums[0];
-        int prev2 = 0, prev = nums[0], sum1;
-        // ignore the last element
-        for(int i = 1; i < n - 1; ++i) {
-            int pick = nums[i] + prev2;
-            int dontPick = prev;
-            int currentMax = max(pick, dontPick);
-            prev2 = prev;
-            prev = currentMax;
-        }
-        // ignore the first element
-        sum1 = prev, prev2 = 0, prev = nums[1];
-        for(int i = 2; i < n; ++i) {
-            int pick = nums[i] + prev2;
-            int dontPick = prev;
-            int currentMax = max(pick, dontPick);
-            prev2 = prev;
-            prev = currentMax;
-        }
-        return max(sum1, prev);
+        // ignore the last element or the first element
+        return max(maxRob(nums, 0, n - 1), maxRob(nums, 1, n));
     }
 };
