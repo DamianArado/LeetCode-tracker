b4b85c66a46d172cbd873303c2b29a6cde3cb48b
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-06-23 02:01:36 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (90.21%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -91,7 +91,7 @@ public:
     // TC - O(m) using combinatorics
     int uniquePaths(int m, int n) {
         int N = m + n - 2;
-        int r = m - 1;
+        int r = min(m - 1, n - 1);
         double result = 1;
         for(int i = 1; i <= r; ++i)
             result = result * (N - r + i) / i;
