b6cb0b03dc7736100af63dd2d2cac6dc8c590776
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-06-23 20:07:11 +0530

    Time: 15 ms (48.88%), Space: 10 MB (50.56%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,6 +1,6 @@
 /**
 
-Solution 1: Using memoization - O(mn) tc and O(mn + (m+n)) sc
+Approach 1: Using memoization - O(mn) tc and O(mn + (m+n)) sc
 
     int f(int m, int n, vector<vector<int>> &grid, vector<vector<int>> &dp) {
         if(m == 0 and n == 0) return grid[0][0];
@@ -17,12 +17,9 @@ public:
         return f(m - 1, n - 1, grid, dp);
     }
 
+Approach 2: Using tabulation - O(mn) tc and sc
 
-**/
-
-class Solution {
-public:
-    int minPathSum(vector<vector<int>>& grid) {
+int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
         vector<vector<int>> dp(m, vector<int> (n, 0));
         for(int i = 0; i < m; ++i) {
@@ -42,5 +39,35 @@ public:
             }
         }
         return dp[m - 1][n - 1];
+    }
+    
+Approach 3: Using space optimization
+
+**/
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<int> dp(n, 0);
+        for(int i = 0; i < m; ++i) {
+            vector<int> temp(n, 0);
+            for(int j = 0; j < n; ++j) {
+                if(i == 0 and j == 0) temp[j] = grid[0][0];
+                else {
+                    int left = 0, up = 0;
+                    
+                    if(j > 0) left += grid[i][j] + temp[j - 1];
+                    else left += 1e7;
+                    
+                    if(i > 0) up += grid[i][j] + dp[j];
+                    else up += 1e7;
+                    
+                    temp[j] = min(left, up);
+                }
+            }
+            dp = temp;
+        }
+        return dp[n - 1];
     }
 };
