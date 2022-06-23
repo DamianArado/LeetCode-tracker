037373629a29e3e05ec22474b06524336e330ccf
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-06-23 20:00:45 +0530

    Time: 4 ms (98.18%), Space: 10.2 MB (5.99%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,5 +1,7 @@
-class Solution {
-private:
+/**
+
+Solution 1: Using memoization - O(mn) tc and O(mn + (m+n)) sc
+
     int f(int m, int n, vector<vector<int>> &grid, vector<vector<int>> &dp) {
         if(m == 0 and n == 0) return grid[0][0];
         if(m < 0 or n < 0) return 1e7;
@@ -13,5 +15,32 @@ public:
         int m = grid.size(), n = grid[0].size();
         vector<vector<int>> dp(m, vector<int> (n, -1));
         return f(m - 1, n - 1, grid, dp);
+    }
+
+
+**/
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int> (n, 0));
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(i == 0 and j == 0) dp[i][j] = grid[0][0];
+                else {
+                    int left = 0, up = 0;
+                    
+                    if(j > 0) left += grid[i][j] + dp[i][j - 1];
+                    else left += 1e7;
+                    
+                    if(i > 0) up += grid[i][j] + dp[i - 1][j];
+                    else up += 1e7;
+                    
+                    dp[i][j] = min(left, up);
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 };
