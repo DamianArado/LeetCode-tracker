6208f4ea359f09805ab9ee8fffe7a0ebb9ff30e1
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-06-25 03:34:11 +0530

    Time: 21 ms (53.05%), Space: 10.3 MB (48.19%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,4 +1,19 @@
-class Solution {
+/***
+
+Approach 1: Recurrence (n - 1, j) -> (0, j)
+
+f(row, col):
+    if(col < 0 or col >= n): return INT_MAX
+    if(row == 0): return a[0][col]
+    up = f(row - 1, col)
+    upLeft = f(row - 1, col - 1)
+    upRight = f(row - 1, col + 1)
+    return min(up, upLeft, upRight)
+    
+TC: O(3^n), SC: O(n)
+
+Approach 2: Memoization as we see overlapping subproblems
+
 private:
     int f(int row, int col, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
         if(col < 0 or col >= matrix.size()) return 1e7;
@@ -13,10 +28,48 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size(), minPath = INT_MAX;
         vector<vector<int>> dp(n, vector<int>(n, -1));
-        
         int minPathSum = INT_MAX; 
         for(int i = 0; i < n; ++i)
             minPathSum = min(minPathSum, f(n - 1, i, matrix, dp));
+        return minPathSum;
+    }
+    
+TC: O(n^2), SC: O(n^2 + n)
+
+Approach 3: Tabulation based approach (0, j) -> (n - 1, j)
+
+
+
+TC and SC: O(n^2)
+
+*/
+
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size(), minPath = INT_MAX;
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        // base case: setting values for the 1st row
+        for(int j = 0; j < n; ++j)
+            dp[0][j] = matrix[0][j];
+        // lets start from the 2nd row
+        for(int i = 1; i < n; ++i) {
+            // and from the 1st column
+            for(int j = 0; j < n; ++j) {
+                // calculating the values down, downLeft and downRight using the values in their above row
+                int down = matrix[i][j] + dp[i - 1][j];
+                int downLeft = 1e7, downRight = 1e7;
+                if(j > 0) 
+                    downLeft = matrix[i][j] + dp[i - 1][j - 1];
+                if(j < n - 1)
+                    downRight = matrix[i][j] + dp[i - 1][j + 1];
+                dp[i][j] = min(down, min(downLeft, downRight));
+            }
+        }
+        // what's the min among each of the entries collected in the last row
+        int minPathSum = INT_MAX;
+        for(int j = 0; j < n; ++j) 
+            minPathSum = min(minPathSum, dp[n - 1][j]);
         return minPathSum;
     }
 };
