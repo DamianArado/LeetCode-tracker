2eea07550240cf114181af041e04e412b9eec1ab
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-06-27 17:53:36 +0530

    Time: 125 ms (56.32%), Space: 41.5 MB (40.76%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -0,0 +1,45 @@
+/**
+
+Approach 1: Using prefix sum
+
+int subarraySum(vector<int>& nums, int k) {
+        int count = 0, n = nums.size() + 1;
+        vector<int> prefixSum(n, 0);
+        // build the prefix sum array
+        for(int i = 1; i < n; ++i)
+            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+        // iterate using 2 loops over the prefix sum array and find k 
+        for(int start = 0; start < n; ++start)
+            for(int end = start + 1; end < n; ++end)
+                if(prefixSum[end] - prefixSum[start] == k)
+                    count++;
+        return count;
+    }
+    
+TC: O(n^2), SC: O(n)  [TLE]
+
+Approach 2: Using prefix sum and unordered map
+
+TC and SC: O(n)
+
+*/
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int ans = 0, cumSum = 0;
+        unordered_map<int,int> sumFreq;
+        sumFreq[0] = 1;
+        for(int num : nums) {
+            cumSum += num;
+            int target = cumSum - k;
+            // check if we already have target inside the map or not
+            if(sumFreq.find(target) != sumFreq.end())
+                // add the frequency of its occurrence to the answer
+                ans += sumFreq[target];
+            // add frequency of this new cumulative sum inside the map
+            sumFreq[cumSum]++;
+        }
+        return ans;
+    }
+};
