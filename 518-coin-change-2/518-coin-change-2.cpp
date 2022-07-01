5cf6140eb9dbc3258cd9fc70f3edb2f77bf08620
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-01 22:43:07 +0530

    Time: 78 ms (20.21%), Space: 18.3 MB (45.36%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -11,23 +11,8 @@ f(index, amount):
         take = f(index, amount - coins[index])
     return take + notTake
     
-Approach 1: Memoization over recursion - O(n*T) TC & O(n*T + T) SC
+Approach 1: Memoization over recursion - O(n*amount) TC & O(n*amount + amount) SC
 
-
-
-class Solution {
-public:
-    int change(int amount, vector<int>& coins) {
-        vector<int> dp(amount+1, 0);
-        dp[0] = 1;
-        for (auto coin : coins)
-            for (int i=1; i<=amount; i++)
-                if (coin <= i) dp[i] += dp[i-coin];
-        return dp[amount];
-    }
-};
-
-*/
 class Solution {
 private:
     int f(int index, int amount, vector<int> &coins, vector<vector<int>> &dp) {
@@ -46,5 +31,42 @@ public:
         int n = coins.size();
         vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
         return f(n - 1, amount, coins, dp);
+    }
+};
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount+1, 0);
+        dp[0] = 1;
+        for (auto coin : coins)
+            for (int i=1; i<=amount; i++)
+                if (coin <= i) dp[i] += dp[i-coin];
+        return dp[amount];
+    }
+};
+
+Approach 2: Tabulation - O(n*amount) TC & SC
+
+*/
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+        // base case
+        for(int i = 0; i <= amount; ++i)
+            if(i % coins[0] == 0) 
+                dp[0][i] = 1;
+        for(int index = 1; index < n; ++index) {
+            for(int target = 0; target <= amount; ++target) {
+                int take = 0;
+                int notTake = dp[index - 1][target];
+                if(coins[index] <= target)
+                    take = dp[index][target - coins[index]];
+                dp[index][target] = take + notTake;
+            }
+        }
+        return dp[n - 1][amount];
     }
 };
