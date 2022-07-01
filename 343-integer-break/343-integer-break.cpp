cf241c7fab4c8f1f548342fcc9e72cdc9611d3cf
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-02 03:31:57 +0530

    Time: 5 ms (20.99%), Space: 5.8 MB (90.59%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -12,23 +12,14 @@ public:
     //  The idea is to utilize as many 3's as possible (~ n % 3)        ///
     //  if n % 3 == 0, ans = 3 * (n % 3)                                ///
     //  if n % 3 == 1, ans = 4 * (n % 3)                                ///
-    //  else ans = 4 * (n % 3)                                          ///
+    //  else ans = 2 * (n % 3)                                          ///
     ///////////////////////////////////////////////////////////////////////
     // Time: O(N), Space: O(1)                                          ///
     ///////////////////////////////////////////////////////////////////////
     long int integerBreak(int n) {
         // edge cases
-        switch(n) {
-            case 2:
-                return 1;
-                break;
-            case 3:
-                return 2;
-                break;
-        }
-        
+        if(n == 2 || n == 3) return n - 1;
         long int ans = 1;
-        
         if(n % 3 == 0) {
             for(int i = 0; i < n / 3; ++i)
                 ans *= 3;
