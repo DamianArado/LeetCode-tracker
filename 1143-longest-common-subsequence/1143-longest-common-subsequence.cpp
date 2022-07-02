47e1aee4a5fb42860e28466d4259956f94e0930a
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-02 23:34:26 +0530

    Time: 44 ms (32.09%), Space: 13 MB (40.40%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -13,7 +13,8 @@ f(index1, index2):
     
 TC: O(2^n1 * 2^n2), SC: (n1 + n2)
 
-*/
+Approach1: Memoization over recursion
+
 class Solution {
 private:
     int f(int index1, int index2, string &text1, string &text2, vector<vector<int>> &dp) {
@@ -34,5 +35,27 @@ public:
         int n1 = text1.size(), n2 = text2.size();
         vector<vector<int>> dp(n1, vector<int>(n2, -1));
         return f(n1 - 1, n2 - 1, text1, text2, dp);
+    }
+};
+
+TC: O(n1 * n2), SC: O(n1 * n2 + (n1 + n2))
+
+*/
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n1 = text1.size(), n2 = text2.size();
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+        for(int idx1 = 1; idx1 <= n1; ++idx1) {
+            for(int idx2 = 1; idx2 <= n2; ++idx2) {
+                // match
+                if(text1[idx1 - 1] == text2[idx2 - 1]) {
+                    dp[idx1][idx2] = 1 + dp[idx1 - 1][idx2 - 1];
+                    continue;
+                }
+                dp[idx1][idx2] = max(dp[idx1 - 1][idx2], dp[idx1][idx2 - 1]);
+            }
+        }
+        return dp[n1][n2];
     }
 };
