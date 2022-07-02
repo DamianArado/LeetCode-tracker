c3a919e0cc4216d6a0ec076fb1944d89a7267aca
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-02 23:54:29 +0530

    Time: 18 ms (80.27%), Space: 12.9 MB (56.25%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -40,7 +40,8 @@ public:
 
 TC: O(n1 * n2), SC: O(n1 * n2 + (n1 + n2))
 
-*/
+Approach 2: Using tabulation - O(n1 * n2) TC & SC
+
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
@@ -56,5 +57,30 @@ public:
             }
         }
         return dp[n1][n2];
+    }
+};
+
+Approach 3: Using space optimization over tabulation
+
+TC: O(n1 * n2), SC: O(max(n1, n2))
+
+*/
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n1 = text1.size(), n2 = text2.size();
+        vector<int> dp(n2 + 1, 0);
+        for(int idx1 = 1; idx1 <= n1; ++idx1) {
+            vector<int> current(n2 + 1, 0);
+            for(int idx2 = 1; idx2 <= n2; ++idx2) {
+                // match
+                if(text1[idx1 - 1] == text2[idx2 - 1]) 
+                    current[idx2] = 1 + dp[idx2 - 1];
+                else 
+                    current[idx2] = max(dp[idx2], current[idx2 - 1]);
+            }
+            dp = current;
+        }
+        return dp[n2];
     }
 };
