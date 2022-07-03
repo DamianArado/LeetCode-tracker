456f15998289ce59c40a06354f47cef205ee7e29
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-03 16:07:08 +0530

    Time: 442 ms (47.46%), Space: 108.5 MB (52.45%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,3 +1,7 @@
+/**
+
+Approach 1: Using tabulation - O(mn) TC & SC
+
 class Solution {
 public:
     // Using the logic of Longest common substring
@@ -22,6 +26,38 @@ public:
                     dp[i][j] = 0;
                 }
             }
+        }
+        return ans;
+    }
+};
+
+*/
+class Solution {
+public:
+    // Using the logic of Longest common substring
+    // TC & SC: O(mn)
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size(), ans = 0;
+        vector<int> dp (n + 1, 0);
+        for(int i = 1; i <= m; ++i) {
+            vector<int> current (n + 1, 0);
+            for(int j = 1; j <= n; ++j) {
+                if(nums1[i - 1] == nums2[j - 1]) {
+                    // match: check if we already have any match before this index on both nums1 and nums2
+                    if(dp[j - 1]) {
+                        current[j] = 1 + dp[j - 1];
+                        ans = max(ans, current[j]);
+                    } else {
+                        // no previous matches
+                        current[j] = 1;
+                        ans = max(ans, current[j]);
+                    }
+                } else {
+                    // not a match
+                    current[j] = 0;
+                }
+            }
+            dp = current;
         }
         return ans;
     }
