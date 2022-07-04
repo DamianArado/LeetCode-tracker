ac05084f7dd0804ca07533a54d5d197d05e686b6
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-04 18:44:56 +0530

    Time: 106 ms (9.92%), Space: 18.4 MB (62.74%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,14 +1,64 @@
+/***
+
+Recurrence:
+
+f(idx1, idx2): in s[0....idx1], how many distinct subsequences of t[0...idx2] can you find?
+    if idx2 == -1: return 1
+    if idx1 == -1: return 0
+    take = f(idx1 - 1, idx2 - 1)
+    notTake = f(idx1 - 1, idx2)
+    if s[idx1] == t[idx2]:
+        return take + notTake
+    return notTake
+
+Approach 1: Using memoization over recurrence - O(n1.n2) TC & O(n1.n2 + n1 + n2) SC 
+
 class Solution {
+private:
+    int find(int idx1, int idx2, string &s, string &t, vector<vector<int>> &dp) {
+        if(idx2 == -1) 
+            return 1;
+        if(idx1 == -1) 
+            return 0;
+        if(dp[idx1][idx2] != -1) 
+            return dp[idx1][idx2];
+        int take = find(idx1 - 1, idx2 - 1, s, t, dp);
+        int notTake = find(idx1 - 1, idx2, s, t, dp);
+        if(s[idx1] == t[idx2])
+            return dp[idx1][idx2] = take + notTake;
+        return dp[idx1][idx2] = notTake;
+    }
 public:
-    unsigned long long numDistinct(string s, string t) {
-         int n = s.length(), m = t.length();
-         vector<unsigned long long> dp(m + 1, 0);
-         dp[0] = 1;
-         for (int j = 1; j <= n; j++){
-             for (int i = m; i >= 1; i--){
-                 dp[i] += s[j - 1] == t[i - 1] ? dp[i - 1] : 0;
-             }
-         }
-        return dp[m];
+    int numDistinct(string s, string t) {
+        int n1 = s.size(), n2 = t.size();
+        vector<vector<int>> dp(n1, vector<int> (n2, -1));
+        return find(n1 - 1, n2 - 1, s, t, dp);
+    }
+};
+
+Approach 2: Using tabulation (bottom - up) - O(n1.n2) TC & SC
+
+*/
+class Solution {
+private:
+    int find(int idx1, int idx2, string &s, string &t, vector<vector<int>> &dp) {
+        if(idx2 == -1) 
+            return 1;
+        if(idx1 == -1) 
+            return 0;
+        if(dp[idx1][idx2] != -1) 
+            return dp[idx1][idx2];
+        int take = find(idx1 - 1, idx2 - 1, s, t, dp);
+        int notTake = find(idx1 - 1, idx2, s, t, dp);
+        if(s[idx1] == t[idx2])
+            return dp[idx1][idx2] = (take + notTake) % prime;
+        return dp[idx1][idx2] = notTake;
+    }
+public:
+    int prime = 1e9 + 7;
+    int numDistinct(string s, string t) {
+        int n1 = s.size(), n2 = t.size();
+        vector<vector<int>> dp(n1, vector<int> (n2, -1));
+        return find(n1 - 1, n2 - 1, s, t, dp);
     }
 };
