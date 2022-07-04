665e8ca1ff6ba46d5e8a15f77d16222956641c34
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-04 19:06:30 +0530

    Time: 69 ms (34.03%), Space: 18.5 MB (47.10%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -16,19 +16,18 @@ Approach 1: Using memoization over recurrence - O(n1.n2) TC & O(n1.n2 + n1 + n2)
 class Solution {
 private:
     int find(int idx1, int idx2, string &s, string &t, vector<vector<int>> &dp) {
-        if(idx2 == -1) 
-            return 1;
-        if(idx1 == -1) 
-            return 0;
-        if(dp[idx1][idx2] != -1) 
-            return dp[idx1][idx2];
+        if(idx2 == -1) return 1;
+        if(idx1 == -1) return 0;
+        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
         int take = find(idx1 - 1, idx2 - 1, s, t, dp);
         int notTake = find(idx1 - 1, idx2, s, t, dp);
         if(s[idx1] == t[idx2])
-            return dp[idx1][idx2] = take + notTake;
+            // need to take a mod with 1e9 + 7 to avoid integer overflow
+            return dp[idx1][idx2] = (take + notTake) % prime;
         return dp[idx1][idx2] = notTake;
     }
 public:
+    int prime = 1e9 + 7;
     int numDistinct(string s, string t) {
         int n1 = s.size(), n2 = t.size();
         vector<vector<int>> dp(n1, vector<int> (n2, -1));
@@ -40,25 +39,27 @@ Approach 2: Using tabulation (bottom - up) - O(n1.n2) TC & SC
 
 */
 class Solution {
-private:
-    int find(int idx1, int idx2, string &s, string &t, vector<vector<int>> &dp) {
-        if(idx2 == -1) 
-            return 1;
-        if(idx1 == -1) 
-            return 0;
-        if(dp[idx1][idx2] != -1) 
-            return dp[idx1][idx2];
-        int take = find(idx1 - 1, idx2 - 1, s, t, dp);
-        int notTake = find(idx1 - 1, idx2, s, t, dp);
-        if(s[idx1] == t[idx2])
-            return dp[idx1][idx2] = (take + notTake) % prime;
-        return dp[idx1][idx2] = notTake;
-    }
 public:
-    int prime = 1e9 + 7;
     int numDistinct(string s, string t) {
+        int prime = 1e9 + 7;
         int n1 = s.size(), n2 = t.size();
-        vector<vector<int>> dp(n1, vector<int> (n2, -1));
-        return find(n1 - 1, n2 - 1, s, t, dp);
+        vector<vector<int>> dp(n1 + 1, vector<int> (n2 + 1, 0));
+        for(int idx1 = 0; idx1 <= n1; ++idx1)
+            dp[idx1][0] = 1;
+        // as dp[0][0] should give you 1
+        for(int idx2 = 1; idx2 <= n2; ++idx2)
+            dp[0][idx2] = 0;
+        
+        for(int idx1 = 1; idx1 <= n1; ++idx1) {
+            for(int idx2 = 1; idx2 <= n2; ++idx2) {
+                int take = dp[idx1 - 1][idx2 - 1];
+                int notTake = dp[idx1 - 1][idx2];
+                if(s[idx1 - 1] == t[idx2 - 1])
+                    dp[idx1][idx2] = (take + notTake)  % prime;
+                else
+                    dp[idx1][idx2] = notTake;
+            }
+        }
+        return dp[n1][n2];
     }
 };
