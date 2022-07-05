29c83a0d479625d6d615404b38c0133fb7ebdf78
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-05 20:32:39 +0530

    Time: 153 ms (20.56%), Space: 11.3 MB (61.38%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -35,10 +35,6 @@ public:
 
 Approach 2: Using memoization - O(n1.n2) TC & O(n1.n2 + (n1 + n2)) SC
 
-
-
-*/
-
 class Solution {
 private:
     bool f(int idx1, int idx2, string &s, string &p, vector<vector<int>> &dp) {
@@ -67,6 +63,48 @@ public:
     bool isMatch(string s, string p) {
         int n1 = s.size(), n2 = p.size();
         vector<vector<int>> dp(n1, vector<int> (n2, -1));
-        return f(n1 - 1, n2 - 1, s, p, dp) == 1;
+        return f(n1 - 1, n2 - 1, s, p, dp);
+    }
+};
+
+Approach 3: Using tabulation bottom-up - O(n1.n2) TC & SC
+
+*/
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n1 = s.size(), n2 = p.size();
+        vector<vector<bool>> dp(n1 + 1, vector<bool> (n2 + 1, false));
+        // base cases
+        dp[0][0] = true;  // when both s and p gets exhausted
+        for(int idx1 = 1; idx1 <= n1; ++idx1)  // when p gets exhausted
+            dp[idx1][0] = false;  // we can't perform matching
+        for(int idx2 = 1; idx2 <= n2; ++idx2) { // when s gets exhausted
+            // there will only be a matching if we are sure that there will be only * left in p
+            bool allAsterisk = true;
+            for(int i = 1; i <= idx2; ++i) {
+                if(p[i - 1] != '*') {
+                    allAsterisk = false;
+                    break;
+                }
+            }
+            dp[0][idx2] = allAsterisk;
+        }
+        for(int idx1 = 1; idx1 <= n1; ++idx1) {
+            for(int idx2 = 1; idx2 <= n2; ++idx2) {
+                // when we see a match or a '?'
+                if(s[idx1 - 1] == p[idx2 - 1] or p[idx2 - 1] == '?')
+                    dp[idx1][idx2] = dp[idx1 - 1][idx2 - 1];
+                // when we see an asterisk
+                else if(p[idx2 - 1] == '*') {
+                    bool consider = dp[idx1 - 1][idx2];
+                    bool notConsider = dp[idx1][idx2 - 1];
+                    dp[idx1][idx2] = consider or notConsider;
+                }
+                else dp[idx1][idx2] = false;
+            }
+        }
+        return dp[n1][n2];
     }
 };
