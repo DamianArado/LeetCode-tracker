8aee7f870c87025e2a8d520443e9c30ace390aa8
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-07 00:42:48 +0530

    Time: 3 ms (97.64%), Space: 12.9 MB (88.48%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -43,7 +43,30 @@ public:
     }
 };
 
-*/
+Approach 3: Using tabulation - O(n*2) TC & SC
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector<int> (2, 0));
+        for(int index = n - 1; index >= 0; --index) {
+            for(int canBuy = 1; canBuy >= 0; --canBuy) {
+                int profit = 0;
+                if(canBuy)
+                    // max (buy a stock today, didn't buy a stock today)
+                    profit = max(-prices[index] + dp[index + 1][0], 0 + dp[index + 1][1]);
+                else
+                    // max (sell a stock today, didn't sell a stock today)
+                    profit = max(prices[index] + dp[index + 1][1], 0 + dp[index + 1][0]);
+                dp[index][canBuy] = profit;
+            }
+        }
+        return dp[0][1];
+    }
+};
+
+Approach 4: Using space optimization over tabulation - O(n*2) TC & O(4) SC
 
 class Solution {
 public:
@@ -65,5 +88,19 @@ public:
             dp = current;
         }
         return dp[1];
+    }
+};
+
+Approach 5: One pass accumulation of profit using adjacent elements - O(n) TC & O(1) SC
+
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        for(int i = 1; i < prices.size(); ++i)
+            profit += max(prices[i] - prices[i - 1], 0);
+        return profit;
     }
 };
