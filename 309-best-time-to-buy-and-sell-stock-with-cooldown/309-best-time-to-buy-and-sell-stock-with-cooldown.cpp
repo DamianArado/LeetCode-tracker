0fd40ed7b981180d35d2cd3d7312ed7cb31a01f5
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-08 16:24:50 +0530

    Time: 0 ms (100.00%), Space: 11.3 MB (72.39%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -24,6 +24,26 @@ public:
 
 Approach 2: Using tabulation - O(n*2) TC & SC
 
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int dp[n + 2][2];
+        int current[n + 2][2];
+        memset(dp, 0, sizeof(dp));
+        memset(current, 0, sizeof(current));
+        for(int index = n - 1; index >= 0; --index) {
+            dp[index][1] = max(-prices[index] + dp[index + 1][0], dp[index + 1][1]);
+            dp[index][0] = max(prices[index] + dp[index + 2][1], dp[index + 1][0]);
+        }
+        return dp[0][1];
+    }
+};
+
+Approach 3: Space optimize using 3 columns of size 2 - O(n*2) TC & O(2*2*2) -> O(1) SC
+
+
+
 */
 
 
@@ -31,18 +51,22 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        int dp[n + 2][2];
-        memset(dp, 0, sizeof(dp));
+        /*
+        
+                        n + 1           n               n - 1       n - 2
+        i = n - 1       previous2       previous1       current
+        i = n - 2                       previous2       previous1   current
+        
+        */
+        vector<int> previous2(2, 0);        
+        vector<int> previous1(2, 0);
+        vector<int> current(2, 0);
         for(int index = n - 1; index >= 0; --index) {
-            for(int canBuy = 1; canBuy >= 0; --canBuy) {
-                int profit = 0;
-                if(canBuy)
-                    profit = max(-prices[index] + dp[index + 1][0], dp[index + 1][1]);
-                else
-                    profit = max(prices[index] + dp[index + 2][1], dp[index + 1][0]);
-                dp[index][canBuy] = profit;
-            }
+            current[1] = max(-prices[index] + previous1[0], previous1[1]);
+            current[0] = max(prices[index] + previous2[1], previous1[0]);
+            previous2 = previous1;
+            previous1 = current;
         }
-        return dp[0][1];
+        return current[1];
     }
 };
