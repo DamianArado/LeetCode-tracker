2daded2c4b8d1223ea5bd18e7fce947ec993b8d8
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-08 16:09:03 +0530

    Time: 0 ms (100.00%), Space: 11.3 MB (72.39%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,3 +1,7 @@
+/**
+
+Approach 1: Memoization using recursion - O(n*2) TC & O(n*2 + n) SC
+
 class Solution {
 private:
     int f(int idx, int canBuy, vector<int>& prices, vector<vector<int>> &dp, int n) {
@@ -15,5 +19,30 @@ public:
         int n = prices.size();
         vector<vector<int>> dp(n, vector<int> (2, -1));
         return f(0, 1, prices, dp, n);
+    }
+};
+
+Approach 2: Using tabulation - O(n*2) TC & SC
+
+*/
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int dp[n + 2][2];
+        memset(dp, 0, sizeof(dp));
+        for(int index = n - 1; index >= 0; --index) {
+            for(int canBuy = 1; canBuy >= 0; --canBuy) {
+                int profit = 0;
+                if(canBuy)
+                    profit = max(-prices[index] + dp[index + 1][0], dp[index + 1][1]);
+                else
+                    profit = max(prices[index] + dp[index + 2][1], dp[index + 1][0]);
+                dp[index][canBuy] = profit;
+            }
+        }
+        return dp[0][1];
     }
 };
