7c53f2745da77f753acacb39300f60de414d5fd2
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-12 18:53:06 +0530

    Time: 21 ms (86.52%), Space: 18.6 MB (52.15%) - LeetHub

--- 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
@@ -1,13 +1,7 @@
 /**
 
-Approach 1 - Calculate the up slope and down slope
+Approach 1: Calculate the up slope and down slope: TC -> O(n) SC -> O(n)
 
-
-
-
-
-
-*/
 class Solution {
 public:
     int longestMountain(vector<int>& arr) {
@@ -27,5 +21,30 @@ public:
                 longestMnt = max(longestMnt, up[i] + down[i] + 1);
         }
         return longestMnt;
+    }
+};
+
+Approach 2: A O(n) time and O(1) space one-pass solution
+
+*/
+class Solution {
+public:
+    int longestMountain(vector<int>& arr) {
+        int maxMountain = 0, n = arr.size(), idx = 1, up = 0, down = 0;
+        while (idx < n) {
+            while(idx < n && arr[idx - 1] == arr[idx])
+                ++idx;
+            // start looking further in array for a possible mountain
+            up = 0;
+            while(idx < n && arr[idx - 1] < arr[idx])
+                ++up, ++idx;
+            down = 0;
+            while(idx < n && arr[idx - 1] > arr[idx])
+                ++down, ++idx;
+            if(up && down)  
+                // + 1 as we do add the peak itself
+                maxMountain = max(maxMountain, up + down + 1);
+        }
+        return maxMountain;
     }
 };
