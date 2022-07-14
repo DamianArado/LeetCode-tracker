b1041dd202e86e452594d113c949d88f6a163e3f
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-14 22:54:56 +0530

    Time: 178 ms (61.76%), Space: 30.5 MB (91.71%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -8,17 +8,24 @@ public:
         for(char &item : corridor) {
             if(item == 'S') 
                 numSeats += 1;
+            // as we only care about plants when we crossed 2 seats
             if(numSeats == 2 and item == 'P')
                 numPlants += 1;
+            // its time to divide as we got a 3rd seat
             if(numSeats == 3) {
+                // + 1 as we count the position of the 3rd seat as well
+                // number of dividers installed till now will be reused in future for total number of dividers
                 dividers *= (numPlants + 1);
                 dividers %= mod;
                 numPlants = 0;
+                // after installing a divider before or on the 3rd seat, its considered as our 1st seat now
                 numSeats = 1;
             }
         }
         
+        // no need to divide
         if(numSeats < 2) return 0;
+        
         return dividers;
     }
 };
