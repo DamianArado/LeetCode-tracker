6912dae28eb476243c703fc847dfcaa192d29022
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-14 20:37:57 +0530

    Time: 100 ms (61.95%), Space: 10.3 MB (39.07%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -23,8 +23,7 @@ public:
     }
 };
 
-Approach 2: Using memoization - O()
-*/
+Approach 2: Using memoization - O(c^3) TC & O(c^2 + c) SC, where c is the size of given cuts array
 
 class Solution {
 private:
@@ -47,5 +46,34 @@ public:
         // as the cuts can be made at these 5 locations only for cuts[]: [1, 2, 4, 5, 6]
         vector<vector<int>> dp(c + 1, vector<int> (c + 1, -1));
         return f(1, c, cuts, dp);
+    }
+};
+
+Approach 3: Tabulation based - O(c^2) TC & SC
+*/
+
+class Solution {
+public:
+    int minCost(int n, vector<int>& cuts) {
+        int c = cuts.size();
+        cuts.insert(cuts.begin(), 0);
+        cuts.push_back(n);
+        sort(cuts.begin(), cuts.end());
+        // as the cuts can be made at these 5 locations only for cuts[]: [1, 2, 4, 5, 6]
+        // adding 1 extra as when i = c, idx will access i + 1
+        vector<vector<int>> dp(c + 2, vector<int> (c + 2, 0));
+        
+        for(int i = c; i >= 1; --i) {
+            for(int j = 1; j <= c; ++j) {
+                if(i > j) continue;
+                int minTotalCost = 1e9;
+                for(int idx = i; idx <= j; ++idx) {
+                    int cost = (cuts[j + 1] - cuts[i - 1]) + dp[i][idx - 1] + dp[idx + 1][j];
+                    minTotalCost = min(minTotalCost, cost);
+                }
+                dp[i][j] = minTotalCost;
+            }
+        }
+        return dp[1][c];
     }
 };
