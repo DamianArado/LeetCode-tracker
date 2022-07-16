634d449e00b585dca93f78348f32b0b0a97d3b64
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-16 21:37:21 +0530

    Time: 16 ms (86.26%), Space: 8.7 MB (32.27%) - LeetHub

---
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -0,0 +1,50 @@
+/***
+
+Approach 1: Using recursion - exponential time and O(n) space
+
+class Solution {
+private:
+    int f(int start, int k, int n, vector<int> &arr) {
+        if(start == n) return 0;
+        int maxNum = -1, maxSum = 0, length = 0;
+        for(int end = start; end < min(n, start + k); ++end) {
+            maxNum = max(maxNum, arr[end]);
+            length += 1;
+            int sum = maxNum * length + f(end + 1, k, n, arr);
+            maxSum = max(maxSum, sum);
+        }
+        return maxSum;
+    }
+public:
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int n = arr.size();
+        return f(0, k, n, arr);
+    }
+};
+
+Approach 2: Using memoization over recursion - O(nk) TC & O(2n) SC
+
+
+*/
+
+class Solution {
+private:
+    int f(int start, int k, int n, vector<int> &arr, vector<int> &dp) {
+        if(start == n) return 0;
+        if(dp[start] != -1) return dp[start];
+        int maxNum = -1, maxSum = 0, length = 0;
+        for(int end = start; end < min(n, start + k); ++end) {
+            maxNum = max(maxNum, arr[end]);
+            length += 1;
+            int sum = maxNum * length + f(end + 1, k, n, arr, dp);
+            maxSum = max(maxSum, sum);
+        }
+        return dp[start] = maxSum;
+    }
+public:
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int n = arr.size();
+        vector<int> dp(n, -1);
+        return f(0, k, n, arr, dp);
+    }
+};
