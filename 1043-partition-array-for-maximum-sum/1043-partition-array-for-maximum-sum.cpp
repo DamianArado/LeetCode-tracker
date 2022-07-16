8f162b15f667180c615992cfec7a7782f534e08c
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-16 21:42:40 +0530

    Time: 15 ms (89.38%), Space: 8.6 MB (32.27%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -24,9 +24,6 @@ public:
 
 Approach 2: Using memoization over recursion - O(nk) TC & O(2n) SC
 
-
-*/
-
 class Solution {
 private:
     int f(int start, int k, int n, vector<int> &arr, vector<int> &dp) {
@@ -46,5 +43,28 @@ public:
         int n = arr.size();
         vector<int> dp(n, -1);
         return f(0, k, n, arr, dp);
+    }
+};
+
+Approach 3: Using tabulation - O(nk) TC & O(n) SC
+
+*/
+
+class Solution {
+public:
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int n = arr.size();
+        vector<int> dp(n + 1, 0);
+        for(int start = n - 1; start >= 0; --start) {
+            int maxNum = -1, maxSum = 0, length = 0;
+            for(int end = start; end < min(n, start + k); ++end) {
+                maxNum = max(maxNum, arr[end]);
+                length += 1;
+                int sum = maxNum * length + dp[end + 1];
+                maxSum = max(maxSum, sum);
+            }
+            dp[start] = maxSum;
+        }
+        return dp[0];
     }
 };
