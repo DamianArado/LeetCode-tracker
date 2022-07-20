352fa2ee9da4f5afc5ab46da4dd47e048b8a0b4b
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-20 21:30:49 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (86.24%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(target == nums[mid]) return mid;
+            else if(target < nums[mid]) end = mid - 1;
+            else start = mid + 1;
+        }
+        return -1;
+    }
+}
