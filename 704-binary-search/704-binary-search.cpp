b4b1f2099cd40106671930fb6ada76ce1be13200
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-20 21:26:37 +0530

    Time: 67 ms (30.52%), Space: 27.6 MB (12.32%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0, end = nums.size() - 1;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(target == nums[mid]) return mid;
+            else if(target < nums[mid]) end = mid - 1;
+            else start = mid + 1;
+        }
+        return -1;
+    }
+};
