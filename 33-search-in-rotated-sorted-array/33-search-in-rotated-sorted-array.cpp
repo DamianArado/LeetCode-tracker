67bc7a60b416597f121dd81ea16e62920f3ed236
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-21 19:06:19 +0530

    Time: 7 ms (52.53%), Space: 11.1 MB (29.04%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    int findMinIdx(vector<int> &nums) {
-        int low = 0, high = nums.size() - 1;
-        
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] > nums[high]) low = mid + 1;
-            else high = mid;
-        }
-        return low;
-    }
-    
     int search(vector<int> &nums, int target) {
-        int minIdx = findMinIdx(nums);
-        
-        if (target == nums[minIdx]) return minIdx;
-        
-        int n = nums.size();
-        
-        int low = (target <= nums[n - 1]) ? minIdx : 0;
-        int high = (target > nums[n - 1]) ? minIdx : n - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] == target) return mid;
-            else if (target > nums[mid]) low = mid + 1;
-            else high = mid - 1;
+        int start = 0, end = nums.size() - 1;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(target == nums[mid]) return mid;
+            // If we check in a part where we see rotation
+            // We start searching in the sorted region if our target lies in that range
+            if(nums[start] > nums[mid]) {
+                if(target > nums[mid] and target <= nums[end])
+                    start = mid + 1;
+                else end = mid - 1;
+            } else if(nums[mid] > nums[end]) {
+                if(target >= nums[start] and target < nums[mid])
+                    end = mid - 1;
+                else start = mid + 1;
+            // If we enter the sorted region
+            } else {
+                if(target > nums[mid]) start = mid + 1;
+                else end = mid - 1;
+            }
         }
         return -1;
     }
