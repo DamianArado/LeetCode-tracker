3e2764059d050bebe9cacf60f3fb777ccc0a9a92
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-22 23:17:03 +0530

    Time: 17 ms (23.22%), Space: 54.3 MB (34.48%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,39 @@
+class Solution {
+    public static boolean canShip(int[] weights, int days, int maxCapacity) {
+        int currentDays = 1, currentCapacity = 0;
+        for(int weight : weights) {
+            // we can't even use a single ship with max capacity for this weight
+            if(weight > maxCapacity) return false;
+            // we need more days to ship
+            if(weight + currentCapacity > maxCapacity) {
+                currentDays += 1;
+                // if we can't ship anymore
+                if(currentDays > days) return false;
+                currentCapacity = weight;
+            } else {
+                // keep adding more weight to my ship today
+                currentCapacity += weight;
+            }
+        }
+        return true;
+    }
+    public int shipWithinDays(int[] weights, int days) {
+        int capacity = 0, maxWeight = 0, sumWeights = 0;
+        for(int weight : weights) {
+            maxWeight = Math.max(maxWeight, weight);
+            sumWeights += weight;
+        }
+        // lets start our binary search
+        int start = maxWeight, end = sumWeights;
+        while(start <= end) {
+            // mid is the max capacity that we are allocated for now in order to ship all the packages in days days
+            int mid = start + (end - start) / 2;
+            if(canShip(weights, days, mid) == true) {
+                // store the answer and try for better answer towards left
+                capacity = mid;
+                end = mid - 1;
+            } else start = mid + 1;  // increase the days boss
+        }
+        return capacity;
+    }
+}
