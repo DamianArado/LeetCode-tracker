6f230158df81cd8da5201017e8595c8b6b5bfea1
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-22 11:49:20 +0530

    Time: 4 ms (76.37%), Space: 8.9 MB (49.64%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -10,7 +10,7 @@ public:
         if(nums[n - 1] > nums[n - 2]) return n - 1;
         
         // now we will check for the remaining elements
-        int start = 1, end = n - 1;
+        int start = 1, end = n - 2;
         while(start <= end) {
             int mid = start + (end - start) / 2;
             if(nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]) return mid;
