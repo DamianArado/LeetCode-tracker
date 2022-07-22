cf94ce151d191f658bd594eb713e61842543f62a
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-22 17:09:04 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (35.40%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,12 +1,28 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length, n = matrix[0].length;
-        int row = 0, col = 0;
-        while(row < m && col < n) {
-            if(target == matrix[row][col]) return true;
-            if(target > matrix[row][n - 1]) row++;
-            else col++;
+        
+        // Step 1: Find the row in which target can lie (the last element which is smaller than target in the 1st column)
+        int start = 0, end = m - 1, targetRow = 0;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(target == matrix[mid][0]) return true;
+            if(target > matrix[mid][0]) {
+                targetRow = mid;
+                start = mid + 1;
+            } else end = mid - 1;
         }
+        
+        // Step 2: Search in this row using binary search
+        start = 0; 
+        end = n - 1;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(target == matrix[targetRow][mid]) return true;
+            if(target > matrix[targetRow][mid]) start = mid + 1;
+            else end = mid - 1;
+        }
+        // If element is not present in the matrix
         return false;
     }
 }
