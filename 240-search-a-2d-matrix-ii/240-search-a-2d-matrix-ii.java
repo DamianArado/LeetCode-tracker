a935976ede55a077c220e28214a11ef044dcc698
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-22 23:09:12 +0530

    Time: 11 ms (50.70%), Space: 57.6 MB (60.49%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // Start from the top-right corner
+        int row = 0, col = matrix[0].length - 1;
+
+        // Keeps searching when there are unclassified rows and columns
+        while(row < matrix.length && col >= 0) {
+            if(matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+
+                // Eliminate this row
+                ++row;
+            } else {
+
+                // A[row][col] > target, eliminate this column
+                --col;
+            }
+        }
+        return false;
+    }
+}
