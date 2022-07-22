3a7a1416b60ff41380f40e3b4c80a60142dc7e1f
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-22 11:49:10 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (66.52%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -9,7 +9,7 @@ class Solution {
         if(nums[n - 1] > nums[n - 2]) return n - 1;
         
         // now we will check for the remaining elements
-        int start = 1, end = n - 1;
+        int start = 1, end = n - 2;
         while(start <= end) {
             int mid = start + (end - start) / 2;
             if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;
