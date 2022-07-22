f180a280554ba913efe4b198409f668f322ad693
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-22 17:14:19 +0530

    Time: 76 ms (36.17%), Space: 14.5 MB (42.69%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,9 +1,29 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         m, n = len(matrix), len(matrix[0])
-        row, col = 0, 0
-        while row < m and col < n:
-            if target == matrix[row][col]: return True
-            if target > matrix[row][n - 1]: row += 1
-            else: col += 1
+        
+        # Step 1: Find the row in which target can lie (the last element which is smaller than target in the 1st column)
+        start, end, targetRow = 0, m - 1, 0
+        while start <= end:
+            mid = start + (end - start) // 2
+            if target == matrix[mid][0]: 
+                return True
+            if target > matrix[mid][0]:
+                targetRow = mid
+                start = mid + 1
+            else:
+                end = mid - 1
+                
+        # Step 2: Search in this row using binary search
+        start, end = 0, n - 1
+        while start <= end:
+            mid = start + (end - start) // 2
+            if target == matrix[targetRow][mid]:
+                return True
+            if target > matrix[targetRow][mid]:
+                start = mid + 1
+            else:
+                end = mid - 1
+        
+        # If element is not present in the matrix
         return False
