c1a92f6e7c11fc4019d831f69c908d21567ee3c1
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-27 16:38:13 +0530

    Time: 389 ms (72.19%), Space: 134.8 MB (57.36%) - LeetHub

---
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(n == 1 || k == 1) return nums;
+        
+        vector<int> maxInWindow;
+        deque<int> dq;
+        
+        // our sliding window exists in the range [startIdx + 1, endIdx]
+        for(int endIdx = 0; endIdx < n; ++endIdx) {
+            int startIdx = endIdx - k;
+            // slide the window forward and hence remove it from the front of the deque
+            if(!dq.empty() and dq.front() == startIdx)
+                dq.pop_front();
+            // before adding any index check if we can remove some elements in the deque from the end which 
+            // are smaller than this element that is to be added and can be a potential entry in our array
+            while(!dq.empty() and nums[endIdx] > nums[dq.back()])
+                dq.pop_back();
+            // having taken all the above conditions into account now we can add the index inside our deque
+            dq.push_back(endIdx);
+            // start adding the candidates as answer 
+            // whichever element's index is there on the front in deque is our answer now
+            if(endIdx >= k - 1) 
+                maxInWindow.push_back(nums[dq.front()]);
+        }
+        
+        return maxInWindow;
+    }
+};
