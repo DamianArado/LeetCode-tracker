2d697520d37df45b81cf0592d9cd2ef099e46718
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-29 21:59:41 +0530

    Time: 8 ms (9.68%), Space: 8.3 MB (77.07%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -9,28 +9,51 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+/**
+
+Approach 1 - O(n) TC & O(height) SC
+
+class Solution {
+private:
+    vector<int> preorder;
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(!root) return preorder;
+        preorder.push_back(root->val);
+        preorderTraversal(root->left);
+        preorderTraversal(root->right);
+        return preorder;
+    }
+};
+
+Approach 2 - approx. O(n) TC & O(1) SC [Morris]
+
+*/
+
 class Solution {
 public:
-    // Morris Traversal - O(n) TC & O(1) SC
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> preorder;
         if(!root) return preorder;
-        TreeNode *curr = root;
-        while(curr) {
-            if(!curr->left) {  // if there is nothing on left it's the leftmost
-                preorder.emplace_back(curr->val);  // print the current root
-                curr = curr->right;  // move right from here
-            } else {  // we have something on left
-                TreeNode *prev = curr->left;  // use prev as marker for the left subtree analysis
-                while(prev->right && prev->right != curr) prev = prev->right; // till we reach the rightmost node of that left subtree
-                if(!prev->right) {  // we reached the rightmost node of the left subtree
-                    preorder.emplace_back(curr->val);  // print the current root
-                    prev->right = curr;  // create the thread that joins the rightmost element to the current root
-                    curr = curr->left;  // now move towards left from here
-                } else {  // if the thread already exists
-                    prev->right = NULL;  // cut the thread that exists
-                    curr = curr->right;  // now move right from here
+        TreeNode* current = root;
+        while(current) {
+            if(current->left) {
+                TreeNode* leftSubTree = current->left;
+                while(leftSubTree->right and leftSubTree->right != current)
+                    leftSubTree = leftSubTree->right;
+                if(!leftSubTree->right) {
+                    leftSubTree->right = current;
+                    preorder.push_back(current->val);
+                    current = current->left;
+                } else {
+                    leftSubTree->right = nullptr;
+                    current = current->right;
                 }
+            } else {
+                preorder.push_back(current->val);
+                current = current->right;
             }
         }
         return preorder;
