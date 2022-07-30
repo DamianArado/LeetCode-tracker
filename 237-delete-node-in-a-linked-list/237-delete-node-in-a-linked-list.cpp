f36b4275225dc7c6e0949e9d31207ca8acfd3880
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-30 18:01:04 +0530

    Time: 24 ms (31.04%), Space: 7.7 MB (39.29%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
@@ -9,9 +9,14 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
+        // since its not the tail node we are 100% sure there is atleast one node after this 
+        // lets swap the node with its next node
         swap(node->val, node->next->val);
+        // mark this to be deleted
         ListNode* temp = node->next;
+        // change the connections
         node->next = temp->next;
+        // now delete this node
         delete temp;
     }
 };
