8f9e435da2bf753642f7f126553af83b95c05c14
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-30 17:14:40 +0530

    Time: 8 ms (86.29%), Space: 18.9 MB (55.79%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -9,22 +9,10 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-/*
-
-We have 2 possible ways to solve this: Recursive and Iterative
-
-The Recursive approach takes O(n) recursive stack space in case of Skewed Binary Tree
-The Iterative approach takes O(n) space in worst case in case of complete Binary Tree
-
-Taking the recursive approach, we break the solution into the recurrence: 1 + max(left, right)
-
-*/
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
         if(!root) return 0;
-        int maxLeft = maxDepth(root->left);
-        int maxRight = maxDepth(root->right);
-        return 1 + max(maxLeft, maxRight);
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
