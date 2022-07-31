5139287ecb12ad5d2820e48f00c01b321a2a168c
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-01 00:38:37 +0530

    Time: 21 ms (36.48%), Space: 20.1 MB (99.62%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -9,36 +9,21 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-/*
-
-Naive solution - O(n^2) TC & O(n) SC
-
-For each of the N nodes, find leftHeight + rightHeight and take the max of it.
-
-diameterOfBinaryTree(root) {
-    if(!root) return;
-    leftHeight = diameterOfBinaryTree(root->left);  // at max O(n) [Skewed Trees]
-    rightHeight = diameterOfBinaryTree(root->right);
-    diameter = max(leftHeight + rightHeight);
-    diameterOfBinaryTree(root->left);  // at max O(n) [left skewed trees]
-    diameterOfBinaryTree(root->right);
-    return diameter;
-}
-
-*/
 class Solution {
 private:
-    int calculate(TreeNode* root, int &diameter) {
+    int calculateHeight(TreeNode* root, int &diameter) {
         if(!root) return 0;
-        int leftHeight = calculate(root->left, diameter);
-        int rightHeight = calculate(root->right, diameter);
+        
+        int leftHeight = calculateHeight(root->left, diameter);
+        int rightHeight = calculateHeight(root->right, diameter);
+        
         diameter = max(diameter, leftHeight + rightHeight);
         return 1 + max(leftHeight, rightHeight);
     }
 public:
     int diameterOfBinaryTree(TreeNode* root) {
         int diameter = 0;
-        calculate(root, diameter);
+        calculateHeight(root, diameter);
         return diameter;
     }
 };
