2d07b219bacb8168d2e7f71c37ec35b1f6ef54d1
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-07-31 23:23:39 +0530

    Time: 22 ms (48.68%), Space: 21 MB (54.77%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -9,25 +9,17 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-/*
-
-In a balanced binary tree, for every node the left and right subtree's heights should not differ by more
-than 1.
-
-*/
 class Solution {
 private:
-    // Calculate the height of the binary tree in O(n)
-    int dfsHeight(TreeNode* root) {
-        if(!root) return 0;
-        int maxLeft = dfsHeight(root->left);
-        int maxRight = dfsHeight(root->right);
-        if(abs(maxLeft - maxRight) > 1) return -1;
-        if(maxLeft == -1 || maxRight == -1) return -1;
-        return 1 + max(maxLeft, maxRight);
+    int maxDepth(TreeNode* root) {
+        return root == nullptr ? 0 : 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 public:
     bool isBalanced(TreeNode* root) {
-        return dfsHeight(root) != -1;
+        if(!root) return true;
+        if((abs(maxDepth(root->left) - maxDepth(root->right)) > 1)
+          or (!isBalanced(root->left)) or !isBalanced(root->right)) 
+            return false;
+        return true;
     }
 };
