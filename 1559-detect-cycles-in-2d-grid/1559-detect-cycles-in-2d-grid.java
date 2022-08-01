82bdcb98d9a2a2b1872c379c7483b4c32a11ada9
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-01 15:19:32 +0530

    Time: 34 ms (54.65%), Space: 135.6 MB (71.24%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
@@ -1,66 +1,27 @@
-/*
-At each point (i, j) in grid, do following:
-
-Start DFS from (i, j).
-Don't visit the last visited point as stated in question.
-Only visit a point if it has same character as starting position
-If you still reach a visited point again, there is a cycle.
-
-TC & SC - O(m * n)
-
-*/
-
 class Solution {
-    int[] DIR_X = {1, -1, 0, 0};
-    int[] DIR_Y = {0, 0, 1, -1};
-	
-    private boolean dfs(int curX, int curY, int lastX, int lastY, int n, int m, boolean[][] vis, char[][] grid, char startChar) {
-        vis[curX][curY] = true;
-        boolean hasCycle = false;
+    // prevMove: 0 - start of DFS, 1 - up, 2 - right, 3 - down, 4 - left
+    private static boolean isCycle2DGrid(char[][] grid, 
+                                         int prevMove, int i, int j, char c, boolean[][] visited) {
+        if((i == -1 || i == grid.length) || (j == -1 || j == grid[0].length) || grid[i][j] != c)
+            return false;
+        if(visited[i][j]) return true;
         
-        // Visit all directions
-        for (int i = 0; i < 4; ++i) {
-            int newX = curX + DIR_X[i];
-            int newY = curY + DIR_Y[i];
-            
-            // Valid point?
-            if (newX >= 0 && newX < n && newY >= 0 && newY < m) {
-                
-                // Don't visit last visited point
-                if (!(newX == lastX && newY == lastY)) {
-                    
-                    // Only visit nodes that equal start character
-                    if (grid[newX][newY] == startChar) {
-
-                        if (vis[newX][newY]) {
-                            
-						    // Still visited? There is a cycle.
-                            return true;
-                            
-                        } else {
-                            hasCycle |= dfs(newX, newY, curX, curY, n, m, vis, grid, startChar);
-                        }
-                    }
-                }
-            }
-        }
-        return hasCycle;
+        visited[i][j] = true;
+        return (prevMove != 1 && isCycle2DGrid(grid, 3, i + 1, j, grid[i][j], visited))
+            || (prevMove != 2 && isCycle2DGrid(grid, 4, i, j - 1, grid[i][j], visited))
+            || (prevMove != 3 && isCycle2DGrid(grid, 1, i - 1, j, grid[i][j], visited))
+            || (prevMove != 4 && isCycle2DGrid(grid, 2, i, j + 1, grid[i][j], visited));
     }
-	
     public boolean containsCycle(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        boolean[][] vis = new boolean[n][m];
-        boolean hasCycle = false;
+        int row = grid.length, col = grid[0].length;
+        boolean[][] visited = new boolean[row][col];
         
-        for (int i = 0; i < grid.length; ++i) {
-            for (int j = 0; j < grid[i].length; ++j) {
-                if (!vis[i][j]) {
-                    hasCycle |= dfs(i, j, -1, -1, n, m, vis, grid, grid[i][j]);
-                }
-            }
-        }
+        for(int i = 0; i < row; ++i)
+            for(int j = 0; j < col; ++j)
+                if(!visited[i][j])
+                    if(isCycle2DGrid(grid, 0, i, j, grid[i][j], visited))
+                        return true;
         
-        return hasCycle;
+        return false;
     }
 }
