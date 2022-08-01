f6d7f819ca24d6c58aaa062b892b5731cacdd5e0
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-01 15:21:26 +0530

    Time: 584 ms (24.72%), Space: 51.8 MB (94.65%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
@@ -1,17 +1,17 @@
 class Solution {
 private:
     // param: move - all 4 moves (0 - start dfs, 1 - up, 2 - right, 3 - down, 4 - left)
-    bool isCycleDFS(vector<vector<char>> &grid, int prevMove, int i, int j, 
+    bool isCycle2DGrid(vector<vector<char>> &grid, int prevMove, int i, int j, 
                     char &c, vector<vector<bool>> &visited) {
         if((i == -1 or i == grid.size()) or (j == -1 or j == grid[0].size()) or grid[i][j] != c)
             return false;
         if(visited[i][j]) return true;
         visited[i][j] = true;
         
-        return (prevMove != 1 and isCycleDFS(grid, 3, i + 1, j, grid[i][j], visited))
-            or (prevMove != 2 and isCycleDFS(grid, 4, i, j - 1, grid[i][j], visited))
-            or (prevMove != 3 and isCycleDFS(grid, 1, i - 1, j, grid[i][j], visited))
-            or (prevMove != 4 and isCycleDFS(grid, 2, i, j + 1, grid[i][j], visited));
+        return (prevMove != 1 and isCycle2DGrid(grid, 3, i + 1, j, grid[i][j], visited))
+            or (prevMove != 2 and isCycle2DGrid(grid, 4, i, j - 1, grid[i][j], visited))
+            or (prevMove != 3 and isCycle2DGrid(grid, 1, i - 1, j, grid[i][j], visited))
+            or (prevMove != 4 and isCycle2DGrid(grid, 2, i, j + 1, grid[i][j], visited));
     }
 public:
     bool containsCycle(vector<vector<char>>& grid) {
@@ -21,7 +21,7 @@ public:
         for(int i = 0; i < row; ++i) 
             for(int j = 0; j < col; ++j) 
                 if(!visited[i][j])
-                    if(isCycleDFS(grid, 0, i, j, grid[i][j], visited))
+                    if(isCycle2DGrid(grid, 0, i, j, grid[i][j], visited))
                         return true;
         
         return false;
