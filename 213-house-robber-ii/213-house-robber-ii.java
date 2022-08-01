9528bc434d99035d957db032374e584d2e6b29f2
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-01 17:50:52 +0530

    Time: 0 ms (100.00%), Space: 40 MB (85.46%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -1,18 +1,16 @@
 class Solution {
-    private static int maxRob(int[] nums, int l, int r) {
-        int prev2 = 0, prev = nums[l];
-        for(int i = l + 1; i < r; ++i) {
-            int pick = nums[i] + prev2;
-            int dontPick = prev;
-            int currentMax = Math.max(pick, dontPick);
+    private static int houseRob(int[] nums, int start, int end) {
+        int prev2 = 0, prev = nums[start];
+        for(int idx = start + 1; idx <= end; ++idx) {
+            int current = Math.max(nums[idx] + prev2, prev);
             prev2 = prev;
-            prev = currentMax;
+            prev = current;
         }
         return prev;
     }
     public int rob(int[] nums) {
         int n = nums.length;
         if(n == 1) return nums[0];
-        return Math.max(maxRob(nums, 0, n - 1), maxRob(nums, 1, n));
+        return Math.max(houseRob(nums, 0, n - 2), houseRob(nums, 1, n - 1));
     }
 }
