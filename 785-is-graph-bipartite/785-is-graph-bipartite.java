58cd791f926ae563b5e40d7b6f926fd31f07a186
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-02 22:48:21 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (88.19%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,26 +1,22 @@
 class Solution {
-    private static boolean checkBipartite(int current, int[][] graph, int[] color) {
-        if(color[current] == -1)
-            color[current] = 1;
-        for(int adj : graph[current]) {
-            if(color[adj] == -1) {
-                color[adj] = 1 - color[current];
-                if(checkBipartite(adj, graph, color) == false)
+    private static boolean isBipartiteDFS(int current, int[][] graph, int[] color) {
+        if(color[current] == -1) color[current] = 1;
+        for(int adjNode : graph[current]) {
+            if(color[adjNode] == -1) {
+                color[adjNode] = 1 - color[current];
+                if(!isBipartiteDFS(adjNode, graph, color)) 
                     return false;
-            } else if(color[adj] == color[current]) 
-                return false;
+            } else if(color[adjNode] == color[current]) return false;
         }
         return true;
     }
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
-        int color[] = new int[n];
+        int[] color = new int[n];
+        for(int i = 0; i < n; ++i) color[i] = -1;
         for(int i = 0; i < n; ++i) 
-            color[i] = -1;
-        for(int i = 0; i < n; ++i) {
-            if(checkBipartite(i, graph, color) == false)
-                return false;
-        }
+            if(color[i] == -1 && !isBipartiteDFS(i, graph, color))
+               return false;
         return true;
     }
 }
