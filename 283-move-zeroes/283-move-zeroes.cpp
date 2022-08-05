00e2ede8fdb739b005152f12c7ac79167d42be69
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-05 23:44:55 +0530

    Time: 23 ms (92.81%), Space: 19 MB (98.07%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int zeroes = 0, n = nums.size();
+        for(int num : nums)
+            if(num == 0) zeroes++;
+        int nonZeroes = n - zeroes;
+        
+        int eIdx = 0;
+        for(int i = 0; i < n; ++i) 
+            if(nums[i] != 0) nums[eIdx++] = nums[i];
+        while(eIdx < n) 
+            nums[eIdx++] = 0;
+    }
+};
