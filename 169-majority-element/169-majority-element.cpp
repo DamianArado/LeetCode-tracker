003244af21ac24bf777c875377dd829fdc7a1861
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-08 00:40:06 +0530

    Time: 33 ms (44.12%), Space: 19.5 MB (92.27%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,12 +1,12 @@
-class Solution { // Using Moore's Voting Algorithm -> O(n) TC & O(1) SC
+class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int count = 0, elem;
+        int count = 0, element = nums[0];
         for(int i = 0; i < nums.size(); ++i) {
-            if(count == 0) elem = nums[i];
-            if(elem == nums[i]) ++count;
-            else --count;
+            if(count == 0) element = nums[i];
+            if(nums[i] == element) count++;
+            else count--;
         }
-        return elem;
+        return element;
     }
 };
