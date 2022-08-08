c3a94dc9eaeb70d18393450bb1aad49e2bfa0553
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-09 01:57:17 +0530

    Time: 21 ms (54.01%), Space: 15.9 MB (51.45%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> majorityElements;
-        int sz = nums.size();
-        int elem1 = -1, elem2 = -1, cnt1 = 0, cnt2 = 0;
-        for(int i = 0; i < sz; ++i) {
-            if(nums[i] == elem1) {
-                ++cnt1;
-            } else if(nums[i] == elem2) {
-                ++cnt2;
+        vector<int> majElem;
+        int n = nums.size(), elem1 = -1, cnt1 = 0, elem2 = -1, cnt2 = 0;
+        for(int num : nums) {
+            if(num == elem1) {
+                cnt1++;
+            } else if(num == elem2) {
+                cnt2++;
             } else if(cnt1 == 0) {
-                elem1 = nums[i]; 
+                elem1 = num;
                 cnt1 = 1;
             } else if(cnt2 == 0) {
-                elem2 = nums[i]; 
+                elem2 = num;
                 cnt2 = 1;
             } else {
-                --cnt1; 
-                --cnt2;
+                cnt1--;
+                cnt2--;
             }
         }
         cnt1 = 0, cnt2 = 0;
-        for(int i = 0; i < sz; ++i) {
-            if(nums[i] == elem1) ++cnt1;
-            if(nums[i] == elem2) ++cnt2;
+        for(int num : nums) {
+            if(num == elem1) cnt1++;
+            if(num == elem2) cnt2++;
         }
-        if(cnt1 > sz / 3) majorityElements.emplace_back(elem1);
-        if(cnt2 > sz / 3 && elem2 != elem1) majorityElements.emplace_back(elem2);
-        return majorityElements;
+        if(cnt1 > n / 3) majElem.push_back(elem1);
+        if(cnt2 > n / 3 and elem2 != elem1) majElem.push_back(elem2);
+        return majElem;
     }
 };
