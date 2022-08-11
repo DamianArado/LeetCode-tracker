02c6b821edfe82f91faca319749f99b050c05466
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-11 16:16:12 +0530

    Time: 412 ms (18.25%), Space: 15.9 MB (100.00%) - LeetHub

---
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        n = len(nums)
+        heap = []
+        for num in nums: 
+            heappush(heap, -1 * num)
+        for i in range (1, n, 2):
+            nums[i] = -1 * heap[0]
+            heappop(heap)
+        for i in range (0, n, 2):
+            nums[i] = -1 * heap[0]
+            heappop(heap)
