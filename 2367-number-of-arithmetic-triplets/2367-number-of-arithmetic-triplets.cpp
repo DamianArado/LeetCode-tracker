847009d559155995a4cbce4d4dd51aafb27cea71
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-11 14:50:05 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (25.00%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     // Check if number - diff and number - 2 * diff occurred in nums
     int arithmeticTriplets(vector<int>& nums, int diff) {
-        int count[201] = {};
+        vector<int> count(201, 0);
         int result = 0;
         for(int num : nums) {
             if(num >= 2 * diff)
