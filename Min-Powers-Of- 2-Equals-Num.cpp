0932cf8657011b3f371e1e62ee1780630e88c9c7
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-11 21:57:06 +0530

    Create Min-Powers-Of- 2-Equals-Num.cpp

---
+++ Min-Powers-Of- 2-Equals-Num.cpp
@@ -0,0 +1,35 @@
+/*
+ *  Given a number n, represent this number as the summation of the minimum number of powers of 2.
+ *
+ *  I/P: 13   O/P: [1, 4, 8]  Explanation: 1 + 4 + 8 = 13
+ *  I/P: 3   O/P: [1, 2]
+ */
+
+// Approach 1 : tc - O(log2n), sc - 2 * O(log2n)
+int minPowersOf2EqualsNum(int number) {
+    vector<int> remainders, minPowersOf2;
+    while(number > 0) {
+        int remainder = number % 2;
+        remainders.push_back(remainder);
+        number = number / 2;
+    }
+    for(int i = 0; i < remainders.size(); ++i) {
+        if(remainders[i] == 1) 
+            minPowersOf2.push_back(pow(2, i));
+    }
+    sort(minPowersOf2.begin(), minPowersOf2.end());
+    return minPowersOf2;
+}
+
+// Approach 2: tc & sc - O(log2n)
+int minPowersOf2EqualsNum(int number) {
+    vector<int> minPowersOf2;
+    int power = 0;
+    while(number > 0) {
+        if(number % 2 == 1)
+            minPowersOf2.push_back(pow(2, power));
+        ++power;
+        number = number / 2;
+    }
+    return minPowersOf2;
+}
