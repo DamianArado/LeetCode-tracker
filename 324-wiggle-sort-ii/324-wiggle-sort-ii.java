b64cd42394f982b03a3694fa24d5733724362ae0
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-11 16:13:18 +0530

    Time: 35 ms (33.92%), Space: 55.4 MB (71.24%) - LeetHub

---
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void wiggleSort(int[] nums) {
+        int n = nums.length;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int num : nums) minHeap.add(-1 * num);
+        for(int i = 1; i < n; i += 2) nums[i] = -1 * minHeap.poll();
+        for(int i = 0; i < n; i += 2) nums[i] = -1 * minHeap.poll();
+    }
+}
