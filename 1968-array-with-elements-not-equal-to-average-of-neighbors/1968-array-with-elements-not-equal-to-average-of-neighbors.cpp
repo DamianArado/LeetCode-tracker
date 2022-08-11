bc1791c11176fdbb8ddd93384aa5deaa2c2805cd
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-11 16:18:46 +0530

    Time: 481 ms (17.16%), Space: 126.8 MB (28.32%) - LeetHub

---
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        priority_queue<int> maxHeap(nums.begin(), nums.end());
+        for(int i = 1; i < n; i += 2) {
+            nums[i] = maxHeap.top();
+            maxHeap.pop();
+        }
+        for(int i = 0; i < n; i += 2) {
+            nums[i] = maxHeap.top();
+            maxHeap.pop();
+        }
+        return nums;
+    }
+};
