27499e3d49131ec97bbeadcfac6744ed0832e6bc
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-13 23:59:33 +0530

    Time: 1181 ms (19.04%), Space: 14.8 MB (20.83%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -3,7 +3,8 @@ class Solution:
         i = j = 0
         for j in range (0, len(nums)):
             if nums[j] == 0: k -= 1
-            if k < 0 and nums[i] == 1: i += 1
+            if k < 0 and nums[i] == 1: 
+                i += 1
             elif k < 0 and nums[i] == 0: 
                 i += 1
                 k += 1
