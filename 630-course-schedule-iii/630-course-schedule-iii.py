6a0368237fa342c33028434e9d08fbb90a144396
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-13 17:22:12 +0530

    Time: 746 ms (92.50%), Space: 19.9 MB (69.84%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.py
+++ 630-course-schedule-iii/630-course-schedule-iii.py
@@ -3,9 +3,9 @@ class Solution:
         heap, total = [], 0
         courses.sort(key = lambda x : x[1])
         for i in range (len(courses)):
-            heappush(heap, -1 * courses[i][0])
+            heappush(heap, -courses[i][0])
             total += courses[i][0]
             if total > courses[i][1]:
-                total -= -1 * heap[0]
+                total -= -heap[0]
                 heappop(heap)
         return len(heap)
