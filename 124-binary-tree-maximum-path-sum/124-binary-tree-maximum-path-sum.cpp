1b38590ab6c80fa72d4b2d8abb1441329b027f8e
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-16 14:07:48 +0530

    Time: 30 ms (83.74%), Space: 27.7 MB (39.55%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -11,17 +11,23 @@
  */
 class Solution {
 private:
-    int calculate(TreeNode* node, int &maxPSum) {
-        if(!node) return 0;
-        int leftSum = max(0, calculate(node->left, maxPSum)); // don't take negative path sum, instead take 0 in case of negative path sum
-        int rightSum = max(0, calculate(node->right, maxPSum));
-        maxPSum = max(maxPSum, leftSum + rightSum + node->val); // store the max path sum among all the nodes
-        return node->val + max(leftSum, rightSum); // to decide which path to take among the left and right as the longest path (maximum path sum from the current node)
+    int calculate(TreeNode* root, int& maxSum) {
+        // sum of the null node is always 0
+        if(!root) return 0;
+        // calculate the left and right path sum for the current node
+        // if the path sum is -ve, you'll not consider that part at all
+        int leftPathSum = max(0, calculate(root->left, maxSum));
+        int rightPathSum = max(0, calculate(root->right, maxSum));
+        // do the calculations to update the maxSum for the current node
+        maxSum = max(maxSum, root->val + leftPathSum + rightPathSum);
+        // return the max path calculation so that we can update the maxSum variable for the current node
+        return root->val + max(leftPathSum, rightPathSum);
     }
 public:
     int maxPathSum(TreeNode* root) {
-        int maxPSum = INT_MIN;
-        calculate(root, maxPSum);
-        return maxPSum;
+        int maxSum = INT_MIN;
+        calculate(root, maxSum);
+        // returning this since it has the max path sum stored in it
+        return maxSum;
     }
 };
