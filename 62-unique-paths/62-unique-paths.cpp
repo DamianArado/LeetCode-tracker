c65976a7294b0e5b61204866f869fda43484b05c
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-19 00:51:50 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (91.15%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -96,6 +96,7 @@ public:
         int R = min(m - 1, n - 1);
         double result = 1;
         // N c R
+        // number of different ways to choose m-1 down-moves and n-1 right-moves from a total of m+n-2 moves
         for(int i = 1; i <= R; ++i)
             result = result * (N - R + i) / i;
         return (int)result;
