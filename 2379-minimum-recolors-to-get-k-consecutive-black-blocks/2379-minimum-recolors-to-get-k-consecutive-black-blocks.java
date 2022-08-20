89e217f5be7ddfd4ebb56db771191e8726169e41
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-20 22:20:33 +0530

    Time: 1 ms (80.00%), Space: 40.4 MB (80.00%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -2,6 +2,12 @@ class Solution {
     public int minimumRecolors(String blocks, int k) {
         int n = blocks.length(), minOps = Integer.MAX_VALUE, flips = 0, count = 0, i = 0;
         for(int j = 0; j < n; ++j) {
+            if(blocks.charAt(j) == 'W') {
+                flips++;
+                count++;
+            } else if(blocks.charAt(j) == 'B') {
+                count++;
+            }
             if(count == k) {
                 minOps = Math.min(minOps, flips);
                 if(blocks.charAt(i) == 'W') {
@@ -9,15 +15,7 @@ class Solution {
                     count--;
                 } else count--;
                 i++;
-            } 
-            if(blocks.charAt(j) == 'W') {
-                flips++;
-                count++;
-            } else if(blocks.charAt(j) == 'B') {
-                count++;
             }
-            if(count == k) 
-                minOps = Math.min(minOps, flips);
         }
         return minOps;
     }
