5647e4dc07da8b063b1a2e81d4284d7d2910f76f
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-22 01:26:53 +0530

    Time: 80 ms (93.18%), Space: 14.9 MB (98.93%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.py
+++ 174-dungeon-game/174-dungeon-game.py
@@ -3,8 +3,6 @@ class Solution:
         row, col = len(dungeon), len(dungeon[0])
         hp = [1e9] * (col + 1)
         hp[col] = hp[col - 1] = 1
-        print(hp)
-        if row == 1 and col == 1 and dungeon[0][0] >= 0: return 1
         for i in range (row - 1, -1, -1):
             temp = [1e9] * (col + 1)
             for j in range (col - 1, -1, -1):
