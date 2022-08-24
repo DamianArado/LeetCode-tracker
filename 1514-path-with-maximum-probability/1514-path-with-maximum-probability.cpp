d2c9aabdb5af983f489182f486636e1e94019611
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-24 13:09:06 +0530

    Time: 218 ms (90.86%), Space: 66.2 MB (61.83%) - LeetHub

---
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -0,0 +1,33 @@
+typedef pair<int,double> pid;
+class Solution {
+public:
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+        // Step 1: Construct the graph
+        vector<vector<pair<int, double>>> graph(n);
+        for(int i = 0; i < edges.size(); ++i) {
+            graph[edges[i][0]].emplace_back(edges[i][1], succProb[i]);            
+            graph[edges[i][1]].emplace_back(edges[i][0], succProb[i]);
+        }
+        // Step 2: Use a modified Dijkstra's algorithm for this
+        priority_queue<pair<double, int>> maxHeap;
+        vector<double> maxProb(n);
+        
+        maxHeap.emplace(1.0, start);
+        maxProb[start] = 1.0;
+        
+        while(!maxHeap.empty()) {
+            auto current = maxHeap.top().second;
+            maxHeap.pop();
+            for(auto &adjNode : graph[current]) {
+                int nextNode = adjNode.first; 
+                double nextProb = adjNode.second;
+                if(maxProb[nextNode] < maxProb[current] * nextProb) {
+                    maxProb[nextNode] = maxProb[current] * nextProb;
+                    maxHeap.emplace(maxProb[nextNode], nextNode);
+                }
+            }
+        }
+        
+        return maxProb[end];
+    }
+};
