28ea43c65ad348c72a354857ef975456bb37e277
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-08-26 15:33:25 +0530

    Time: 34 ms (60.72%), Space: 21.2 MB (17.18%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -1,45 +1,51 @@
 /**
- So my approach followed basically these 2 bits of logic, doing a composition of a DFS based tree traversal and at each iteration checking if the very last value mins the targetSum were already spotted; I used a vector initially, then moved up to an array to keep track of the already seen values with a huge performance boost.
-
-In my approach I use 3 class variables:
-
-my typical accumulator variable res initialised to 0 to count the number of matching occurrences;
-sum that takes the values of targetSum and replaces it in all of my recursive calls (so that I do not have to pass it as a parameter)
-seen, as mentioned to keep track of the values I spotted so far and created to hold 1001 values (an initial 0 at root level, plus other potential 1000 different ones, as specified in the specs).
-Inside my main function I initialise the values and then, provided the tree is not empty (ie: !root), I call dfs.
-
-This function receives 3 parameters:
-
-a mandatory root, which is the node we are currently exploring;
-an optional partialSum value, defaulted to 0 and keeping track of our current partial sum going down a specific branch;
-an optional pos to tell us where to put out next value in seen and up to which point browse it (muuuch faster than expanding and reducing a vector, try the alternative code down below!)
-We are looking for all the occurrences of values in seen which are partialSum - sum, because it means that said value n plus partialSum would equal sum, and for each such instance we increase res by one.
-
-If we have a ->left branch we do the same with it, passing the update root, partialSum and pos (increased by 1) down as parameters; same thing with a ->right branch, if it exists.
-
-When we are done, we just return res
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
+/**
+
+Example: root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+
+           10[10]
+           /     \
+        5[15]   -3[7]
+       /   \         \
+     3[18]  2[17]    11[18]
+     /   \      \
+  3[21] -2[16]  1[18]
+  
+  10 - 8 = 2 (❎)         16 - 8 = 8 (❎)
+  15 - 8 = 7 (✅)         17 - 8 = 7 (❎)
+  18 - 8 = 10 (✅)        18 - 8 = 10 (✅)
+  21 - 8 = 13 (❎)        7 - 8 = -1 (❎)
+    
+Calculate the prefix sum at each node and store it inside a map
+At each node, check whether prefixSum - target is present inside the map
+which basically means that you have visited this path sum already in your current path
+
+*/
 class Solution {
-public:
-    int res = 0, sum;
-    int seen[1001];
-    void dfs(TreeNode* root, long partialSum = 0, int pos = 1) {
-        // updating partialSum with the current node
-        partialSum += root->val;
-        // checking if we already found an interval summing up to that
-        for (int i = 0; i < pos; i++) if (seen[i] == partialSum - sum) res++;
-        // updating seen
-        seen[pos] = partialSum;
-        // searching potential left and right branches
-        if (root->left) dfs(root->left, partialSum, pos + 1);
-        if (root->right) dfs(root->right, partialSum, pos + 1);
+private:
+    int helper(TreeNode *root, long prefixSum, int target, unordered_map<long, int> &store) {
+        if(!root) return 0;
+        prefixSum += root->val;
+        int ans = store[prefixSum - target];
+        store[prefixSum]++;
+        ans += helper(root->left, prefixSum, target, store) + helper(root->right, prefixSum, target, store);
+        store[prefixSum]--;
+        return ans;
     }
+public:
     int pathSum(TreeNode* root, int targetSum) {
-        sum = targetSum;
-        // seen set with initial value 0 will help us match from the root
-        seen[0] = 0;
-        if (!root) return res;
-        dfs(root);
-        return res;
+        unordered_map<long, int> store;
+        store[0] = 1;
+        return helper(root, 0, targetSum, store);
     }
 };
