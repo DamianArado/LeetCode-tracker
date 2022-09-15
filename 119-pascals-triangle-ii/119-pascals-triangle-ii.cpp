72d68cd7ccd656b6b36570e413907727bd190356
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-09-15 18:02:23 +0530

    Time: 5 ms (19.99%), Space: 6.5 MB (32.59%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> prev_row;
+        prev_row.emplace_back(1);
+        for(int level = 1; level <= rowIndex; ++level) {
+            vector<int> curr_row(level + 1);
+            curr_row[0] = curr_row[level] = 1;
+            for(int i = 1; i < level; ++i)
+                curr_row[i] = prev_row[i - 1] + prev_row[i];
+            prev_row = curr_row;
+        }
+        return prev_row;
+    }
+};
