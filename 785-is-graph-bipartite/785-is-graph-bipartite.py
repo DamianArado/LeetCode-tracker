af17b7da14ce941d5090bde834d50a66877640c8
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-09-21 17:46:22 +0530

    Time: 356 ms (34.57%), Space: 14.5 MB (51.21%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.py
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -1,20 +1,19 @@
 class Solution:
-    def bipartite(self, current: int, color: List[int], graph: List[List[int]]) -> bool:
+    def isBipartiteDFS(self, current: int, color: list, graph: List[List[int]]) -> bool:
         if color[current] == -1: 
             color[current] = 1
         for adjNode in graph[current]:
             if color[adjNode] == -1:
                 color[adjNode] = 1 - color[current]
-                if not self.bipartite(adjNode, color, graph):
+                if self.isBipartiteDFS(adjNode, color, graph) == False:
                     return False
             elif color[adjNode] == color[current]:
                 return False
         return True
-    
     def isBipartite(self, graph: List[List[int]]) -> bool:
         n = len(graph)
-        color = [-1] * (n)
+        color = [-1] * n
         for i in range(n):
-            if color[i] == -1 and not self.bipartite(i, color, graph):
+            if color[i] == -1 and self.isBipartiteDFS(i, color, graph) == False:
                 return False
         return True
