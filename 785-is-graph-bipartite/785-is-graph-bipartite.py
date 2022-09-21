cbd31d188961f71f9ecd2631b73b6243f109edce
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-09-21 18:05:22 +0530

    Time: 423 ms (18.11%), Space: 14.5 MB (39.08%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.py
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -1,19 +1,24 @@
+from queue import Queue
+
 class Solution:
-    def isBipartiteDFS(self, current: int, color: list, graph: List[List[int]]) -> bool:
-        if color[current] == -1: 
+    def isBipartiteBFS(self, current: int, color: list, graph: List[List[int]]) -> bool:
+        q = Queue()
+        if color[current] == -1:
             color[current] = 1
-        for adjNode in graph[current]:
-            if color[adjNode] == -1:
-                color[adjNode] = 1 - color[current]
-                if self.isBipartiteDFS(adjNode, color, graph) == False:
+        q.put(current)
+        while not q.empty():
+            current = q.get()
+            for adjNode in graph[current]:
+                if color[adjNode] == -1:
+                    color[adjNode] = 1 - color[current]
+                    q.put(adjNode)
+                elif color[adjNode] == color[current]:
                     return False
-            elif color[adjNode] == color[current]:
-                return False
         return True
     def isBipartite(self, graph: List[List[int]]) -> bool:
         n = len(graph)
         color = [-1] * n
         for i in range(n):
-            if color[i] == -1 and self.isBipartiteDFS(i, color, graph) == False:
+            if color[i] == -1 and not self.isBipartiteBFS(i, color, graph):
                 return False
         return True
