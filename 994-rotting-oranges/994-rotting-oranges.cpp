d900d2c29f82c5e8da109017ca830489eee162d1
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-09-25 18:45:48 +0530

    Time: 5 ms (90.04%), Space: 13.1 MB (50.89%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,46 +1,34 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        int oranges = 0;  // no. of valid cells (cells with orange)
-        int result = -1;  // no. of minutes to rot all the oranges
-        queue<pair<int,int>> q; // to store rotten oranges
-        vector<vector<int>> directions = {{-1,0},{1,0},{0,-1},{0,1}};
+        int row = size(grid), col = size(grid[0]), total = 0, count = 0, minutes = 0;
+        const int dx[4] = {-1, 1, 0, 0}, dy[4] = {0, 0, -1, 1};
+        queue<pair<int, int>> rotten;
         
-        for(int i = 0; i < m; ++i) {
-            for(int j = 0; j < n; ++j) {
-                // increase the count of oranges
-                if(grid[i][j] > 0) 
-                    oranges++;
-                // add only the cells having rotten oranges
-                if(grid[i][j] == 2)
-                    q.push({i,j});
+        for(int i = 0; i < row; ++i) {
+            for(int j = 0; j < col; ++j) {
+                if(grid[i][j] != 0) total++;
+                if(grid[i][j] == 2) rotten.emplace(i, j);
             }
         }
         
-        // start BFS
-        while(!q.empty()) {
-            result++;  // increase the no. of minutes for rotting oranges
-            int qSize = q.size();
-            for(int k = 0; k < qSize; ++k) {
-                auto it = q.front();
-                q.pop();
-                oranges--;  // decrease the no. of orange as it is rotten
-
-                // check for rotten oranges in all the 4 directions
+        while(!rotten.empty()) {
+            int sz = size(rotten);
+            count += sz;
+            for(int i = 0; i < sz; ++i) {
+                int x = rotten.front().first, y = rotten.front().second;
+                rotten.pop();
                 for(int i = 0; i < 4; ++i) {
-                    int x = it.first + directions[i][0];
-                    int y = it.second + directions[i][1];
-                    // skip if both go beyond the current limits
-                    if(x == m || y == n || x < 0 || y < 0 || grid[x][y] != 1)
+                    int dxi = x + dx[i], dxj = y + dy[i];
+                    if(dxi == -1 or dxi == row or dxj == -1 or dxj == col or grid[dxi][dxj] != 1)
                         continue;
-                    grid[x][y] = 2;
-                    q.push({x,y});  // add this rotten one to queue
+                    grid[dxi][dxj] = 2;
+                    rotten.emplace(dxi, dxj);
                 }
             }
+            if(!rotten.empty()) minutes++;
         }
         
-        // since all oranges have been rotten now, return the result
-        return (oranges == 0) ? max(0, result) : -1;
+        return count == total ? minutes : -1;
     }
 };
