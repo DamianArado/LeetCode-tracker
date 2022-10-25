d218892aef180f74773a1642fca69b7c3eef42bb
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-10-25 12:43:10 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (78.21%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string breakPalindrome(string palindrome) {
         int n = palindrome.length();
-        if(n < 2) return "";
+        if(n <= 1) return "";
         for(int i = 0; i < n / 2; ++i) {
             if(palindrome[i] > 'a') {
                 palindrome[i] = 'a';
