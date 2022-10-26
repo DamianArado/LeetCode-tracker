5c0af43e191c392da26c5cb107f00d6d143cebab
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-10-26 23:38:31 +0530

    Time: 0 ms (100.00%), Space: 7 MB (89.47%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,3 +1,65 @@
+/**
+
+Amortized O(1) push, pop & top operations - we will have 2 stacks input & output stack
+
+push - push inside input stack
+pop & top - 
+if output stack is not empty: output.pop()
+else transfer everything from input to output and then output.pop()
+empty - input is empty?
+
+*/
+class MyQueue {
+private:
+    stack<int> input, output;
+public:
+    MyQueue() {}
+    
+    void push(int x) {
+        input.emplace(x);
+    }
+    
+    int pop() {
+        int top = -1;
+        if(!output.empty()) {
+            top = output.top();
+            output.pop();
+            return top;
+        }
+        while(!input.empty()) {
+            output.emplace(input.top());
+            input.pop();
+        }
+        top = output.top();
+        output.pop();
+        return top;
+    }
+    
+    int peek() {
+        if(!output.empty()) return output.top();
+        while(!input.empty()) {
+            output.emplace(input.top());
+            input.pop();
+        }
+        return output.top();
+    }
+    
+    bool empty() {
+        return input.empty() and output.empty();
+    }
+};
+/**
+
+O(n) time & space
+
+You have 2 stacks: s1 and s2
+push - O(n)
+1. first transfer everything from s1 to s2
+2. push into s1
+3. transfer back everything from s2 to s1
+pop & top - return top of s1 - O(1)
+empty - check if s1 is empty - O(1)
+
 class MyQueue {
 private:
     stack<int> s1, s2;
@@ -31,6 +93,8 @@ public:
     }
 };
 
+
+*/
 /**
  * Your MyQueue object will be instantiated and called as such:
  * MyQueue* obj = new MyQueue();
