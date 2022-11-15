06caef4b7e65cb5bf16b8b6c2cbcd5eedc55f11c
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-11-15 10:11:13 +0530

    Time: 48 ms (24.41%), Space: 48.9 MB (27.53%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -1,36 +1,40 @@
 class Solution {
     public String shortestCommonSupersequence(String str1, String str2) {
-        int m = str1.length(), n = str2.length();
-        int[][] lcs = new int[m + 1][n + 1];
-        for(int i = 1; i <= m; ++i) {
-            for(int j = 1; j <= n; ++j) {
-                if(str1.charAt(i - 1) == str2.charAt(j - 1))
-                    lcs[i][j] = 1 + lcs[i - 1][j - 1];
-                else 
-                    lcs[i][j] = Math.max(lcs[i - 1][j], lcs[i][j - 1]);
+        int n1 = str1.length(), n2 = str2.length();
+        // Step1: a DP table constructed using LCS
+        int[][] dp = new int[n1 + 1][n2 + 1];
+        for(int i1 = 1; i1 <= n1; ++i1) {
+            for(int i2 = 1; i2 <= n2; ++i2) {
+                if(str1.charAt(i1 - 1) == str2.charAt(i2 - 1))
+                    dp[i1][i2] = 1 + dp[i1 - 1][i2 - 1];
+                else
+                    dp[i1][i2] = Math.max(dp[i1 - 1][i2], dp[i1][i2 - 1]);
             }
         }
-        int row = m, col = n;
+        
+        // Step2: using this table construct the SCS
+        int row = n1, col = n2;
         String scs = "";
         while(row > 0 && col > 0) {
             if(str1.charAt(row - 1) == str2.charAt(col - 1)) {
                 scs += str1.charAt(row - 1);
-                row--; col--;
-            } else if(lcs[row - 1][col] > lcs[row][col - 1]) {
-                scs += str1.charAt(row - 1);
-                row--;
-            } else {
+                --row;
+                --col;
+            } else if(dp[row][col - 1] > dp[row - 1][col]) {
                 scs += str2.charAt(col - 1);
-                col--;
+                --col;
+            } else {
+                scs += str1.charAt(row - 1);
+                --row;
             }
         }
         while(row > 0) {
             scs += str1.charAt(row - 1);
-            row--;
+            --row;
         }
         while(col > 0) {
             scs += str2.charAt(col - 1);
-            col--;
+            --col;
         }
         return new StringBuilder(scs).reverse().toString();
     }
