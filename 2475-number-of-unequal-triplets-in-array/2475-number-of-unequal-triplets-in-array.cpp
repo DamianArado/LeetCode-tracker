2dce5a587d01ea556ca40db2509bdc4db8776a2a
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-11-20 21:51:58 +0530

    Time: 3 ms (100.00%), Space: 8.6 MB (36.36%) - LeetHub

--- 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.cpp
+++ 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.cpp
@@ -1,15 +1,30 @@
+// class Solution {
+// public:
+//     int unequalTriplets(vector<int>& nums) {
+//         int n = size(nums), count = 0;
+//         for(int i = 0; i < n - 2; ++i) {
+//             for(int j = i + 1; j < n - 1; ++j) {
+//                 for(int k = j + 1; k < n; ++k) {
+//                     if(nums[i] != nums[j] && nums[i] != nums[k] && nums[j] != nums[k]) 
+//                         count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
     int unequalTriplets(vector<int>& nums) {
-        int n = size(nums), count = 0;
-        for(int i = 0; i < n - 2; ++i) {
-            for(int j = i + 1; j < n - 1; ++j) {
-                for(int k = j + 1; k < n; ++k) {
-                    if(nums[i] != nums[j] && nums[i] != nums[k] && nums[j] != nums[k]) 
-                        count++;
-                }
-            }
+        unordered_map<int, int> mp;
+        for(int &n : nums) mp[n]++;
+        int left = 0, right = size(nums), ans = 0;
+        for(auto &[n, cnt] : mp) {
+            right -= cnt;
+            ans += cnt * left * right;
+            left += cnt;
         }
-        return count;
+        return ans;
     }
 };
