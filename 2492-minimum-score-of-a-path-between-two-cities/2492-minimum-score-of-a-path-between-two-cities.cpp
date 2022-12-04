f1ea06ce351419694044f77e84484a32176f289a
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-12-04 11:36:38 +0530

    Time: 682 ms (76.19%), Space: 127.6 MB (76.19%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    int dfs(int current, vector<bool> &visited, vector<vector<pair<int, int>>> &graph) {
+        int minScore = INT_MAX;
+        visited[current] = true;
+        for(auto &[next, distance] : graph[current]) {
+            minScore = min(minScore, distance);
+            if(!visited[next]) {
+                visited[next] = true;
+                minScore = min(minScore, dfs(next, visited, graph));
+            }
+        }
+        return minScore;
+    }
+public:
+    int minScore(int n, vector<vector<int>>& roads) {
+        // Step 1: create a graph from the network of roads
+        vector<vector<pair<int, int>>> graph(n + 1);
+        for(auto &road : roads) {
+            graph[road[0]].emplace_back(road[1], road[2]);
+            graph[road[1]].emplace_back(road[0], road[2]);
+        }
+        // Step 2: Perform a DFS starting from the city 1 and since it will have an edge 
+        // connecting the last city, you are darn sure that you just need to track the min edge-weight
+        vector<bool> visited(n + 1);
+        return dfs(1, visited, graph);
+    }
+};
