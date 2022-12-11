26a2da9c5f7b9b931898ee6b8a74cd0453bfa25e
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-12-11 12:01:47 +0530

    Time: 467 ms (33.33%), Space: 65.2 MB (88.89%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.cpp
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -3,6 +3,7 @@ public:
     // we shouldn't miss any rock in our path
     // this problem then boils down to finding the max difference between alternate stones 
     // in odd path and even path
+    // since the array is sorted it will not be optimal to choose 2 consecutive stones
     int maxJump(vector<int>& stones) {
         int maxDiff = stones[1] - stones[0], n = size(stones);
         for(int i = 2; i < n; i += 2) maxDiff = max(maxDiff, stones[i] - stones[i - 2]);
