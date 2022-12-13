2df54b349a1cd782bb444fbc75306e57245f77ce
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-12-13 12:43:54 +0530

    Time: 14 ms (72.19%), Space: 41 MB (69.52%) - LeetHub

---
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countHousePlacements(int n) {
+        if(n == 1) return 4;
+        if(n == 2) return 9;
+        long pprev = 2, prev = 3, mod = 1000000007;
+        for(int i = 3; i <= n; ++i) {
+            long current = (prev + pprev) % (mod);
+            pprev = prev;
+            prev = current;
+        }
+        long res = (prev * prev) % (mod);
+        return (int) res;
+    }
+}
