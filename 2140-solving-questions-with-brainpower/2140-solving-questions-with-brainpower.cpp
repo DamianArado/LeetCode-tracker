ab3585cbe56283adb820254aabdd81a5fabcaa96
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-12-15 07:51:07 +0530

    Time: 364 ms (95.47%), Space: 115.3 MB (61.67%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -22,12 +22,12 @@ class Solution {
 public:
     long long mostPoints(vector<vector<int>>& questions) {
         int n = size(questions);
-        vector<long long> dp(n + 1);
+        vector<long long> dp(n);
         dp[n - 1] = questions[n - 1][0];
         for(int idx = n - 2; idx >= 0; --idx) {
             int point = questions[idx][0], bpower = questions[idx][1];
-            long long solve = questions[idx][0] + dp[min(idx + bpower + 1, n)];
-            long long skip = dp[idx + 1];
+            long long solve = questions[idx][0], skip = dp[idx + 1];
+            if(idx + bpower + 1 < n) solve += dp[idx + bpower + 1];
             dp[idx] = max(solve, skip);
         }
         return dp[0];
