8b5bab354b629da3f0ce718f3800aca00e56e43b
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-12-23 22:37:36 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (51.82%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        if(n == 0) return 0;
+        if(n == 1 || n == 2) return 1;
+        int frst = 0, scnd = 1;
+        for(int i = 2; i <= n; ++i) {
+            int ans = frst + scnd;
+            frst = scnd;
+            scnd = ans;
+        }
+        return scnd;
+    }
+}
