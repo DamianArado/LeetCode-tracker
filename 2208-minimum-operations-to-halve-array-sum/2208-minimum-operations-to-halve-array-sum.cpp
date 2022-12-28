c610feb94d6bc493cce0b0821a2975dd21f027cc
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-12-28 23:53:05 +0530

    Time: 271 ms (98.37%), Space: 80.9 MB (89.98%) - LeetHub

---
+++ 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int halveArray(vector<int>& nums) {
+        priority_queue<double> maxHeap(begin(nums), end(nums));
+        double operations = 0;
+        double sum = 0;
+        for(int &i : nums) sum += i;
+        if(size(nums) == sum) return sum;
+        double half = sum;
+        while(half > sum / 2) {
+            double max = maxHeap.top();
+            maxHeap.pop();
+            double reduced = max / 2;
+            half -= reduced;
+            maxHeap.emplace(reduced);
+            operations++;
+        }
+        return (int)operations;
+    }
+};
