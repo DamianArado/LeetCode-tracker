46dae0cc5f1f019365ca95bb69d9f29c23bb479c
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2022-12-29 23:39:30 +0530

    Time: 687 ms (82.27%), Space: 129.5 MB (51.59%) - LeetHub

---
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        int n = size(tasks);
+        if(n == 1) return {0};
+        // minHeap: [processingTime, index]
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
+        // [enqueueTime, processingTime, index]
+        for(int i = 0; i < n; ++i) tasks[i].emplace_back(i);
+        // sort on the basis of increasing enqueue times
+        sort(begin(tasks), end(tasks));
+        long idx = 0, endTime = 0;
+        vector<int> order;
+        while(idx < n or !minHeap.empty()) {
+            if(minHeap.empty())
+                endTime = max(endTime, (long) tasks[idx][0]);
+            while(idx < n and endTime >= tasks[idx][0]) {
+                minHeap.emplace(tasks[idx][1], tasks[idx][2]);
+                idx++;
+            }
+            auto [time, index] = minHeap.top();
+            minHeap.pop();
+            order.emplace_back(index);
+            endTime += time;
+        }
+        return order;
+    }
+};
