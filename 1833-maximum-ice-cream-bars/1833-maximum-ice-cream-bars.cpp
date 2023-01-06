92b6f6388f7caeefb184642ad19744e96bf4940b
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-01-06 12:49:48 +0530

    Time: 431 ms (29.71%), Space: 76.6 MB (7.43%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        if(size(costs) == 1) return (costs[0] <= coins ? 1 : 0);
         sort(begin(costs), end(costs));
         int iceCream = 0, i = 0;
-        while(coins >= costs[i] and i < size(costs)) {
+        while(i < size(costs) and coins >= costs[i]) {
             coins -= costs[i];
             ++i;
             ++iceCream;
