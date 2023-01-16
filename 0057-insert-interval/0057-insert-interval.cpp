999e0d8c691f99b814bcef5fb540ac69e5629b1e
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-01-16 13:26:35 +0530

    Time: 16 ms (83.48%), Space: 17.1 MB (45.20%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> result;
+        // edge case
+        if(intervals.size() == 0) {
+            result.push_back(newInterval);
+            return result;
+        }
+        // to keep a track of the intervals' indices so as to add the correct intervals in result
+        int index = 0;
+        // check if there is any independent interval before the newInterval
+        while(index < intervals.size() && intervals[index][1] < newInterval[0]) {
+            result.push_back(intervals[index++]);
+        }
+        // checking for overlapping between intervals : check if the start of intervals <= end of newInterval
+        while(index < intervals.size() && intervals[index][0] <= newInterval[1]) {
+            // now we'll change the newInterval according to the number of overlapped intervals with min and max of the overlapped intervals
+            newInterval[0] = min(intervals[index][0], newInterval[0]);
+            newInterval[1] = max(intervals[index][1], newInterval[1]);
+            // move on to check the next index in intervals
+            index++;
+        }
+        // push this overlapped interval in our resultant
+        result.push_back(newInterval);
+        // check if we have some elements more after newInterval region, adding them straightaway
+        while(index < intervals.size()) {
+            result.push_back(intervals[index++]);
+        }
+        return result;
+    }
+};
