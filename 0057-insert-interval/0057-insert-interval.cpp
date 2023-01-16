aac254f4ec1a26a46e7199480bb4e8923b1a12c6
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-01-16 13:59:21 +0530

    Time: 21 ms (66.96%), Space: 16.9 MB (92.65%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -3,18 +3,17 @@ public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> result;
         // edge case
-        if(intervals.size() == 0) {
-            result.push_back(newInterval);
+        if(size(intervals) == 0) {
+            result.emplace_back(newInterval);
             return result;
         }
         // to keep a track of the intervals' indices so as to add the correct intervals in result
         int index = 0;
         // check if there is any independent interval before the newInterval
-        while(index < intervals.size() && intervals[index][1] < newInterval[0]) {
-            result.push_back(intervals[index++]);
-        }
+        while(index < intervals.size() and intervals[index][1] < newInterval[0])
+            result.emplace_back(intervals[index++]);
         // checking for overlapping between intervals : check if the start of intervals <= end of newInterval
-        while(index < intervals.size() && intervals[index][0] <= newInterval[1]) {
+        while(index < intervals.size() and intervals[index][0] <= newInterval[1]) {
             // now we'll change the newInterval according to the number of overlapped intervals with min and max of the overlapped intervals
             newInterval[0] = min(intervals[index][0], newInterval[0]);
             newInterval[1] = max(intervals[index][1], newInterval[1]);
@@ -22,11 +21,10 @@ public:
             index++;
         }
         // push this overlapped interval in our resultant
-        result.push_back(newInterval);
+        result.emplace_back(newInterval);
         // check if we have some elements more after newInterval region, adding them straightaway
-        while(index < intervals.size()) {
-            result.push_back(intervals[index++]);
-        }
+        while(index < intervals.size())
+            result.emplace_back(intervals[index++]);
         return result;
     }
 };
