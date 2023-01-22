442647c5c7a311d12c3ce1acf5a2a97851211835
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-01-22 22:35:35 +0530

    Time: 30 ms (62.50%), Space: 13 MB (12.50%) - LeetHub

--- 2546-apply-bitwise-operations-to-make-strings-equal/2546-apply-bitwise-operations-to-make-strings-equal.cpp
+++ 2546-apply-bitwise-operations-to-make-strings-equal/2546-apply-bitwise-operations-to-make-strings-equal.cpp
@@ -6,6 +6,7 @@ public:
         // (0, 1) -> (1, 1)
         // (1, 1) -> (1, 0)
         // if it has a 1 it can flip it's value as 0 cannot change
+        // so both should have atleast a single 1 in order to do the transformation
         return (s.find('1') != string::npos) == (t.find('1') != string::npos);
     }
 };
