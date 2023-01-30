3226e656b40e35e4a3749a1cfc0ee327f1919059
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-01-30 23:25:15 +0530

    Time: 602 ms (17.44%), Space: 189 MB (6.02%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,62 +1,108 @@
+// Hardcoded node - maybe a better thing during interviews!
+
+// class LRUCache {
+// private:
+//     class node {
+//         public:
+//             int key, value;
+//             node *prev, *next;
+//             node(int key, int value) {
+//                 this->key = key;
+//                 this->value = value;
+//             }
+//     };
+//     node *head = new node(-1, -1), *tail = new node(-1, -1);
+//     int capacity;
+//     unordered_map<int, node*> m;
+// public:
+//     LRUCache(int capacity) {
+//         this->capacity = capacity;
+//         head->next = tail;
+//         tail->prev = head;
+//     }
+    
+//     void addNode(node *newNode) {
+//         node *temp = head->next;
+//         newNode->next = temp;
+//         newNode->prev = head;
+//         head->next = newNode;
+//         temp->prev = newNode;
+//     }
+    
+//     void deleteNode(node *deletedNode) {
+//         node *delPrev = deletedNode->prev, *delNext = deletedNode->next;
+//         delPrev->next = delNext;
+//         delNext->prev = delPrev;
+//         //delete(deletedNode);
+//     }
+    
+//     int get(int key) {
+//         if(!m.count(key)) return -1;
+//         node *valNode = m[key];
+//         int val = valNode->value;
+//         m.erase(key);
+//         deleteNode(valNode);
+//         addNode(valNode);
+//         m[key] = head->next;
+//         return val;
+//     }
+    
+//     void put(int key, int value) {
+//         if(m.count(key)) {
+//             node *existingNode = m[key];
+//             m.erase(key);
+//             deleteNode(existingNode);
+//         }
+//         if(size(m) == this->capacity) {
+//             m.erase(tail->prev->key);
+//             deleteNode(tail->prev);
+//         }
+//         addNode(new node(key, value));
+//         m[key] = head->next;
+//     }
+// };
+
 class LRUCache {
 private:
-    class node {
-        public:
-            int key, value;
-            node *prev, *next;
-            node(int key, int value) {
-                this->key = key;
-                this->value = value;
-            }
-    };
-    node *head = new node(-1, -1), *tail = new node(-1, -1);
     int capacity;
-    unordered_map<int, node*> m;
+    list<int> dll; 
+    unordered_map<int, int> keyValue; // key : value
+    unordered_map<int, list<int>::iterator> keyAddr; // key : address of key
 public:
     LRUCache(int capacity) {
         this->capacity = capacity;
-        head->next = tail;
-        tail->prev = head;
-    }
-    
-    void addNode(node *newNode) {
-        node *temp = head->next;
-        newNode->next = temp;
-        newNode->prev = head;
-        head->next = newNode;
-        temp->prev = newNode;
-    }
-    
-    void deleteNode(node *deletedNode) {
-        node *delPrev = deletedNode->prev, *delNext = deletedNode->next;
-        delPrev->next = delNext;
-        delNext->prev = delPrev;
-        //delete(deletedNode);
     }
     
     int get(int key) {
-        if(!m.count(key)) return -1;
-        node *valNode = m[key];
-        int val = valNode->value;
-        m.erase(key);
-        deleteNode(valNode);
-        addNode(valNode);
-        m[key] = head->next;
-        return val;
+        if(!keyValue.count(key)) return -1;
+        
+        int value = keyValue[key];
+        auto node = keyAddr[key];
+        dll.erase(node);
+        keyAddr.erase(key);
+        
+        dll.emplace_front(key);
+        keyAddr[key] = begin(dll);
+        
+        return value;
     }
     
     void put(int key, int value) {
-        if(m.count(key)) {
-            node *existingNode = m[key];
-            m.erase(key);
-            deleteNode(existingNode);
+        if(keyValue.count(key)) {
+            auto node = keyAddr[key];
+            dll.erase(node);
+            keyAddr.erase(key);
+            keyValue.erase(key);
         }
-        if(size(m) == this->capacity) {
-            m.erase(tail->prev->key);
-            deleteNode(tail->prev);
+        if(size(keyValue) == this->capacity) {
+            int lru = dll.back();
+            keyAddr.erase(lru);
+            keyValue.erase(lru);
+            dll.pop_back();
         }
-        addNode(new node(key, value));
-        m[key] = head->next;
+        dll.emplace_front(key);
+        keyAddr[key] = begin(dll);
+        keyValue[key] = value;
     }
 };
 
