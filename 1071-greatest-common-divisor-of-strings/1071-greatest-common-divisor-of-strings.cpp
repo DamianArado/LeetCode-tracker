2c25e39641a80e4a72f327e3e20de626990d4b86
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-02-01 13:18:47 +0530

    Time: 4 ms (61.25%), Space: 8.6 MB (16.79%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     string gcdOfStrings(string s1, string s2) {
-        if (s1.size() < s2.size()) return gcdOfStrings(s2, s1);
-        if (s2.empty()) return s1;
-        if (s1.substr(0, s2.size()) != s2) return "";
-        return gcdOfStrings(s1.substr(s2.size()), s2);
+        if(s1.size() < s2.size()) return gcdOfStrings(s2, s1); 
+        if(s2.empty()) return s1;  // got the required GCD
+        if(s1.substr(0, s2.size()) != s2) return "";  // can't be a GCD
+        return gcdOfStrings(s1.substr(s2.size()), s2);  // remove the s2.size() chars from front of s1
     }
 };
