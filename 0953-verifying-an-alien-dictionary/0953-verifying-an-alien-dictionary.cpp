1dd905f9c27748b17d822bffb5bda1693cc58bc4
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-02-02 13:41:41 +0530

    Time: 3 ms (93.87%), Space: 9.7 MB (49.27%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,24 @@
+/*
+    Explanation
+    Build a transform mapping from order,
+    Find all alien words with letters in normal order.
+
+    For example, if we have order = "xyz..."
+    We can map the word "xyz" to "abc" or "123"
+
+    Then we check if all words are in sorted order.
+
+    Complexity: Time O(NS), Space O(1)
+*/
+class Solution {
+public:
+    bool isAlienSorted(vector<string> words, string order) {
+        int mapping[26];
+        for(int i = 0; i < 26; ++i)
+            mapping[order[i] - 'a'] = i;
+        for(string &word : words)
+            for(char &c : word) 
+                c = mapping[c - 'a'];
+        return is_sorted(words.begin(), words.end());
+    }
+};
