2e778b0801dca72e6fb232c19b86c2dfc2d9a038
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-02-07 18:51:17 +0530

    Time: 7 ms (80.46%), Space: 7.4 MB (56.71%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,31 +1,17 @@
 class Solution {
-    bool areVectorsEqual(vector<int> a, vector<int> b){
-        for(int i=0; i<26; i++){
-            if(a[i]!=b[i]) return false;
-        }
-        return true;
-    }
 public:
     bool checkInclusion(string s1, string s2) {
-        if(s2.size()<s1.size()) return false;
-        vector<int> freqS1(26, 0);
-        for(char c: s1) freqS1[c-'a']++;
-        
-        vector<int> freqS2(26, 0);
-        int i=0, j=0;
-        
-        while(j<s2.size()){
-            freqS2[s2[j]-'a']++;
-            
-            if(j-i+1==s1.size()){
-                if(areVectorsEqual(freqS1, freqS2)) return true;
-            }
-            
-            if(j-i+1<s1.size()) j++;
-            else{
-                freqS2[s2[i]-'a']--;
-                i++;
-                j++;
+        vector<int> freqS1(26), freqS2(26);
+        for(const char &ch : s1) ++freqS1[ch - 'a'];
+        int n = size(s2), i = 0;
+        for(int j = 0; j < n; ++j) {
+            int windowSize = j - i + 1;
+            ++freqS2[s2[j] - 'a'];
+            if(windowSize == size(s1) and freqS1 == freqS2)
+                return true;
+            if(windowSize >= size(s1)) {
+                --freqS2[s2[i] - 'a'];
+                ++i;
             }
         }
         return false;
