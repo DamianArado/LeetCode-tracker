50472417e62429cfbc3cfa6a19a7b81301de5783
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-02-08 23:37:00 +0530

    Time: 253 ms (58.26%), Space: 52.8 MB (28.37%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,27 +1,31 @@
+// Similar to Word Break I
 class Solution {
-public:
-    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-        unordered_set<string> words_set;
-        for (string word : words) words_set.insert(word);
-        vector<string> res;
-    
-        for (string word : words) {
-            int n = word.size();
-            vector<int> dp(n + 1, 0);
-            dp[0] = 1;
-            for (int i = 0; i < n; i++) {
-                if (!dp[i]) continue;
-                for (int j = i + 1; j <= n; j++) {
-                    if (j - i < n && words_set.count(word.substr(i, j - i))) {
-                        dp[j] = 1;
-                    }
-                }
-                if (dp[n]) {
-                    res.push_back(word);
-                    break;
-                }
-            }
+private:
+    bool f(int idx, string &word, unordered_set<string> &wordSet, vector<int> &dp) {
+        if(wordSet.empty()) return false;
+        if(idx == size(word)) return true;
+        if(dp[idx] != -1) return dp[idx];
+        string temp;
+        for(int i = idx; i < size(word); ++i) {
+            temp += word[i];
+            if(wordSet.count(temp) and f(i + 1, word, wordSet, dp))
+                return dp[idx] = true;
         }
-        return res;
+        return dp[idx] = false;
+    }
+public:
+    // TC: O(n*30^3)
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        vector<string> ans;
+        unordered_set<string> wordSet;
+        sort(begin(words), end(words), [&](auto x, auto y) { return size(x) < size(y); });
+        for(string &word : words) {
+            // for each word check if it is made by the concatenation of atleast 2 words
+            vector<int> dp(size(word), -1);
+            if(f(0, word, wordSet, dp))
+                ans.emplace_back(word);
+            wordSet.insert(word);
+        }
+        return ans;
     }
 };
