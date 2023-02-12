d22fe6e9acecfa7cf386d3a3d4d65cc6eecc5d2f
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-02-12 16:40:05 +0530

    Time: 20 ms (76.18%), Space: 15 MB (69.91%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
     vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
-        vector<vector<pair<int,int>>> adj(n);
-       
-       for(auto &it : redEdges){
-         adj[it[0]].push_back({it[1],0});
-       }
-       
-       for(auto &it : blueEdges){
-         adj[it[0]].push_back({it[1],1});
-       }
-       
-       vector<int> ans(n,-1);
-       vector<vector<bool>> visited(n, vector<bool>(2));
-       queue<vector<int>> q; 
-       
-       q.push({0,0,-1}); // node, step, color
-       visited[0][0]=true;
-       visited[0][1]=true;
-       ans[0]=0;
-       
-       while(!q.empty()){
-         vector<int> curr=q.front(); q.pop();
-         int node=curr[0],step=curr[1],prevcolor=curr[2];
-         
-         for(auto [nbr,color]: adj[node]){
-           if(!visited[nbr][color]==true && color!=prevcolor){
-             visited[nbr][color]=true;
-             q.push({nbr,1+step,color});  
-             if(ans[nbr]==-1){
-             ans[nbr]=1+step;
-             }       
-           }         
-         }    
-       }
-       return ans; 
+        // Step1: Create a graph of type node - (adjNode, edgeColor)
+        vector<vector<pair<int, int>>> graph(n);
+        for(auto &e : redEdges) graph[e[0]].emplace_back(e[1], 0);
+        for(auto &e : blueEdges) graph[e[0]].emplace_back(e[1], 1);
+        
+        // Step2: Perform a BFS traversal for shortest distance to each node
+        vector<int> distance(n, -1);
+        distance[0] = 0;
+        vector<vector<bool>> visited(n, vector<bool> (2));  // <node, edgeColor>
+        visited[0][0] = visited[0][1] = true;
+        queue<tuple<int, int, int>> q;
+        q.emplace(0, 0, -1);  // <node, distance, color
+        
+        while(!q.empty()) {
+            auto [currNode, currDist, currColor] = q.front();
+            q.pop();
+            for(auto &[nextNode, nextColor] : graph[currNode]) {
+                if(!visited[nextNode][nextColor] and nextColor != currColor) {
+                    visited[nextNode][nextColor] = true;
+                    q.emplace(nextNode, currDist + 1, nextColor);
+                    if(distance[nextNode] == -1)
+                        distance[nextNode] = currDist + 1;
+                }
+            }  
+        }
+        return distance;
     }
 };
