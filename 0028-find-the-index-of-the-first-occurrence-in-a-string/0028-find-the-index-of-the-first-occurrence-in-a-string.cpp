5cf7b3cf9f859d377b74a0d3c5af4f15df756427
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-03-03 21:57:13 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (11.62%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,6 +1,7 @@
+/**
 class Solution {
 public:
-    // brute force
+    // brute force - O(n1.n2) TC & O(1) SC
     int strStr(string haystack, string needle) {
         int n1 = size(haystack), n2 = size(needle);
         for(int i = 0; i <= n1 - n2; ++i) {
@@ -9,6 +10,40 @@ public:
                 if(haystack[i + j] != needle[j])
                     break;
             if(j == n2) return i;
+        }
+        return -1;
+    }
+};
+*/
+class Solution {
+private:
+    vector<int> constructLPS(string &needle) {
+        int n = size(needle);
+        vector<int> lps(n);
+        int i = 0, j = 1;
+        while(j < n) {
+            if(needle[i] == needle[j]) {
+                lps[j] = ++i, ++j;
+            }
+            else if(i == 0)
+                lps[j] = 0, ++j;
+            else 
+                i = lps[i - 1];
+        }
+        return lps;
+    }
+public:
+    // KMP - O(n1 + n2) TC & O() SC
+    int strStr(string haystack, string needle) {
+        int n1 = size(haystack), n2 = size(needle);
+        vector<int> lps = constructLPS(needle);
+        for(int i = 0, j = 0; i < n1; ) {
+            if(haystack[i] == needle[j])
+                ++i, ++j;
+            if(j == n2) 
+                return i - j;
+            if(haystack[i] != needle[j])
+                j ? j = lps[j - 1] : ++i;
         }
         return -1;
     }
