aee136297c0826a164ff51c717a4986f0f942ced
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-03-04 22:49:20 +0530

    Time: 355 ms (50.00%), Space: 72.7 MB (100.00%) - LeetHub

---
+++ 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    const int MOD = 1e9 + 7;
+public:
+    int power(int n) {
+        if(n == 1) return 2;
+        long long ans= power(n / 2);
+        ans = (ans * ans) % MOD;
+        if(n % 2 == 1) ans = (ans * 2) % MOD;
+        return ans;
+    }
+    int countWays(vector<vector<int>>& ranges) {
+        sort(begin(ranges), end(ranges));
+        int count = 1, lastMax = ranges[0][1];
+        // merge intervals
+        for(int i = 1; i < size(ranges); ++i) {
+            int currMin = ranges[i][0];
+            if(currMin > lastMax) ++count;
+            lastMax = max(lastMax, ranges[i][1]);
+        }
+        return power(count);
+        
+    }
+};
