e2afb37f0947d0a6fd046a4028ea9f3f76b66609
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-03-05 11:50:35 +0530

    Time: 598 ms (100.00%), Space: 14.5 MB (33.33%) - LeetHub

--- 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.cpp
+++ 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.cpp
@@ -1,13 +1,12 @@
 class Solution { 
 private:
-    const int MOD = 1e9 + 7; 
-public: 
+    const int MOD = 1e9 + 7;
     int solve(vector<vector<int>>& types, int i, int marks, int target, vector<vector<int>> &dp) { 
         if(marks > target) 
             return 0; 
         if(marks == target) 
             return 1; 
-        if(i == types.size()) 
+        if(i == size(types)) 
             return 0; 
         if(dp[i][marks] != -1) 
             return dp[i][marks]; 
@@ -17,9 +16,10 @@ public:
             ans %= MOD; 
         } 
         return dp[i][marks] = ans; 
-    } 
+    }
+public:  
     int waysToReachTarget(int target, vector<vector<int>>& types) { 
-        vector<vector<int>> dp(types.size(), vector<int>(target + 1, -1)); 
+        vector<vector<int>> dp(size(types), vector<int>(target + 1, -1)); 
         return solve(types, 0, 0, target, dp); 
     } 
 };
