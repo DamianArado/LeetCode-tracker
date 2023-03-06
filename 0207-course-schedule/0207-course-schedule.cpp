a7e1604bf09811ba3ad827f3b655a6034a8db058
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-03-06 17:46:28 +0530

    Time: 29 ms (36.46%), Space: 14 MB (51.61%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    bool isCycleDFS(int current, vector<int> adjList[], vector<bool> &visited, vector<bool> &dfsVisit) {
+    bool isCycleDFS(int current, vector<vector<int>> &adjList, vector<bool> &visited, vector<bool> &dfsVisit) {
         visited[current] = dfsVisit[current] = true;
         for(int &adjNode : adjList[current]) {
             if(!visited[adjNode] and isCycleDFS(adjNode, adjList, visited, dfsVisit))
@@ -15,10 +15,10 @@ public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         // We cannot finish all courses if we found a cycle in our graph
         // for that let's first create our graph
-        vector<int> adjList[numCourses];
+        vector<vector<int>> adjList(numCourses);
         vector<bool> visited(numCourses), dfsVisit(numCourses);
-        for(auto &prereq : prerequisites)
-            adjList[prereq[0]].push_back(prereq[1]);
+        for(const auto &prereq : prerequisites)
+            adjList[prereq[0]].emplace_back(prereq[1]);
         // Lets use DFS to check whether or not there exists a cycle in our graph
         for(int i = 0; i < numCourses; ++i) {
             if(!visited[i] and isCycleDFS(i, adjList, visited, dfsVisit))
