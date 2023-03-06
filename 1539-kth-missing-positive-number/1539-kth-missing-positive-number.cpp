2219f0d2a7d571fbcd7a56fb68e54c20170122a9
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-03-06 16:47:38 +0530

    Time: 10 ms (27.76%), Space: 9.5 MB (73.33%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
+    // int findKthPositive(vector<int>& arr, int k) {
+    //     // brute force
+    //     unordered_map<int, int> mp;
+    //     for(int &i : arr) mp[i]++;
+    //     int current = 1;
+    //     while(k > 0) {
+    //         if(!mp.count(current)) --k;
+    //         ++current;
+    //     }
+    //     return current - 1;
+    // }
     int findKthPositive(vector<int>& arr, int k) {
-        // brute force
-        unordered_map<int, int> mp;
-        for(int &i : arr) mp[i]++;
-        int current = 1;
-        while(k > 0) {
-            if(!mp.count(current)) --k;
-            ++current;
+        // find the 1st index where the no. of missing elements is greater than k
+        // we are virtually considering an array which stores the no. of missing elements till an index i
+        // element in each index: vArr[i] = arr[i] - i - 1
+        int start = 0, end = size(arr); // end till last index + 1 coz it may happen that we didnt have any missing no. till last index in array
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+            if(arr[mid] - mid - 1 < k)
+                start = mid + 1;
+            else 
+                end = mid;
         }
-        return current - 1;
+        // Answer = arr[end] - (vArr[end] - k + 1) = end + k 
+        return end + k;
     }
 };
