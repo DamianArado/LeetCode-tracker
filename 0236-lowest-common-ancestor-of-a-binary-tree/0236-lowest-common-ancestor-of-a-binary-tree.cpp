3e7a28f5ba7673c63cbd0ca42f23439303580fd8
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-03-06 12:31:50 +0530

    Time: 17 ms (64.95%), Space: 14.1 MB (98.05%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,12 +7,21 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// Code for finding ancestor of target in a binary tree
+// TreeNode *findAncestor(TreeNode* root, int target) {
+//     if(root == NULL) return NULL;
+//     if((root->left != NULL and root->left->val == target) or (root->right != NULL and root->right->val == target)) return root;
+//     TreeNode *left = findAncestor(root->left, target);
+//     TreeNode *right = findAncestor(root->right, target);
+//     return left != NULL ? left : right;
+// }
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root == nullptr or root == p or root == q) return root;
+        if(root == NULL or root == p or root == q) return root;
         TreeNode *left = lowestCommonAncestor(root->left, p, q);
         TreeNode *right = lowestCommonAncestor(root->right, p, q);
-        return left and right ? root : (left ? left : right);
+        return left != NULL and right != NULL ? root : (left != NULL ? left : right);
     }
 };
