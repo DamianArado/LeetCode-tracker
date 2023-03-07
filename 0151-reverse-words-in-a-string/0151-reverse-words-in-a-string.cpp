8a29baaa473c09cf871123ccf9c5868766729a9b
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-03-07 10:43:25 +0530

    Time: 7 ms (55.70%), Space: 6.8 MB (99.32%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,42 +1,19 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n = s.length();
-        // step 1: remove the leading and trailing white spaces
-        int i = 0;
-        for(i = 0; i < n; ++i)
-            if(s[i] != ' ') 
-                break;
-        s = s.substr(i, n);
-        
-        n = s.length();
-        for(i = n - 1; i >= 0; --i)
-            if(s[i] != ' ')
-                break;
-        s = s.substr(0, i + 1);
-        n = s.length();
-        
-        // step 2: reverse the complete string
         reverse(begin(s), end(s));
-        
-        cout <<s;
-        
-        // step 3: reverse the words inside the string
-        string ans = "";
-        string word = "";
-        for(i = 0; i < n; ++i) {
-            if(s[i] == ' ') {
-                reverse(begin(word), end(word));
-                ans = ans + (word == "" ? word + "" : word + " ");
-                word = "";
-                continue;
+        int i = 0, j = 0, n = size(s), lastIdx = 0;
+        while(j < n) {
+            while(j < n and s[j] == ' ') ++j;
+            int startIdx = i;
+            while(j < n and s[j] != ' ') {
+                s[i++] = s[j++];
+                lastIdx = i;
             }
-            word += s[i];
+            s[i++] = ' ';
+            reverse(begin(s) + startIdx, begin(s) + lastIdx);
         }
-        reverse(begin(word), end(word));
-        ans = ans + (word == "" ? word + "" : word + " ");
-        n  = ans.length();
-        ans = ans.substr(0, n - 1);
-        return ans;
+        s.resize(lastIdx);
+        return s;
     }
 };
