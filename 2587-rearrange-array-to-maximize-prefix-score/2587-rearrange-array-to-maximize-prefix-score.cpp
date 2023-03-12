7bbf3cdf9908b1566479ebf306c948dfc49886d9
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-03-12 12:03:04 +0530

    Time: 160 ms (100.00%), Space: 83.3 MB (25.00%) - LeetHub

---
+++ 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.cpp
@@ -0,0 +1,28 @@
+// Brute force: O(n) tc & O(n) sc
+// class Solution {
+// public:
+//     int maxScore(vector<int>& nums) {
+//         vector<double> aux(size(nums));
+//         sort(begin(nums), end(nums));
+//         reverse(begin(nums), end(nums));
+//         aux[0] = nums[0];
+//         for(int i = 1; i < size(nums); ++i) 
+//             aux[i] = aux[i - 1] + nums[i];
+//         int count = 0;
+//         for(double &i : aux) if(i > 0) ++count;
+//         return count;
+//     }
+// };
+class Solution {
+public:
+    int maxScore(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+        int count = 0, n = size(nums);
+        double sum = 0;
+        for(int i = n - 1; i >= 0; --i) {
+            sum += nums[i];
+            if(sum > 0) ++count;
+        }
+        return count;
+    }
+};
