e2663a4bc49c4bd7cead60f7bb52d0a78fec7fea
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-03-19 15:19:55 +0530

    Time: 20 ms (61.40%), Space: 26.5 MB (23.76%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -9,23 +9,26 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// Given an inorder and a postorder traversal of a binary tree, the binary tree that is generated from it will be unique
 class Solution {
 private:
-    TreeNode* buildTree(vector<int> &inorder, int inStart, int inEnd, vector<int> &postorder, int postStart, int postEnd, map<int, int> &inMappings) {
-        if(inStart > inEnd || postStart > postEnd) return NULL; // base case
-        TreeNode* root = new TreeNode(postorder[postEnd]); // create the root node of this tree
-        int inRoot = inMappings[root->val]; // index at which the root node is present
-        int numsLeft = inRoot - inStart; // number of nodes to the left of root node
-        // attach the left and right subtrees using recursion
-        root->left = buildTree(inorder, inStart, inRoot - 1, postorder, postStart, postStart + numsLeft - 1, inMappings);
-        root->right = buildTree(inorder, inRoot + 1, inEnd, postorder, postStart + numsLeft, postEnd - 1, inMappings);
+    TreeNode *buildTree(vector<int> &inorder, int inStart, int inEnd, vector<int> &postorder, int postStart,
+                       int postEnd, unordered_map<int, int> &inMappings) {
+        if(inStart > inEnd or postStart > postEnd) return NULL;
+        TreeNode *root = new TreeNode(postorder[postEnd]);
+        int inorderRoot = inMappings[root->val];
+        int numTowardsLeft = inorderRoot - inStart;
+        root->left = buildTree(inorder, inStart, inorderRoot - 1, postorder, 
+                               postStart, postStart + numTowardsLeft - 1, inMappings);
+        root->right = buildTree(inorder, inorderRoot + 1, inEnd, 
+                                postorder, postStart + numTowardsLeft, postEnd - 1, inMappings);
         return root;
     }
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        map<int, int> inMappings; // [node.val : index]
-        for(int i = 0; i < inorder.size(); ++i) inMappings[inorder[i]] = i;
-        return buildTree(inorder, 0, inorder.size() - 1, postorder, 0, postorder.size() - 1, inMappings);
+        int n1 = size(inorder), n2 = size(postorder);
+        unordered_map<int, int> inMappings;
+        for(int i = 0; i < n1; ++i)
+            inMappings[inorder[i]] = i;  // help us to track in inorder 
+        return buildTree(inorder, 0, n1 - 1, postorder, 0, n2 - 1, inMappings);
     }
 };
