4dbfdbcf4fb4c8b3349bd8ec21fad347a2863b49
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-03-19 20:46:34 +0530

    Time: 54 ms (39.38%), Space: 19 MB (51.61%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,16 +1,24 @@
 class Solution {
-public:
-    int minEatingSpeed(vector<int>& piles, int H) {
-        int l = 1, r = 1e9;
-        while (l < r) {
-            int m = (l + r) / 2, total = 0;
-            for (int p : piles)
-                total += (p + m - 1) / m;
-            if (total > H)
-                l = m + 1;
-            else
-                r = m;
+private:
+    bool canEat(vector<int> &piles, int expectedHrs, int k) {
+        long long calculatedHrs = 0;
+        for(const int &pile : piles) {
+            calculatedHrs += pile / k;
+            if(pile % k != 0) ++calculatedHrs;
         }
-        return l;
+        return calculatedHrs <= expectedHrs;
+    }
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        // k can lie in the range: [1, max(piles[i])]
+        // we can use binary search to check whether or not we can eat all bananas within h hours
+        int left = 1, right = 0;
+        for(const int &pile : piles) right = max(right, pile);
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(canEat(piles, h, mid)) right = mid - 1;
+            else left = mid + 1;
+        }
+        return left;
     }
 };
