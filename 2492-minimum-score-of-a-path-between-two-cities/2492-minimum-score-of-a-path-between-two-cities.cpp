258cf43025bf5f50cd0c39915ee4276d239b406a
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-03-22 11:38:57 +0530

    Time: 441 ms (88.62%), Space: 127.8 MB (64.62%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -3,7 +3,7 @@ private:
     int dfs(int current, vector<bool> &visited, vector<vector<pair<int, int>>> &graph) {
         int minScore = INT_MAX;
         visited[current] = true;
-        for(auto &[next, distance] : graph[current]) {
+        for(const auto &[next, distance] : graph[current]) {
             minScore = min(minScore, distance);
             if(!visited[next]) {
                 visited[next] = true;
@@ -16,12 +16,13 @@ public:
     int minScore(int n, vector<vector<int>>& roads) {
         // Step 1: create a graph from the network of roads
         vector<vector<pair<int, int>>> graph(n + 1);
-        for(auto &road : roads) {
+        for(const auto &road : roads) {
             graph[road[0]].emplace_back(road[1], road[2]);
             graph[road[1]].emplace_back(road[0], road[2]);
         }
         // Step 2: Perform a DFS starting from the city 1 and since it will have an edge 
         // connecting the last city, you are darn sure that you just need to track the min edge-weight
+        // inside the component connecting city 1 and city n
         vector<bool> visited(n + 1);
         return dfs(1, visited, graph);
     }
