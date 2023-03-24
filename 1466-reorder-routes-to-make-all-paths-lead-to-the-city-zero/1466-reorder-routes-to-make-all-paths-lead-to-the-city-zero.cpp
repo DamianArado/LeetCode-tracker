4ee6ae8426e208feb5d27d6f58ce4f11e8a4c9c1
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-03-24 15:21:59 +0530

    Time: 360 ms (82.93%), Space: 107.7 MB (70.32%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,23 +1,46 @@
-// Solution 1: uses a visited array
+// // Solution 1: uses a visited array
+// class Solution {
+// private:
+//     int dfs(int source, vector<vector<int>> &graph, vector<bool> &visited) {
+//         visited[source] = true;
+//         int count = 0;
+//         for(const int &adjNode : graph[source])
+//             if(!visited[abs(adjNode)])
+//                 count += ((adjNode > 0) + dfs(abs(adjNode), graph, visited));
+//         return count;
+//     }
+// public:
+//     int minReorder(int n, vector<vector<int>>& connections) {
+//         // step 1: build the graph with nodes: +ve for front edge and -ve for back edge
+//         vector<vector<int>> graph(n);
+//         for(const auto &c : connections) {
+//             graph[c[0]].emplace_back(c[1]);
+//             graph[c[1]].emplace_back(-c[0]);
+//         }
+//         vector<bool> visited(n, false);
+//         return dfs(0, graph, visited);
+//     }
+// };
+
+// Solution 2: Uses just the edges and count how many of them need to be reversed
 class Solution {
 private:
-    int dfs(int source, vector<vector<int>> &graph, vector<bool> &visited) {
-        visited[source] = true;
-        int count = 0;
-        for(const int &adjNode : graph[source])
-            if(!visited[abs(adjNode)])
-                count += ((adjNode > 0) + dfs(abs(adjNode), graph, visited));
-        return count;
+    void dfs(int source, int parent, vector<vector<pair<int, int>>> &graph, int &reversed) {
+        for(const auto &adjNode : graph[source]) {
+            if(adjNode.first == parent) continue;
+            reversed += adjNode.second;
+            dfs(adjNode.first, source, graph, reversed);
+        }
     }
 public:
     int minReorder(int n, vector<vector<int>>& connections) {
-        // step 1: build the graph with nodes: +ve for front edge and -ve for back edge
-        vector<vector<int>> graph(n);
+        vector<vector<pair<int, int>>> graph(n);
         for(const auto &c : connections) {
-            graph[c[0]].emplace_back(c[1]);
-            graph[c[1]].emplace_back(-c[0]);
+            graph[c[0]].emplace_back(c[1], 1);
+            graph[c[1]].emplace_back(c[0], 0);
         }
-        vector<bool> visited(n, false);
-        return dfs(0, graph, visited);
+        int reversed = 0;
+        dfs(0, -1, graph, reversed);
+        return reversed;
     }
 };
