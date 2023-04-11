33c0b9f5afe5235c65822c60347e08caacb92750
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-04-11 15:53:35 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (43.31%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,10 +3,11 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n = size(matrix);
         // swap alternating row elements
-        reverse(matrix.begin(), matrix.end());
+        reverse(begin(matrix), end(matrix));
         // take transpose
         for(int i = 0; i < n; ++i)
             for(int j = i; j < n; ++j) 
-                swap(matrix[i][j], matrix[j][i]);
+                if(i != j)
+                    swap(matrix[i][j], matrix[j][i]);
     }
 };
