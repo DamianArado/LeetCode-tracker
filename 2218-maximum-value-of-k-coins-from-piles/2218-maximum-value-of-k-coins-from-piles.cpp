751f01b55928c4189609f4c48030c3099a3a9302
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-04-18 05:17:37 +0530

    Time: 264 ms (84.37%), Space: 18.2 MB (60.05%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -1,33 +1,23 @@
-class Solution
-{
-    public:
-        int maxValueOfCoins(vector<vector < int>> &p, int k)
-        {
-            int n = p.size();
-            vector<vector < int>> dp(n + 1, vector<int> (k + 1, -1));
-            return solve(0, dp, p, k);
+class Solution {
+private:
+    int f(int idx, vector<vector<int>> &piles, int n, int k, vector<vector<int>> &dp) {
+        if(idx == n) return 0;
+        if(dp[idx][k] != -1) return dp[idx][k];
+        int currentVal = 0, maxVal = 0;
+        // not take
+        maxVal = max(maxVal, f(idx + 1, piles, n, k, dp));
+        // take from top of piles: min 1 and max k 
+        for(int i = 0; i < size(piles[idx]); ++i) {
+            currentVal += piles[idx][i];
+            if(k - (i + 1) >= 0)
+                maxVal = max(maxVal, currentVal + f(idx + 1, piles, n, k - (i + 1), dp));
         }
-        int solve(int index, vector<vector < int>> &dp, vector< vector< int>> &p, int k)
-        {
-            if (index >= p.size())
-                return 0;
-
-            int maxi = 0, sum = 0;
-
-            if (dp[index][k] != -1)
-                return dp[index][k];
-            
-            //dont take from that pile
-            maxi = max(maxi, solve(index + 1, dp, p, k));
-
-            for (int i = 0; i < p[index].size(); i++)
-            {
-                sum += p[index][i];
-                
-                //take some certain prefix from that pile, if we can take that much elements.
-                if (k - (i + 1) >= 0)
-                    maxi = max(maxi, sum + solve(index + 1, dp, p, k - (i + 1)));
-            }
-            return dp[index][k] = maxi;	// return max
-        }
+        return dp[idx][k] = maxVal;
+    }
+public:
+    int maxValueOfCoins(vector<vector<int>>& piles, int k) {
+        int n = size(piles);
+        vector<vector<int>> dp(n, vector<int> (k + 1, -1));
+        return f(0, piles, n, k, dp);
+    }
 };
