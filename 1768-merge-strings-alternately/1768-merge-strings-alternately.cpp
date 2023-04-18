e850b316b617f01f5939d7dfe86e3af19459e730
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-04-18 05:39:51 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (60.87%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans;
+        int i = 0, j = 0, n1 = size(word1), n2 = size(word2);
+        while(i < n1 and j < n2) {
+            ans += word1[i];
+            ans += word2[j];
+            ++i;
+            ++j;
+        }
+        while(i < n1) ans += word1[i++];
+        while(j < n2) ans += word2[j++];
+        return ans;
+    }
+};
