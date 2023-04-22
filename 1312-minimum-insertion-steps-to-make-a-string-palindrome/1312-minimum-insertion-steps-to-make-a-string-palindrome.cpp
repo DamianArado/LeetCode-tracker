08013e2807bab2a842731e73d0eb61b08c2ec6b8
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-04-22 21:59:57 +0530

    Time: 84 ms (50.05%), Space: 27.9 MB (66.81%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,11 +1,11 @@
 // TC: O(n^2), SC: O(n)
 class Solution {
 private:
-    int findLCS(string &s1, string &s2) {
+    int findLCS(const string &s1, const string &s2) {
         int n = s1.size();
-        vector<int> dp(n + 1, 0);
+        vector<int> dp(n + 1);
         for(int i = 1; i <= n; ++i) {
-            vector<int> current(n + 1, 0);
+            vector<int> current(n + 1);
             for(int j = 1; j <= n; ++j) {
                 // match
                 if(s1[i - 1] == s2[j - 1])
@@ -21,7 +21,7 @@ private:
 public:
     int minInsertions(string &s1) {
         string s2 = s1;
-        reverse(s2.begin(), s2.end());
-        return s1.size() - findLCS(s1, s2);
+        reverse(begin(s2), end(s2));
+        return size(s1) - findLCS(s1, s2);
     }
 };
