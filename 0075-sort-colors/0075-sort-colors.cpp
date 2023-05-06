f2bc44c6ccab27ccffccafbfb89c7a9c5dcb6fa1
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-05-06 14:04:36 +0530

    Time: 6 ms (23.52%), Space: 8.3 MB (28.17%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -4,17 +4,9 @@ public:
         // this is a standard implementation of dutch national flag algo
         int low = 0, mid = 0, high = nums.size() - 1;
         while(mid <= high) {
-            switch(nums[mid]) {
-                case 0:
-                    swap(nums[mid++], nums[low++]);
-                    break;
-                case 1:
-                    ++mid;
-                    break;
-                case 2:
-                    swap(nums[mid], nums[high--]);
-                    break;
-            }
+            if(nums[mid] == 0) swap(nums[mid++], nums[low++]);
+            else if(nums[mid] == 1) ++mid;
+            else swap(nums[mid], nums[high--]);
         }
     }
 };
