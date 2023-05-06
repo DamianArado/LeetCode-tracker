8a6f6a9cc7ea3c9a4d80e4031fbbae418e1b4cac
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-05-06 14:45:33 +0530

    Time: 45 ms (38.68%), Space: 19 MB (86.03%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,11 +3,9 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n = size(intervals);
         if(n == 1) return intervals;
-        
         vector<vector<int>> mergedIntervals;
         sort(begin(intervals), end(intervals));
         int start = intervals[0][0], end = intervals[0][1];
-        
         for(int i = 1; i < n; ++i) {
             int next_start = intervals[i][0], next_end = intervals[i][1];
             if(next_start <= end) end = max(end, next_end);
@@ -17,7 +15,6 @@ public:
             }
         }
         mergedIntervals.push_back({start, end});
-        
         return mergedIntervals;
     }
 };
