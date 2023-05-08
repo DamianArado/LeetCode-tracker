0ae9dd448162e90bd677f6ef726116ab494ca58c
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-05-09 00:08:29 +0530

    Time: 3 ms (56.48%), Space: 9.9 MB (77.81%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(!p or !q) return p == q;
+        if(p == NULL or q == NULL) return p == q;
         return p->val == q->val and isSameTree(p->left, q->left) and isSameTree(p->right, q->right);
     }
 };
