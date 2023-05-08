fd6a0b7e24c5d73ceb682fd01a431c80568c4391
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-05-08 20:22:35 +0530

    Time: 7 ms (68.11%), Space: 8.9 MB (57.56%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,30 +3,28 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n1 = size(nums1), n2 = size(nums2);
         if(n2 == 1) return {-1};
-
-        // Step 1: find the next greater element for each nums2[i]
+        // step 1: find NGE for each element in nums2
         stack<int> s;
-        s.emplace(nums2[0]);
-        int x = nums2[1];
-        unordered_map<int, int> mp;
+        s.push(nums2[0]);
+        unordered_map<int, int> m;  // for storing NGE
+        int current = nums2[1]; // element which we will use to compare
         for(int i = 1; i < n2; ++i) {
-            x = nums2[i];
-            if(x <= s.top()) s.emplace(x);
-            else {
-                while(!s.empty() and x > s.top()) {
-                    mp[s.top()] = x;
+            current = nums2[i];
+            if(current > s.top())
+                while(!s.empty() and current > s.top()) {  // can be NGE
+                    m[s.top()] = current;
                     s.pop();
                 }
-                s.emplace(x);
-            }
+            s.push(current); // keep pushing 
         }
         while(!s.empty()) {
-            mp[s.top()] = -1;
+            m[s.top()] = -1;  // they don't have any NGE
             s.pop();
         }
-        // Step 2: use this result to fill answer for nums1 guys
-        vector<int> res;
-        for(int &num : nums1) res.emplace_back(mp[num]);
-        return res;
+        // step 2: use this resultant map to calculate the result for nums1
+        vector<int> ans;
+        for(const int &num1 : nums1)
+            ans.push_back(m[num1]);
+        return ans;
     }
 };
