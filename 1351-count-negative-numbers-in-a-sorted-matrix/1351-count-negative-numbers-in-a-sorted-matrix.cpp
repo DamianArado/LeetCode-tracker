486c8f502c6254af225698426645a166e663bf48
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-06-09 01:04:37 +0530

    Time: 20 ms (30.41%), Space: 10.4 MB (31.99%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -11,24 +11,37 @@
 //     }
 // };
 
+// class Solution {
+// private:
+//     int binarySearch(const vector<int> &row, int &n) {
+//         int start = 0, end = n - 1;
+//         while (start <= end) {
+//             int mid = start + (end - start) / 2;
+//             if (row[mid] < 0) end = mid - 1;
+//             else start = mid + 1;
+//         }
+//         return end;
+//     }
+// public:
+//     int countNegatives(vector<vector<int>>& grid) {
+//         int count = 0, n = size(grid[0]);
+//         for (const vector<int> &row : grid) {
+//             // apply binary search here and find the first element which is -ve
+//             int negatives = binarySearch(row, n);
+//             count += (n - 1 - negatives);
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
-private:
-    int binarySearch(const vector<int> &row, int &n) {
-        int start = 0, end = n - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (row[mid] < 0) end = mid - 1;
-            else start = mid + 1;
-        }
-        return end;
-    }
 public:
     int countNegatives(vector<vector<int>>& grid) {
-        int count = 0, n = size(grid[0]);
+        int count = 0, n = size(grid[0]), currentNegIdx = n - 1;
         for (const vector<int> &row : grid) {
-            // apply binary search here and find the first element which is -ve
-            int negatives = binarySearch(row, n);
-            count += (n - 1 - negatives);
+            while(currentNegIdx >= 0 and row[currentNegIdx] < 0)
+                --currentNegIdx;
+            count += (n - 1 - currentNegIdx);
         }
         return count;
     }
