7498ad06b34b7b035e1516aa9887e8d5edd8180a
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-06-09 00:54:44 +0530

    Time: 23 ms (11.48%), Space: 10.5 MB (13.32%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,11 +1,34 @@
+// class Solution {
+// public:
+//     // brute force
+//     int countNegatives(vector<vector<int>>& grid) {
+//         int count = 0;
+//         for (const vector<int> &row : grid) {
+//             for (const int &val : row) 
+//                 if (val < 0) ++count;
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
+private:
+    int binarySearch(const vector<int> &row, int &n) {
+        int start = 0, end = n - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if(row[mid] < 0) end = mid - 1;
+            else start = mid + 1;
+        }
+        return end;
+    }
 public:
-    // brute force
     int countNegatives(vector<vector<int>>& grid) {
-        int count = 0;
+        int count = 0, n = size(grid[0]);
         for (const vector<int> &row : grid) {
-            for (const int &val : row) 
-                if (val < 0) ++count;
+            // apply binary search here and find the first element which is -ve
+            int negatives = binarySearch(row, n);
+            count += (n - 1 - negatives);
         }
         return count;
     }
