55bfd8e1ce52cedf67b029f098b8519cd797a488
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-08-17 20:13:49 +0530

    Time: 52 ms (98.20%), Space: 27.2 MB (98.79%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -4,21 +4,21 @@ public:
         // Using DP here is a 2-step process
         int rows = size(mat), cols = size(mat[0]);
         // Step1: Compute shortest distances from top-left to bottom-right
-        for(int i = 0; i < rows; ++i) {
-            for(int j = 0; j < cols; ++j) {
-                if(mat[i][j] == 0) continue;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (mat[i][j] == 0) continue;
                 int top = 1e9, left = 1e9;
-                if(i > 0) top = mat[i - 1][j];
-                if(j > 0) left = mat[i][j - 1];
+                if (i > 0) top = mat[i - 1][j];
+                if (j > 0) left = mat[i][j - 1];
                 mat[i][j] = 1 + min(top, left);
             }
         }
         // Step2: Compute shortest distances from bottom-right to top-left
-        for(int i = rows - 1; i >= 0; --i) {
-            for(int j = cols - 1; j >= 0; --j) {
+        for (int i = rows - 1; i >= 0; --i) {
+            for (int j = cols - 1; j >= 0; --j) {
                 int bottom = 1e9, right = 1e9;
-                if(i < rows - 1) bottom = mat[i + 1][j];
-                if(j < cols - 1) right = mat[i][j + 1];
+                if (i < rows - 1) bottom = mat[i + 1][j];
+                if (j < cols - 1) right = mat[i][j + 1];
                 mat[i][j] = min(mat[i][j], 1 + min(bottom, right));
             }
         }
