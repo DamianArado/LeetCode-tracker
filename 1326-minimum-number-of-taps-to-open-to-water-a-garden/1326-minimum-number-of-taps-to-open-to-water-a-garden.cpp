22bb7a875be60c9b848b3ec2f2d85495a852b3f8
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-08-31 20:22:54 +0530

    Time: 32 ms (58.74%), Space: 14.8 MB (39.27%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     int minTaps(int n, vector<int>& ranges) {
-        int minTaps = 0;
-        vector<pair<int,int>> waterRange;
-        for(int i = 0; i <= n; ++i)
-            waterRange.push_back({i - ranges[i], i + ranges[i]});
-        sort(waterRange.begin(), waterRange.end(), [](pair<int,int> a, pair<int,int> b) {
-           return a.second > b.second;
-        });
-        int start = n, end = n, idx = 0;
-        while(end > 0) {
-            while(idx < waterRange.size() and waterRange[idx].second >= end) {
-                start = min(start, waterRange[idx].first);
-                idx++;
-            }
-            if(start == end)
-                return -1;
-            end = start;
-            minTaps++;
+        // dp[i] : min taps to water till index i
+        vector<int> dp(n + 1, n + 2);
+        dp[0] = 0;
+        for (int i = 0; i <= n; ++i) {
+            int left = max(0, i - ranges[i]), right = min(n, i + ranges[i]);
+            for (int j = left + 1; j <= right; ++j)
+                dp[j] = min(dp[j], dp[left] + 1);
         }
-        return minTaps;
+        return dp[n] >= n + 2 ? -1 : dp[n];
     }
 };
