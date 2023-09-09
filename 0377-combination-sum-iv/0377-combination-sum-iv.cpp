5fadc56ac2b0a6bc4cc6b5ecfaa19b2032c3cfd7
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-09-09 16:02:32 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (20.91%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,20 +1,43 @@
+// TC & SC : O(n*target)
+
+// class Solution {
+// private:
+//     int f(int idx, const int &n, const vector<int> &nums, const int &target, vector<vector<int>> &dp) {
+//         if (idx == n or target < 0) 
+//             return 0;
+//         if (target == 0)
+//             return 1;
+//         if (dp[idx][target] != -1)
+//             return dp[idx][target];
+//         int take = f(0, n, nums, target - nums[idx], dp);
+//         int skip = f(idx + 1, n, nums, target, dp);
+//         return dp[idx][target] = take + skip;
+//     }
+// public:
+//     int combinationSum4(vector<int>& nums, int target) {
+//         int n = size(nums);
+//         vector<vector<int>> dp(n, vector<int> (target + 1, -1));
+//         return f(0, n, nums, target, dp);
+//     }
+// };
+// O(n*target) tc but O(t) sc
 class Solution {
 private:
-    int f(int idx, const int &n, const vector<int> &nums, const int &target, vector<vector<int>> &dp) {
+    int f(int idx, const int &n, const vector<int> &nums, const int &target, vector<int> &dp) {
         if (idx == n or target < 0) 
             return 0;
         if (target == 0)
             return 1;
-        if (dp[idx][target] != -1)
-            return dp[idx][target];
+        if (dp[target] != -1)
+            return dp[target];
         int take = f(0, n, nums, target - nums[idx], dp);
         int skip = f(idx + 1, n, nums, target, dp);
-        return dp[idx][target] = take + skip;
+        return dp[target] = take + skip;
     }
 public:
     int combinationSum4(vector<int>& nums, int target) {
         int n = size(nums);
-        vector<vector<int>> dp(n, vector<int> (target + 1, -1));
+        vector<int> dp(target + 1, -1);
         return f(0, n, nums, target, dp);
     }
 };
