7cfb37df819df82b334706d3a49506214379567a
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-09-18 13:27:54 +0530

    Time: 8 ms (76.19%), Space: 10.7 MB (13.97%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -0,0 +1,30 @@
+typedef pair<int, int> pii;
+class Solution {
+private:
+    int findSoldierCount(const vector<int> &row) {
+        int left = 0, right = size(row) - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (row[mid] == 1)
+                left = mid + 1;
+            else 
+                right = mid - 1;
+        }
+        return left;
+    }
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        priority_queue<pii, vector<pii>, greater<>> minHeap;
+        int idx = 0, n = size(mat), hSize = n;
+        vector<int> ans;
+        for (const auto &row : mat) {
+            int soldiers = findSoldierCount(row);
+            minHeap.emplace(soldiers, idx++);
+        }
+        while (hSize-- > n - k) {
+            ans.emplace_back(minHeap.top().second);
+            minHeap.pop();
+        }
+        return ans;
+    }
+};
