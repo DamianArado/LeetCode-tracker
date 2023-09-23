28f95ed9780f19adcbe2f876d7c0c2136bb87c1d
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-09-24 01:30:18 +0530

    Time: 75 ms (84.85%), Space: 13.7 MB (56.47%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -6,18 +6,20 @@ private:
     
     bool match(string &a, string &b) {
         // by default a > b and a.size - b.size = 1
-        if(size(a) - size(b) != 1) return false;
+        if (size(a) - size(b) != 1) 
+            return false;
         // if not then start matching 
         int ptr1 = 0, ptr2 = 0;
-        while(ptr1 < size(a)) {
-            if(a[ptr1] == b[ptr2] and ptr2 < size(b)) {
+        while (ptr1 < size(a)) {
+            if (a[ptr1] == b[ptr2] and ptr2 < size(b)) {
                 // for cases like: a = "humay", b = "huma"
                 ptr1++;
                 ptr2++;
             } else ptr1++;
         }
         // if both reach the end, matching done
-        if(ptr1 == size(a) and ptr2 == size(b)) return true;
+        if (ptr1 == size(a) and ptr2 == size(b)) 
+            return true;
         return false;
     }
 public:
@@ -27,11 +29,10 @@ public:
         sort(begin(words), end(words), comparator);
         int maxStrChain = 1;
         vector<int> dp(n, 1);
-        for(int idx = 1; idx < n; ++idx) {
-            for(int prev = 0; prev < idx; ++prev) {
-                if(match(words[idx], words[prev]) and dp[idx] < dp[prev] + 1)
+        for (int idx = 1; idx < n; ++idx) {
+            for (int prev = 0; prev < idx; ++prev) 
+                if (match(words[idx], words[prev]) and dp[idx] < dp[prev] + 1)
                     dp[idx] = 1 + dp[prev];
-            }
             maxStrChain = max(maxStrChain, dp[idx]);
         }
         return maxStrChain;
