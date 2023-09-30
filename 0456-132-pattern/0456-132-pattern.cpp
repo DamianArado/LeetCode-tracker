8548f9ea10e0f70437c383eb116d2f5c521b4ea8
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-09-30 12:32:54 +0530

    Time: 75 ms (27.39%), Space: 48.8 MB (37.36%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
-        int second = INT_MIN;
-        stack<int> s; // using a monotonically increasing stack
-        for(int i = size(nums) - 1; i >= 0; --i) {
-            // taking care of 1 in our pattern
-            if(nums[i] < second) return true;
-            // taking care of the 32 in our pattern
-            while(!s.empty() and nums[i] > s.top()) {
-                second = s.top();
-                s.pop();
-            }
+        // [1, 3, 2]: 1- nums[i], 3: stack, 2: last
+        int last = INT_MIN;
+        stack<int> s;
+        for (int i = size(nums) - 1; i >= 0; --i) {
+            // checking if it's the smallest
+            if (nums[i] < last)
+                return true;
+            // insert the bigger one in stack and maintain the smaller as last
+            while (!s.empty() and nums[i] > s.top()) 
+                last = s.top(), s.pop();
             s.emplace(nums[i]);
         }
         return false;
