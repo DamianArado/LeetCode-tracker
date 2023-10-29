c1fed9712d8c153c86bf0d19d8d2510d065bf304
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-10-29 18:26:29 +0530

    Time: 2 ms (53.09%), Space: 6.4 MB (39.09%) - LeetHub

--- 0458-poor-pigs/0458-poor-pigs.cpp
+++ 0458-poor-pigs/0458-poor-pigs.cpp
@@ -4,6 +4,7 @@ public:
         // min_pig_count determined by equation: buckets =max_sub_job_load ** min_pig_count
 		// max_sub_job_load  = minutesToTest / minutesToDie + 1
         // min_pig_count = ceil(log(buckets) / log(minutesToTest / minutesToDie + 1));
+        // (T+1)^p >= N
         return ceil(log2(buckets) / log2(minutesToTest / minutesToDie + 1));
     }
 };
