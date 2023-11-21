0100d15d8da11d2ace62463fdaf55d30698d163b
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2023-11-21 13:35:00 +0530

    Time: 85 ms (90.17%), Space: 57.1 MB (83.57%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
@@ -1,8 +1,8 @@
 class Solution {
 private:
-    int findReverse(int num) {
+    static int findReverse(int num) {
         int ans = 0;
-        for(; num > 0; num /= 10)
+        for (; num > 0; num /= 10)
             ans = ans * 10 + num % 10;
         return ans;
     }
@@ -11,7 +11,7 @@ public:
         // nums[i] - rev(nums[i]) == nums[j] - rev(nums[j])
         int ans = 0, mod = 1e9 + 7;
         unordered_map<int, int> mp;
-        for(int i = 0; i < size(nums); ++i)
+        for (int i = 0; i < size(nums); ++i)
             ans = (ans + mp[nums[i] - findReverse(nums[i])]++) % mod;
         return ans;
     }
