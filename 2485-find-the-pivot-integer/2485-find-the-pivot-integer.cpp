6ef529a96ee7e80c0dfa6e59fc9a44697f70c2d2
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2024-03-14 05:10:40 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (15.62%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -23,14 +23,10 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        if(n == 1) return 1;
-        int sum = n * (n + 1) / 2, start = 0, end = n;
-        while(start < end) {
-            int mid = start + (end - start) / 2;
-            if(mid * mid == sum) return mid;
-            else if(mid * mid > sum) end = mid - 1;
-            else start = mid + 1;
-        }
+        int sum = n * (n + 1) / 2;
+        double square = sqrt(sum);
+        if (ceil(square) == square)
+            return int(square);
         return -1;
     }
 };
