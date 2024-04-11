0110b11080b0a2e739d695d20494ad21c4c1a96b
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2024-04-12 01:01:16 +0530

    Time: 11 ms (59.46%), Space: 9.7 MB (86.82%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        // tc and sc: O(n)
+        if (num.size() == k) return "0";
+        string ans = "";
+        for (const char &c : num) {
+            // Step 1: check if the last element in string is greater than the one we are standing at
+            //         and if yes, remove it
+            while (!ans.empty() and ans.back() > c and k)
+                 --k, ans.pop_back();
+            // Step 2: otherwise add elements to our answer
+            // ans.size() > 0 just to make sure that we consider the trailing zeroes as well
+            if (!ans.empty() or c != '0') 
+                ans.push_back(c);
+        }
+        // just if we already have k left
+        while (!ans.empty() and k--)
+            ans.pop_back();
+        return ans == "" ? "0" : ans;
+    }
+};
