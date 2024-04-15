06eec3fa5b172cbfccac8b806fed1d22702f92ae
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2024-04-16 01:55:14 +0530

    Time: 5 ms (26.52%), Space: 10.9 MB (80.39%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -9,28 +9,42 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// MORE VERBOSE FOR SHITHEAD LIKE ME!
+// class Solution {
+// private:
+//     void calculateSum(TreeNode *root, int &sum, int &currentSum) {
+//         if (!root) return;
+//         if (!root->left and !root->right) {
+//             currentSum += root->val;
+//             sum += currentSum;
+//             return;
+//         }
+//         currentSum += root->val;
+//         currentSum *= 10;
+//         calculateSum(root->left, sum, currentSum);
+//         currentSum /= 10;
+//         currentSum *= 10;
+//         calculateSum(root->right, sum, currentSum);
+//         currentSum /= 10;
+//     }
+// public:
+//     int sumNumbers(TreeNode* root) {
+//         if (!root) return 0;
+//         int sum = 0, currentSum = 0;
+//         calculateSum(root, sum, currentSum);
+//         return sum;
+//     }
+// };
 class Solution {
 private:
-    void calculateSum(TreeNode *root, int &sum, int &currentSum) {
-        if (!root) return;
-        if (!root->left and !root->right) {
-            currentSum += root->val;
-            sum += currentSum;
-            return;
-        }
-        currentSum += root->val;
-        currentSum *= 10;
-        calculateSum(root->left, sum, currentSum);
-        currentSum /= 10;
-        currentSum *= 10;
-        calculateSum(root->right, sum, currentSum);
-        currentSum /= 10;
+    int sumNumbers(TreeNode *root, int sum) {
+        if (root == NULL) return 0;
+        if (root->left == NULL and root->right == NULL) 
+            return sum * 10 + root->val;
+        return sumNumbers(root->left, sum * 10 + root->val) + sumNumbers(root->right, sum * 10 + root->val);
     }
 public:
     int sumNumbers(TreeNode* root) {
-        if (!root) return 0;
-        int sum = 0, currentSum = 0;
-        calculateSum(root, sum, currentSum);
-        return sum;
+        return sumNumbers(root, 0);
     }
 };
