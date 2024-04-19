6e958e229531a8eafbc323476511fd14f4ef141c
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2024-04-20 02:47:04 +0530

    Time: 19 ms (98.04%), Space: 15.8 MB (81.51%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -2,17 +2,18 @@
 class Solution {
 private:
     void removeLand(vector<vector<char>>& grid, const int &i, const int &j, const int &row, const int &column) {
-        // check boundary conditions
-        if (i < 0 or i >= row or j < 0 or j >= column or grid[i][j] == '0') 
-            return;
         // otherwise change this land to water (remove land)
         // alternative approach using extra space would involve marking visited
         grid[i][j] = '0';
         // do the same in all 4 directions (this stops until we find a point where we see 0's in all 4 directions)
-        removeLand(grid, i - 1, j, row, column);
-        removeLand(grid, i + 1, j, row, column);
-        removeLand(grid, i, j - 1, row, column);
-        removeLand(grid, i, j + 1, row, column);
+        if (i - 1 >= 0 and grid[i - 1][j] == '1') 
+            removeLand(grid, i - 1, j, row, column);
+        if (i + 1 < row and grid[i + 1][j] == '1')
+            removeLand(grid, i + 1, j, row, column);
+        if (j - 1 >= 0 and grid[i][j - 1] == '1') 
+            removeLand(grid, i, j - 1, row, column);
+        if (j + 1 < column and grid[i][j + 1] == '1') 
+            removeLand(grid, i, j + 1, row, column);
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
