1f082c7cb50b2249e730715279fd8bee06a14bdf
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2024-04-27 02:37:08 +0530

    Time: 310 ms (36.38%), Space: 18.3 MB (34.63%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,31 +1,26 @@
 class Solution {
-public:
-    /**
-    
-    We are maintaining the answer in firstMin, here we will consider min 
-    to be firstMin if its not in the same column
-    as the previous firstMin, otherwise we'll consider min to be secondMin.
-    firstMin2, secondMin2 and pos2 are just here during the current iteration (column)
-    and they help us in updating firstMin, secondMin and position for the next column
-    
-    TC: O(n^2), SC: O(1)
-    
-    */
-    int minFallingPathSum(vector<vector<int>>& grid) {
-        int n = grid.size(), firstMin = 0, secondMin = 0, position = -1;
-        if(n == 1) return grid[0][0];
-        for(int row = 0; row < n; ++row) {
-            int firstMin2 = INT_MAX, secondMin2 = INT_MAX, position2 = -1;
-            for(int col = 0; col < n; ++col) {
-                int currentMin = position != col ? firstMin : secondMin;
-                if(grid[row][col] + currentMin < firstMin2) {
-                    secondMin2 = firstMin2;
-                    firstMin2 = grid[row][col] + currentMin;
-                    position2 = col;
-                } else secondMin2 = min(secondMin2, grid[row][col] + currentMin);
-            }
-            firstMin = firstMin2, secondMin = secondMin2, position = position2;
+private:
+    int findSum(int row, int col, const int &n, vector<vector<int>> &grid, vector<vector<int>> &dp) {
+        if (col < 0 or col > n - 1) 
+            return 1e9;
+        if (row == 0)
+            return grid[row][col];
+        if (dp[row][col] != -1)
+            return dp[row][col];
+        int current = grid[row][col], ans = INT_MAX;
+        for (int colTemp = 0; colTemp < n; ++colTemp) {
+            if (colTemp == col) continue;
+            ans = min(ans, findSum(row - 1, colTemp, n, grid, dp));
         }
-        return firstMin;
+        return dp[row][col] = current + ans;
+    }
+public:
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        int n = size(grid), minSum = 1e9;
+        vector<vector<int>> dp(n, vector<int> (n, -1));
+        // try starting with each element in the last row
+        for (int i = 0; i < n; ++i)
+            minSum = min(minSum, findSum(n - 1, i, n, grid, dp));
+        return minSum;
     }
 };
