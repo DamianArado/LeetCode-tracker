a8f30f8611903501dff932e68a9bdaf30eef809c
Mohammad Humayun Khan <khanhumayun95@gmail.com>
2024-05-04 13:58:06 +0530

    Time: 52 ms (82.35%), Space: 45.6 MB (8.97%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -5,9 +5,11 @@ public:
         int l = 0, r = size(people) - 1, boats = 0;
         while (l <= r) {
             int weight = people[l] + people[r];
-            if (weight <= limit) 
+            if (weight <= limit)
+                // both can go together
                 ++boats, ++l, --r;
             else 
+                // heaviest go alone
                 ++boats, --r;
         }
         return boats;
